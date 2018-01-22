7119807c84704cfdf9977c4034353bda14e2d96b
Konstantin Dinev <kdinev@mail.bw.edu>
2018-01-22 17:24:30 +0200

    Fixing the build error

--- src/app/grid/services/data.service.ts
+++ src/app/grid/services/data.service.ts
@@ -2,6 +2,10 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs/Observable";
 
+interface IServiceResponse {
+    results: any[];
+}
+
 @Injectable()
 export class DataService {
     private rndUsr: any[];
@@ -11,9 +15,10 @@ export class DataService {
 
     public getData(): Promise<any> {
         return new Promise((resolve, reject) => {
-            this.http.get("https://randomuser.me/api/?inc=gender,name,picture&results=" + 200).subscribe((data) => {
-                resolve(data.results);
-            });
+            this.http.get("https://randomuser.me/api/?inc=gender,name,picture&results=" + 200)
+                .subscribe((data: IServiceResponse) => {
+                    resolve(data.results);
+                });
         });
     }
 
