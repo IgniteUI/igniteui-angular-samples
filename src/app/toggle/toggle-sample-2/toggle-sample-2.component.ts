dd537580b8bcd8cbb6ba1f6c3d1bcef1d22be63a
INFRAGISTICS\DDincheva <desislava9dincheva@gmail.com>
2018-02-15 11:46:43 +0200

    toggle-samples added

---
+++ src/app/toggle/toggle-sample-2/toggle-sample-2.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { IgxToggleActionDirective, IgxToggleDirective } from "igniteui-angular/main";
+
+@Component({
+  selector: "app-toggle-sample-2",
+  styleUrls: ["./toggle-sample-2.component.css"],
+  templateUrl: "./toggle-sample-2.component.html"
+})
+export class ToggleSample2Component implements OnInit {
+
+  constructor() { }
+
+  public ngOnInit() {
+  }
+
+}
---
+++ src/app/toggle/toggle-sample-3/toggle-sample-3.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-toggle-sample-3",
+  styleUrls: ["./toggle-sample-3.component.css"],
+  templateUrl: "./toggle-sample-3.component.html"
+})
+export class ToggleSample3Component implements OnInit {
+
+  constructor() { }
+
+  public ngOnInit() {
+  }
+
+}
