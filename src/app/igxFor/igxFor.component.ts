dece004b4c8a56f4141b5bfc5896ab878f86e020
Maya <MKirova@infragistics.com>
2018-02-19 13:34:31 +0200

    Generating random names and avatars.

--- src/app/igxFor/igxFor.component.ts
+++ src/app/igxFor/igxFor.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from "@angular/core";
 import { IgxFilterOptions } from "igniteui-angular/main";
+import {femaleFNames, lastName, maleFNames, middleNames} from "./names";
 
 @Component({
     encapsulation: ViewEncapsulation.None,
@@ -8,98 +9,47 @@ import { IgxFilterOptions } from "igniteui-angular/main";
     templateUrl: "./igxFor.component.html"
 })
 export class IgxForComponent implements OnInit {
-
-  public search1: string;
-  public data: object[] = [];
-  public navItems: object[] = [{
-        avatar: "assets/images/avatar/1.jpg",
-        favorite: true,
-        key: "1",
-        link: "#",
-        phone: "770-504-2217",
-        text: "Terrance Orta"
-    }, {
-        avatar: "assets/images/avatar/2.jpg",
-        favorite: false,
-        key: "2",
-        link: "#",
-        phone: "423-676-2869",
-        text: "Richard Mahoney"
-    }, {
-        avatar: "assets/images/avatar/3.jpg",
-        favorite: false,
-        key: "3",
-        link: "#",
-        phone: "859-496-2817",
-        text: "Donna Price"
-    }, {
-        avatar: "assets/images/avatar/4.jpg",
-        favorite: false,
-        key: "4",
-        link: "#",
-        phone: "901-747-3428",
-        text: "Lisa Landers"
-    }, {
-        avatar: "assets/images/avatar/12.jpg",
-        favorite: true,
-        key: "5",
-        link: "#",
-        phone: "573-394-9254",
-        text: "Dorothy H. Spencer"
-    }, {
-        avatar: "assets/images/avatar/13.jpg",
-        favorite: false,
-        key: "6",
-        link: "#",
-        phone: "323-668-1482",
-        text: "Stephanie May"
-    }, {
-        avatar: "assets/images/avatar/14.jpg",
-        favorite: false,
-        key: "7",
-        link: "#",
-        phone: "401-661-3742",
-        text: "Marianne Taylor"
-    }, {
-        avatar: "assets/images/avatar/15.jpg",
-        favorite: true,
-        key: "8",
-        link: "#",
-        phone: "662-374-2920",
-        text: "Tammie Alvarez"
-    }, {
-        avatar: "assets/images/avatar/16.jpg",
-        favorite: true,
-        key: "9",
-        link: "#",
-        phone: "240-455-2267",
-        text: "Charlotte Flores"
-    }, {
-        avatar: "assets/images/avatar/17.jpg",
-        favorite: false,
-        key: "10",
-        link: "#",
-        phone: "724-742-0979",
-        text: "Ward Riley"
-    }];
-
-  get fo1() {
+    public search: string;
+    public data: object[] = [];
+    get fo() {
         const _fo = new IgxFilterOptions();
-        _fo.key = "text";
-        _fo.inputValue = this.search1;
+        _fo.key = "name";
+        _fo.inputValue = this.search;
         return _fo;
     }
-  constructor() { }
-
-  public ngOnInit() {
-    const data = this.navItems;
-    for (let i = 10; i < 100000; i++) {
-        const obj = Object.assign({}, data[i % 10]);
-        const key = "key";
-        obj[key] = i;
-        data.push(obj);
+    constructor() { }
+    public ngOnInit() {
+        const data = [];
+        for (let i = 0; i < 100000; i++) {
+            const item = this.generatePerson(i);
+            data.push(item);
+        }
+        this.data = data;
     }
-    this.data = data;
-  }
+    private generatePerson(index): object {
+        const item = new Person();
+        item.key = index;
+        const gender = index % 2 === 0 ? "M" : "F";
+        item.name = this.generateName(gender);
+        item.avatar = "https://randomuser.me/api/portraits/" +
+        (gender === "M" ? "men" : "women") +
+        "/" + Math.floor((Math.random() * 100)) + ".jpg";
+        item.favorite = Math.floor((Math.random() * 3)) % 3 === 0;
+        return item;
+    }
+    private generateName(gender): string {
+        let name = "";
+        const fNames = gender === "M" ? maleFNames : femaleFNames;
+        name += fNames[Math.floor(Math.random() * fNames.length)] + " ";
+        name += middleNames[Math.floor(Math.random() * middleNames.length)] + " ";
+        name += lastName[Math.floor(Math.random() * lastName.length)];
+        return name;
+    }
+}
 
+export class Person {
+  public key: number;
+  public name: string;
+  public favorite: boolean;
+  public avatar: string;
 }
