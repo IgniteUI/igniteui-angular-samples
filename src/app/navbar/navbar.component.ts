672b3b8de71a29b163e677f4e34d59e7f524cd19
INFRAGISTICS\DDincheva <desislava9dincheva@gmail.com>
2018-02-20 14:17:23 +0200

    add location as provider in navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,17 @@
-import { Location } from "@angular/common";
+import { Location, LocationStrategy, PathLocationStrategy } from "@angular/common";
 import { Component, OnInit, ViewEncapsulation } from "@angular/core";
 
 const CURRENT_VIEW = "Ignite UI JS Blocks";
 
 @Component({
     encapsulation: ViewEncapsulation.None,
+    providers: [Location, {provide: LocationStrategy, useClass: PathLocationStrategy}],
     selector: "app-navbar",
     styleUrls: ["./navbar.component.css"],
     templateUrl: "./navbar.component.html"
 })
 export class NavbarComponent implements OnInit {
+
     public currentView: string;
 
     constructor(private _location: Location) { }
