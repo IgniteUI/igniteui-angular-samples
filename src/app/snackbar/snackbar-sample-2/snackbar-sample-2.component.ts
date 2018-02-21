6356fc2acb06c9e8c05848f8171c78eb1683286f
Svetla Boykova <sboykova@infragistics.com>
2018-02-21 21:41:03 +0200

    Snackbar samples reworked
    #82

---
+++ src/app/snackbar/snackbar-sample-2/snackbar-sample-2.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-snackbar-sample-2",
+  styleUrls: ["./snackbar-sample-2.component.css"],
+  templateUrl: "./snackbar-sample-2.component.html"
+})
+export class SnackbarSample2Component implements OnInit {
+
+  constructor() { }
+
+  public ngOnInit() {
+  }
+
+  public close(element) {
+    element.hide();
+  }
+}
