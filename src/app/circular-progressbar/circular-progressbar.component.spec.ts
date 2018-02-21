6646575909bc952db778be84808042a497f074d7
INFRAGISTICS\DDincheva <desislava9dincheva@gmail.com>
2018-02-21 08:43:31 +0200

    initially added circular progressbar and fixed linting errors

---
+++ src/app/circular-progressbar/circular-progressbar.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CircularProgressbarComponent } from './circular-progressbar.component';
+
+describe('CircularProgressbarComponent', () => {
+  let component: CircularProgressbarComponent;
+  let fixture: ComponentFixture<CircularProgressbarComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CircularProgressbarComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CircularProgressbarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/circular-progressbar/circular-progressbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-circular-progressbar',
+  templateUrl: './circular-progressbar.component.html',
+  styleUrls: ['./circular-progressbar.component.css']
+})
+export class CircularProgressbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
