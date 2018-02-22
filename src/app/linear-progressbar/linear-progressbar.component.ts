65444c8aa0e28e6ba0373e17dc168c0f0438136e
INFRAGISTICS\DDincheva <desislava9dincheva@gmail.com>
2018-02-22 10:24:46 +0200

    removed unnecessary code from linear-progressbar sample

--- src/app/linear-progressbar/linear-progressbar.component.ts
+++ src/app/linear-progressbar/linear-progressbar.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit, QueryList, ViewChildren, ViewEncapsulation } from "@angular/core";
+import { Component, QueryList, ViewChildren, ViewEncapsulation } from "@angular/core";
 import { IgxCircularProgressBarComponent, IgxLinearProgressBarComponent } from "igniteui-angular/main";
 
 @Component({
+  encapsulation: ViewEncapsulation.None,
   selector: "app-linear-progressbar",
   styleUrls: ["./linear-progressbar.component.css"],
   templateUrl: "./linear-progressbar.component.html"
 })
-export class LinearProgressbarComponent implements OnInit {
+export class LinearProgressbarComponent {
 
-  public disable = false;
   public interval: any;
 
   @ViewChildren(IgxLinearProgressBarComponent, { read: IgxLinearProgressBarComponent })
@@ -16,30 +16,21 @@ export class LinearProgressbarComponent implements OnInit {
 
   constructor() { }
 
-  public ngOnInit() {
-  }
-
   public changeIcon() {
     return this.interval ? "pause" : "play_arrow";
-}
+  }
 
   public updateValue() {
-      this.disable = true;
       this.linearBars.map((bar) => bar.value += this.randomIntFromInterval(1, 3));
       const shouldStop = this.linearBars.toArray().every((bar) => bar.value >= bar.max);
       if (shouldStop) {
-        this.disable = false;
         this.interval = clearInterval(this.interval);
       }
-  }
-  public reset() {
-    this.linearBars.toArray().forEach((bar) => bar.value = 0);
   }
 
   public tick() {
       if (this.interval) {
           this.interval = clearInterval(this.interval);
-          this.disable = false;
           return;
       }
       this.interval = setInterval(this.updateValue.bind(this), 60);
