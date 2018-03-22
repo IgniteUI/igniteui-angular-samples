b9e91e3030d87a58085b344fca551ad94423ba62
hanastasov <hristo.anastasov@gmail.com>
2018-03-22 10:23:00 +0200

    Calendar template parts parsing for browser not supporting Intl

--- src/app/calendar/calendar-sample-4/calendar-sample-4.component.ts
+++ src/app/calendar/calendar-sample-4/calendar-sample-4.component.ts
@@ -6,6 +6,32 @@ import { IgxCalendarComponent } from "igniteui-angular/main";
   styleUrls: ["./calendar-sample-4.component.scss"],
   templateUrl: "./calendar-sample-4.component.html"
 })
+
 export class CalendarSample4Component {
+    public intlDateTimeFormat = new Intl.DateTimeFormat() as any;
+    public supportIntl: boolean = this.intlDateTimeFormat.formatToParts;
+    public MONTH_NAMES = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+      "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+    ];
+    public WEEKDAYS = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
     constructor() { }
-}
+
+    public parseYear(val: any) {
+      const date = val.date as Date;
+      return date.getFullYear();
+    }
+
+    public parseMonth(val: any) {
+      const date = val.date as Date;
+      return this.MONTH_NAMES[date.getMonth()];
+    }
+
+    public parseHeaderTemplate(val: any) {
+      const date = val.date as Date;
+      return this.MONTH_NAMES[date.getMonth()] + " " + date.getDate() + ", "
+        + this.WEEKDAYS[date.getDay()];
+    }
+
+
+ }
