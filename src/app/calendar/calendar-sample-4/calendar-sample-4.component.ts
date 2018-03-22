fecf592364d11bdd499b572259b19db90150943d
hanastasov <hristo.anastasov@gmail.com>
2018-03-22 15:18:35 +0200

    Parse calendar template parts for browsers not supporting Intl

--- src/app/calendar/calendar-sample-4/calendar-sample-4.component.ts
+++ src/app/calendar/calendar-sample-4/calendar-sample-4.component.ts
@@ -1,37 +1,18 @@
 import { Component, ViewChild } from "@angular/core";
 import { IgxCalendarComponent } from "igniteui-angular/main";
+import { DateParser} from "../../date-parser";
 
 @Component({
+  providers: [DateParser],
   selector: "app-calendar",
   styleUrls: ["./calendar-sample-4.component.scss"],
   templateUrl: "./calendar-sample-4.component.html"
 })
 
 export class CalendarSample4Component {
-    public intlDateTimeFormat = new Intl.DateTimeFormat() as any;
-    public supportIntl: boolean = this.intlDateTimeFormat.formatToParts;
-    public MONTH_NAMES = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
-      "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
-    ];
-    public WEEKDAYS = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  public intlDateTimeFormat = new Intl.DateTimeFormat() as any;
+  public formatParts: boolean = this.intlDateTimeFormat.formatToParts;
 
-    constructor() { }
-
-    public parseYear(val: any) {
-      const date = val.date as Date;
-      return date.getFullYear();
-    }
-
-    public parseMonth(val: any) {
-      const date = val.date as Date;
-      return this.MONTH_NAMES[date.getMonth()];
-    }
-
-    public parseHeaderTemplate(val: any) {
-      const date = val.date as Date;
-      return this.MONTH_NAMES[date.getMonth()] + " " + date.getDate() + ", "
-        + this.WEEKDAYS[date.getDay()];
-    }
-
+  constructor(public parser: DateParser) { }
 
  }
