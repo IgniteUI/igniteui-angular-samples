6069d059c9d0156fc3cd0693448f29d5384ee81a
Michael Harrington <mharrington@infragistics.com>
2018-04-06 18:17:08 -0400

    adding category chart overview sample

---
+++ src/app/category-chart/category-chart-samples.module.ts
@@ -0,0 +1,24 @@
+import { NgModule, ModuleWithProviders  } from '@angular/core';
+import { FormsModule } from "@angular/forms";
+import { CommonModule } from '@angular/common';
+import { CategoryChartOverviewComponent } from "./overview/category-chart-overview-sample.component"
+import { DataService } from "./services/data.service"
+import { IgxCategoryChartComponent } from "@infragistics/igniteui-angular-charts/ES5/igx-category-chart-component"
+import { IgxCategoryChartModule } from "@infragistics/igniteui-angular-charts/ES5/igx-category-chart-module"
+
+@NgModule({
+	imports: [ CommonModule, FormsModule, IgxCategoryChartModule ],
+	declarations: [
+		CategoryChartOverviewComponent
+	],
+	providers: [ DataService ],
+	exports: [ CategoryChartOverviewComponent ]
+})
+export class CategoryChartSamplesModule {
+	static forRoot(): ModuleWithProviders {
+		return {
+			ngModule: CategoryChartSamplesModule,
+			providers: [ DataService ]
+		}
+	}
+}
---
+++ src/app/category-chart/overview/category-chart-overview-sample.component.ts
@@ -0,0 +1,18 @@
+import { Component, ViewChild } from "@angular/core";
+import { DataService } from "../services/data.service";
+
+@Component({
+    selector: "app-category-chart-overview-sample",
+    styleUrls: ["./category-chart-overview-sample.component.scss"],
+	templateUrl: "./category-chart-overview-sample.component.html",
+	providers: [ DataService ]
+})
+export class CategoryChartOverviewComponent {
+
+	public chartType: string = "Auto";
+	public data: any;
+
+	constructor(private dataService: DataService) {
+		this.data = this.dataService.getData();
+	}
+}
