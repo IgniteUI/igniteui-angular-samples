baa88e5f71d04593fbaf28d1a76fab3581c077ae
Michael Harrington <mharrington@infragistics.com>
2018-04-13 12:08:45 -0400

    removing dataService for category-chart

--- src/app/category-chart/category-chart-samples.module.ts
+++ src/app/category-chart/category-chart-samples.module.ts
@@ -4,7 +4,6 @@ import { FormsModule } from "@angular/forms";
 import { IgxCategoryChartComponent } from "igniteui-angular-charts/ES5/igx-category-chart-component";
 import { IgxCategoryChartModule } from "igniteui-angular-charts/ES5/igx-category-chart-module";
 import { CategoryChartOverviewComponent } from "./overview/category-chart-overview-sample.component";
-import { DataService } from "./services/data.service";
 
 @NgModule({
     declarations: [
@@ -15,8 +14,7 @@ import { DataService } from "./services/data.service";
 export class CategoryChartSamplesModule {
     public static forRoot(): ModuleWithProviders {
         return {
-            ngModule: CategoryChartSamplesModule,
-            providers: [ DataService ]
+            ngModule: CategoryChartSamplesModule
         };
     }
 }
