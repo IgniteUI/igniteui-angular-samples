44d63d82dfa94c5db7611b9149e502647c3efd29
Deyan Kamburov <dkamburov@infragistics.com>
2018-04-19 16:36:05 +0300

    Add buttons for pinning and hiding columns

--- src/app/grid/grid-crm/grid-crm.component.ts
+++ src/app/grid/grid-crm/grid-crm.component.ts
@@ -1,13 +1,15 @@
 import {
     Component,
     OnInit,
+    QueryList,
     ViewChild,
     ViewEncapsulation
 } from "@angular/core";
 
+import { IgxColumnComponent } from "igniteui-angular/grid/column.component";
 import { IgxDateSummaryOperand, IgxNumberSummaryOperand, IgxSummaryResult } from "igniteui-angular/grid/grid-summary";
 import { IgxGridComponent } from "igniteui-angular/grid/grid.component";
-import { STRING_FILTERS } from "igniteui-angular/main";
+import { IgxToggleDirective, STRING_FILTERS } from "igniteui-angular/main";
 import { data } from "./data";
 
 class DealsSummary extends IgxNumberSummaryOperand {
@@ -72,14 +74,68 @@ export class GridCRMComponent implements OnInit {
     @ViewChild("grid1", { read: IgxGridComponent })
     public grid1: IgxGridComponent;
 
+    @ViewChild("toggleRefHiding") public toggleHiding: IgxToggleDirective;
+    @ViewChild("toggleRefPinning") public togglePinning: IgxToggleDirective;
+
     public localData: any[];
     public dealsSummary = DealsSummary;
     public earliestSummary = EarliestSummary;
     public soonSummary = SoonSummary;
 
+    public cols: QueryList<IgxColumnComponent>;
+    public hiddenColsLength: number;
+    public pinnedColsLength: number;
+
     constructor() { }
 
     public ngOnInit() {
         this.localData = data;
+    }
+
+    public ngAfterViewInit() {
+        this.cols = this.grid1.columnList;
+        this.hiddenColsLength = this.cols.filter((col) => col.hidden).length;
+        this.pinnedColsLength = this.grid1.pinnedColumns.length;
+    }
+
+    public toggleHidingContent() {
+        if (this.toggleHiding.collapsed) {
+            if (!this.togglePinning.collapsed) {
+                this.togglePinning.close(true);
+            }
+            this.toggleHiding.open(true);
+        } else {
+            this.toggleHiding.close(true);
+        }
+    }
+
+    public togglePinningContent() {
+        if (this.togglePinning.collapsed) {
+            if (!this.toggleHiding.collapsed) {
+                this.toggleHiding.close(true);
+            }
+            this.togglePinning.open(true);
+        } else {
+            this.togglePinning.close(true);
+        }
+    }
+
+    public toggleVisibility(col: IgxColumnComponent) {
+        if (col.hidden) {
+            this.hiddenColsLength--;
+        } else {
+            this.hiddenColsLength++;
+        }
+        col.hidden = !col.hidden;
+    }
+
+    public togglePin(col: IgxColumnComponent) {
+        if (col.pinned) {
+            this.grid1.unpinColumn(col.field);
+            this.pinnedColsLength--;
+        } else {
+            this.grid1.pinColumn(col.field);
+            this.pinnedColsLength++;
+        }
     }
 }
