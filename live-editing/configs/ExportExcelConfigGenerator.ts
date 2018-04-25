747ff8b7782518af41f7b070973848cae59f6457
INFRAGISTICS\tiliev <tiliev@infragistics.com>
2018-04-25 11:58:02 +0300

    Added immediate package to CSV/Excel live editing samples.

--- live-editing/configs/ExportExcelConfigGenerator.ts
+++ live-editing/configs/ExportExcelConfigGenerator.ts
@@ -5,10 +5,16 @@ import { IgxExcelExporterService } from "igniteui-angular/services/index";
 import { ExcelExportComponent } from "../../src/app/export-excel/excel-export.component";
 import { ExcelExportSample1Component } from "../../src/app/export-excel/excel-export-sample-1/excel-export-sample-1.component";
 import { IgxGridModule } from "igniteui-angular/main";
+import * as DR from "../DependencyResolver";
 
 export class ExportExcelConfigGenerator implements IConfigGenerator {
     generateConfigs(): Config[] {
         let configs = new Array<Config>();
+
+        // Including this because of the immediate package. Will remove it as soon as
+        // we resolve the issue with it.
+        let dependencies = DR.DEFAULT_DEPENDENCIES.slice();
+        dependencies.push("immediate");
 
         configs.push(new Config({
             component: ExcelExportComponent,
@@ -17,7 +23,8 @@ export class ExportExcelConfigGenerator implements IConfigGenerator {
                 ngDeclarations: [ExcelExportComponent],
                 ngImports: [],
                 ngProviders: [IgxExcelExporterService]
-            })
+            }),
+            packageDependencies: dependencies
         }));
 
         configs.push(new Config({
@@ -28,7 +35,8 @@ export class ExportExcelConfigGenerator implements IConfigGenerator {
                 ngImports: [IgxGridModule],
                 ngProviders: [IgxExcelExporterService]
             }),
-            shortenComponentPathBy: "/export-excel/"
+            shortenComponentPathBy: "/export-excel/",
+            packageDependencies: dependencies
         }));
 
         return configs;
