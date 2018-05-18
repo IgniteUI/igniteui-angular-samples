e330cb31162ad68088109f4f0a8572b95eaaaf71
Tacho <tjelev22@abv.bg>
2018-05-18 17:29:02 +0300

    feat: Add styles and prevent caret movement
    IgniteUI/igniteui-angular#961

--- src/app/grid/grid-search-sample/grid-search-sample.component.ts
+++ src/app/grid/grid-search-sample/grid-search-sample.component.ts
@@ -26,8 +26,10 @@ export class GridSearchSampleComponent implements OnInit {
 
     public searchKeyDown(ev) {
         if (ev.key === "Enter" || ev.key === "ArrowDown" || ev.key === "ArrowRight") {
+            ev.preventDefault();
             this.grid.findNext(this.searchText, this.caseSensitive);
         } else if (ev.key === "ArrowUp" || ev.key === "ArrowLeft") {
+            ev.preventDefault();
             this.grid.findPrev(this.searchText, this.caseSensitive);
         }
     }
