73f34537833c488dc4ae4f540cc8235811aca4f9
zdrawku <zdrawko.kolew@gmail.com>
2018-05-22 16:41:34 +0300

    Remove using of the service for images entirely

--- src/app/list/list-sample-4/list-sample-4.component.ts
+++ src/app/list/list-sample-4/list-sample-4.component.ts
@@ -13,31 +13,31 @@ export class ListSample4Component implements OnInit {
       isFavorite: false,
       name: "Terrance Orta",
       phone: "770-504-2217",
-      photo: "https://randomuser.me/api/portraits/men/27.jpg"
+      photo: "assets/images/men/27.jpg"
     },
     {
       isFavorite: true,
       name: "Richard Mahoney",
       phone: "423-676-2869",
-      photo: "https://randomuser.me/api/portraits/men/1.jpg"
+      photo: "assets/images/men/1.jpg"
     },
     {
       isFavorite: false,
       name: "Donna Price",
       phone: "859-496-2817",
-      photo: "https://randomuser.me/api/portraits/women/50.jpg"
+      photo: "assets/images/women/50.jpg"
     },
     {
       isFavorite: false,
       name: "Lisa Landers",
       phone: "901-747-3428",
-      photo: "https://randomuser.me/api/portraits/women/3.jpg"
+      photo: "assets/images/women/3.jpg"
     },
     {
       isFavorite: true,
       name: "Dorothy H. Spencer",
       phone: "573-394-9254",
-      photo: "https://randomuser.me/api/portraits/women/67.jpg"
+      photo: "assets/images/women/67.jpg"
     }
   ];
 
