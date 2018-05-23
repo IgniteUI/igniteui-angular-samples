7eecd8ceead19e38adcab7e40c3e30086802f8e1
Svetla Boykova <sboykova@infragistics.com>
2018-05-23 20:08:22 +0300

    docs(grid): Removed unused package #353

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { InMemoryWebApiModule } from "angular-in-memory-web-api";
 import {
     IgxAvatarModule,
     IgxBadgeModule,
