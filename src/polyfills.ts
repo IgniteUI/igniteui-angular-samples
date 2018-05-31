7de4d0badce82d3041cfce5847f93919691e013e
zdrawku <zdrawko.kolew@gmail.com>
2018-05-31 16:36:10 +0300

    Remove Intl polyfills, remove forEach usage in grid sample

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -64,9 +64,9 @@ import "zone.js/dist/zone";  // Included with Angular CLI.
  * Date, currency, decimal and percent pipes.
  * Needed for: All but Chrome, Firefox, Edge, IE11 and Safari 10
  */
-import "intl";  // Run `npm install --save intl`.
-import "intl/locale-data/jsonp/de";
+// import "intl";  // Run `npm install --save intl`.
+// import "intl/locale-data/jsonp/de";
 /**
  * Need to import at least one locale-data with intl.
  */
-import "intl/locale-data/jsonp/en";
+// import "intl/locale-data/jsonp/en";
