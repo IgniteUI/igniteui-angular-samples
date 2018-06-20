fbaa49728e29cdd93242efbf45ad0e8387887fc7
Konstantin Dinev <kdinev@mail.bw.edu>
2018-06-20 18:34:04 +0300

    Fixing the IForOfState reference

--- src/app/grid/grid-sample-4/grid-sample-4.component.ts
+++ src/app/grid/grid-sample-4/grid-sample-4.component.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { ChangeDetectorRef, Component, Injectable, TemplateRef, ViewChild } from "@angular/core";
-import { IgxColumnComponent, IgxGridComponent } from "igniteui-angular";
-import { IForOfState } from "igniteui-angular/lib/directives/for-of/IForOfState";
+import { IForOfState, IgxColumnComponent, IgxGridComponent } from "igniteui-angular";
 import { BehaviorSubject, Observable } from "rxjs";
 
 @Injectable()
