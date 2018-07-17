e5daabbcfbd826ad42ac3114c938597461ec46b2
npaunov <npaunov@infragistics.com>
2018-07-17 17:34:59 +0300

    feat(igx-icon): add icon size sample

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -146,6 +146,7 @@ import { GridToolbarSample3Component} from "./grid/grid-toolbar-sample/grid-tool
 import { GridComponent } from "./grid/grid.component";
 import { GridMultiColumnHeadersComponent } from "./grid/multi-column-headers/multi-column-headers";
 import { IconSample1Component } from "./icon/icon-sample-1/icon-sample-1.component";
+import { IconSample2Component } from "./icon/icon-sample2/icon-sample2.component";
 import { IgxForComponent } from "./igxFor/igxFor.component";
 import { InputGroupSample1Component } from "./input-group/input-group-sample-1/input-group-sample-1.component";
 import { InputGroupSample2Component } from "./input-group/input-group-sample-2/input-group-sample-2.component";
@@ -611,6 +612,10 @@ export const appRoutes: Routes = [
     {
         component: IconSample1Component,
         path: "icon-sample-1"
+    },
+    {
+        component: IconSample2Component,
+        path: "icon-sample2"
     },
     {
         component: ButtonsSample1Component,
---
+++ src/app/icon/icon-sample2/icon-sample2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-icon-sample2',
+  templateUrl: './icon-sample2.component.html',
+  styleUrls: ['./icon-sample2.component.scss']
+})
+export class IconSample2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
