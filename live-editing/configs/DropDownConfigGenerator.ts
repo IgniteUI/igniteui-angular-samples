42aaab20695369a4a6567f0823c5cf1ee80f7497
ViktorSlavov <viktorslavov91@gmail.com>
2018-08-15 12:11:25 +0300

    fix(igx-dropdown): add IgxButtonModule to sample confing for IgxDropDown, #597

--- live-editing/configs/DropDownConfigGenerator.ts
+++ live-editing/configs/DropDownConfigGenerator.ts
@@ -1,6 +1,6 @@
 /* tslint:disable:object-literal-sort-keys */
 
-import { IgxDropDownModule} from "igniteui-angular";
+import { IgxButtonModule, IgxDropDownModule} from "igniteui-angular";
 import { DropDownSample1Component } from "../../src/app/dropdown/dropdown-sample-1/dropdown-sample-1.component";
 import { DropDownSample2Component } from "../../src/app/dropdown/dropdown-sample-2/dropdown-sample-2.component";
 import { DropDownSample3Component } from "../../src/app/dropdown/dropdown-sample-3/dropdown-sample-3.component";
@@ -16,9 +16,9 @@ export class DropDownConfigGenerator implements IConfigGenerator {
         configs.push(new Config({
             component: DropDownSample1Component,
             appModuleConfig: new AppModuleConfig({
-                imports: [IgxDropDownModule, DropDownSample1Component],
+                imports: [IgxDropDownModule, DropDownSample1Component, IgxButtonModule],
                 ngDeclarations: [DropDownSample1Component],
-                ngImports: [IgxDropDownModule]
+                ngImports: [IgxDropDownModule, IgxButtonModule]
             }),
             shortenComponentPathBy: "/dropdown/"
         }));
@@ -26,9 +26,9 @@ export class DropDownConfigGenerator implements IConfigGenerator {
         configs.push(new Config({
             component: DropDownSample2Component,
             appModuleConfig: new AppModuleConfig({
-                imports: [IgxDropDownModule, DropDownSample2Component],
+                imports: [IgxDropDownModule, DropDownSample2Component, IgxButtonModule],
                 ngDeclarations: [DropDownSample2Component],
-                ngImports: [IgxDropDownModule]
+                ngImports: [IgxDropDownModule, IgxButtonModule]
             }),
             shortenComponentPathBy: "/dropdown/"
         }));
@@ -36,9 +36,9 @@ export class DropDownConfigGenerator implements IConfigGenerator {
         configs.push(new Config({
             component: DropDownSample3Component,
             appModuleConfig: new AppModuleConfig({
-                imports: [IgxDropDownModule, DropDownSample3Component],
+                imports: [IgxDropDownModule, DropDownSample3Component, IgxButtonModule],
                 ngDeclarations: [DropDownSample3Component],
-                ngImports: [IgxDropDownModule]
+                ngImports: [IgxDropDownModule, IgxButtonModule]
             }),
             shortenComponentPathBy: "/dropdown/"
         }));
