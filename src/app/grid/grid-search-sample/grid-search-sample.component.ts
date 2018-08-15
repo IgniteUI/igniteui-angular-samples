44c0c60b7db26a71e4fd2d4aefe811bd5a5c498a
Tacho <tjelev22@abv.bg>
2018-08-15 14:37:54 +0300

    feat(exactMatch): include exactMatch option to gridSearch sample
    IgniteUI/igniteui-angular#2369

--- src/app/grid/grid-search-sample/grid-search-sample.component.ts
+++ src/app/grid/grid-search-sample/grid-search-sample.component.ts
@@ -13,6 +13,7 @@ export class GridSearchSampleComponent implements OnInit {
     public data: any[];
     public searchText: string = "";
     public caseSensitive: boolean = false;
+    public exactMatch: boolean = false;
 
     public ngOnInit(): void {
         this.data = MARKET_DATA;
@@ -26,15 +27,20 @@ export class GridSearchSampleComponent implements OnInit {
     public searchKeyDown(ev) {
         if (ev.key === "Enter" || ev.key === "ArrowDown" || ev.key === "ArrowRight") {
             ev.preventDefault();
-            this.grid.findNext(this.searchText, this.caseSensitive);
+            this.grid.findNext(this.searchText, this.caseSensitive, this.exactMatch);
         } else if (ev.key === "ArrowUp" || ev.key === "ArrowLeft") {
             ev.preventDefault();
-            this.grid.findPrev(this.searchText, this.caseSensitive);
+            this.grid.findPrev(this.searchText, this.caseSensitive, this.exactMatch);
         }
     }
 
     public updateSearch() {
         this.caseSensitive = !this.caseSensitive;
-        this.grid.findNext(this.searchText, this.caseSensitive);
+        this.grid.findNext(this.searchText, this.caseSensitive, this.exactMatch);
+    }
+
+    public updateExactSearch() {
+        this.exactMatch = !this.exactMatch;
+        this.grid.findNext(this.searchText, this.caseSensitive, this.exactMatch);
     }
 }
