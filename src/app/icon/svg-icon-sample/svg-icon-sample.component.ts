26aecf094566fcf5a94408aaff69511ff74cdf5f
Svetla Boykova <sboykova@infragistics.com>
2018-10-04 16:29:48 +0300

    docs(icon): Fixes in naming #662

--- src/app/icon/svg-icon-sample/svg-icon-sample.component.ts
+++ src/app/icon/svg-icon-sample/svg-icon-sample.component.ts
@@ -12,12 +12,12 @@ export class SvgIconSampleComponent implements OnInit {
 
     public ngOnInit() {
         // register custom SVG icons
-        this.iconService.addSvgIcon("contains", "/assets/images/svg/contains.svg", "filtering");
-        this.iconService.addSvgIcon("does_not_contain", "/assets/images/svg/does_not_contain.svg", "svg-flags");
-        this.iconService.addSvgIcon("does_not_equal", "/assets/images/svg/does_not_equal.svg", "svg-flags");
-        this.iconService.addSvgIcon("ends_with", "/assets/images/svg/ends_with.svg", "svg-flags");
-        this.iconService.addSvgIcon("equals", "/assets/images/svg/equals.svg", "svg-flags");
-        this.iconService.addSvgIcon("is_empty", "/assets/images/svg/is_empty.svg", "svg-flags");
-        this.iconService.addSvgIcon("starts_with", "/assets/images/svg/starts_with.svg", "svg-flags");
+        this.iconService.addSvgIcon("contains", "/assets/images/svg/contains.svg", "filter-icons");
+        this.iconService.addSvgIcon("does_not_contain", "/assets/images/svg/does_not_contain.svg", "filter-icons");
+        this.iconService.addSvgIcon("does_not_equal", "/assets/images/svg/does_not_equal.svg", "filter-icons");
+        this.iconService.addSvgIcon("ends_with", "/assets/images/svg/ends_with.svg", "filter-icons");
+        this.iconService.addSvgIcon("equals", "/assets/images/svg/equals.svg", "filter-icons");
+        this.iconService.addSvgIcon("is_empty", "/assets/images/svg/is_empty.svg", "filter-icons");
+        this.iconService.addSvgIcon("starts_with", "/assets/images/svg/starts_with.svg", "filter-icons");
     }
 }
