318884ed95901d08a42f7294d3751a93ced296f3
Martin Kamenov <MKamenov@infragistics.com>
2018-10-04 09:59:22 +0300

    Removed initial area. Added swapping of puzzle parts.

--- src/app/drag-drop/drag-drop.component.ts
+++ src/app/drag-drop/drag-drop.component.ts
@@ -40,25 +40,16 @@ export class DragAndDropSampleComponent implements OnInit {
         }
     ];
 
-    private completed: boolean;
-
     constructor() {
     }
 
     public ngOnInit() {
+        this.puzzleBoard = [];
         this.shuffleArray();
-        this.puzzleBoard = [ [-1, -1, -1], [-1, -1, -1], [-1, -1, -1] ];
-        this.completed = false;
     }
 
     public getAllPiecesArray() {
         return [].concat(...this.pieces);
-    }
-
-    public checkIfPuzzleCompleted() {
-        if (this.completed) {
-            alert("Congratulations!\nYou have successfully solved the puzzle.");
-        }
     }
 
     public onPieceDropped(ev) {
@@ -67,22 +58,40 @@ export class DragAndDropSampleComponent implements OnInit {
         const dropId = dropIdString.match(/\d+/g).map(Number)[0];
         const dragId = dragIdString.match(/\d+/g).map(Number)[0];
 
-        const coll = dropId % 3;
-        const row = Math.floor(dropId / 3);
-        if (dropId === 9) {
-            this.removePart(dragId);
-            return;
+        const foundIndex = this.findIndexOfDrag(dragId);
+        const firstPartRow = foundIndex.row;
+        const firstPartColl = foundIndex.coll;
+        const secondPartRow = Math.floor(dropId / 3);
+        const secondPartColl = dropId % 3;
+        this.swapPieces(firstPartRow, firstPartColl, secondPartRow, secondPartColl);
+        ev.cancel = true;
+        ev.drag.dropFinished();
+        if (this.checkForCompletedPuzzle()) {
+            setTimeout(this.showMessage, 100);
         }
+    }
 
-        if (this.puzzleBoard[row][coll] === -1) {
-            this.removePart(dragId);
-            this.puzzleBoard[row][coll] = dragId;
-            if (this.checkForCompletedPuzzle()) {
-                this.completed = true;
+    private showMessage() {
+        alert("Congratulations!\nYou have successfully solved the puzzle.");
+    }
+
+    private swapPieces(firstPartRow, firstPartColl, secondPartRow, secondPartColl) {
+        const temp = this.puzzleBoard[firstPartRow][firstPartColl];
+        this.puzzleBoard[firstPartRow][firstPartColl] = this.puzzleBoard[secondPartRow][secondPartColl];
+        this.puzzleBoard[secondPartRow][secondPartColl] = temp;
+
+        const temp2 = this.pieces[firstPartRow][firstPartColl];
+        this.pieces[firstPartRow][firstPartColl] = this.pieces[secondPartRow][secondPartColl];
+        this.pieces[secondPartRow][secondPartColl] = temp2;
+    }
+
+    private findIndexOfDrag(dragId) {
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (this.puzzleBoard[i][j] === dragId) {
+                    return { row: i, coll: j };
+                }
             }
-        } else {
-            ev.cancel = true;
-            ev.drag.dropFinished();
         }
     }
 
@@ -98,16 +107,6 @@ export class DragAndDropSampleComponent implements OnInit {
         return true;
     }
 
-    private removePart(id) {
-        for (let i = 0; i < 3; i++) {
-            for (let j = 0; j < 3; j++) {
-                if (this.puzzleBoard[i][j] === id) {
-                    this.puzzleBoard[i][j] = -1;
-                }
-            }
-        }
-    }
-
     private shuffleArray(): void {
         for (let i = this.piecesArr.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
@@ -118,8 +117,10 @@ export class DragAndDropSampleComponent implements OnInit {
 
         for (let i = 0; i < 3; i++) {
             this.pieces.push([]);
+            this.puzzleBoard.push([]);
             for (let j = 0; j < 3; j++) {
                 this.pieces[i].push(this.piecesArr[i * 3 + j]);
+                this.puzzleBoard[i].push(this.piecesArr[i * 3 + j].id);
             }
         }
     }
