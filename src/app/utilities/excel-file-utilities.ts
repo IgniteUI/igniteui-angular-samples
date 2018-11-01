9c2b857706d6a5213c0708592741b332d09e9500
agoldenbaum <andrew.goldenbaum@gmail.com>
2018-11-01 15:23:35 -0400

    De-lint files

--- src/app/utilities/excel-file-utilities.ts
+++ src/app/utilities/excel-file-utilities.ts
@@ -1,58 +1,68 @@
+import { saveAs } from "file-saver";
 import { Workbook } from "igniteui-angular-excel/ES5/Workbook";
-import { WorkbookSaveOptions } from "igniteui-angular-excel/ES5/WorkbookSaveOptions";
 import { WorkbookFormat } from "igniteui-angular-excel/ES5/WorkbookFormat";
-import { saveAs } from 'file-saver';
+import { WorkbookSaveOptions } from "igniteui-angular-excel/ES5/WorkbookSaveOptions";
 
 export class ExcelFileUtilities {
-    static getFileExtension(format: WorkbookFormat) {
+    public static getFileExtension(format: WorkbookFormat) {
         switch (format) {
             case WorkbookFormat.StrictOpenXml:
             case WorkbookFormat.Excel2007:
-              return ".xlsx";
+                return ".xlsx";
             case WorkbookFormat.Excel2007MacroEnabled:
-              return ".xlsm";
+                return ".xlsm";
             case WorkbookFormat.Excel2007MacroEnabledTemplate:
-              return ".xltm";
+                return ".xltm";
             case WorkbookFormat.Excel2007Template:
-              return ".xltx";
+                return ".xltx";
             case WorkbookFormat.Excel97To2003:
-              return ".xls";
+                return ".xls";
             case WorkbookFormat.Excel97To2003Template:
-              return ".xlt";
-          }
+                return ".xlt";
+        }
     }
 
-    static load(file: File): Promise<Workbook> {
-      return new Promise<Workbook>((resolve, reject) => {
-        ExcelFileUtilities.readFileAsUint8Array(file).then((a) =>
-        {
-          Workbook.load(a, null, (w) =>
-          {
-            resolve(w);
-          }, (e) =>
-          {
-            reject(e);
-          });
-        }, (e) =>
-        {
-          reject(e);
+    public static load(file: File): Promise<Workbook> {
+        return new Promise<Workbook>((resolve, reject) => {
+            ExcelFileUtilities.readFileAsUint8Array(file).then((a) => {
+                Workbook.load(a, null, (w) => {
+                    resolve(w);
+                }, (e) => {
+                    reject(e);
+                });
+            }, (e) => {
+                reject(e);
+            });
         });
-      });
+    }
+
+    public static save(workbook: Workbook, fileNameWithoutExtension: string): Promise<string> {
+        return new Promise<string>((resolve, reject) => {
+            const opt = new WorkbookSaveOptions();
+            opt.type = "blob";
+
+            workbook.save(opt, (d) => {
+                const fileName = fileNameWithoutExtension + ExcelFileUtilities.getFileExtension(workbook.currentFormat);
+                saveAs(d as Blob, fileName);
+                resolve(fileName);
+            }, (e) => {
+                reject(e);
+            });
+        });
     }
 
     private static readFileAsUint8Array(file: File): Promise<Uint8Array> {
-        return new Promise<Uint8Array>((resolve, reject) =>
-        {
-            var fr = new FileReader();
+        return new Promise<Uint8Array>((resolve, reject) => {
+            const fr = new FileReader();
             fr.onerror = (e) => {
                 reject(fr.error);
             };
 
             if (fr.readAsBinaryString) {
                 fr.onload = (e) => {
-                    var str: string = (<any>fr).resultString != null ? (<any>fr).resultString : fr.result;
-                    var result = new Uint8Array(str.length);
-                    for (var i = 0; i < str.length; i++) {
+                    const str: string = (fr as any).resultString != null ? (fr as any).resultString : fr.result;
+                    const result = new Uint8Array(str.length);
+                    for (let i = 0; i < str.length; i++) {
                         result[i] = str.charCodeAt(i);
                     }
                     resolve(result);
@@ -61,26 +71,10 @@ export class ExcelFileUtilities {
                 fr.readAsBinaryString(file);
             } else {
                 fr.onload = (e) => {
-                    resolve(new Uint8Array(<ArrayBuffer>fr.result));
+                    resolve(new Uint8Array(fr.result as ArrayBuffer));
                 };
-                
                 fr.readAsArrayBuffer(file);
             }
         });
-    }
-
-    static save(workbook: Workbook, fileNameWithoutExtension: string): Promise<string> {
-      return new Promise<string>((resolve, reject) => {
-        var opt = new WorkbookSaveOptions();
-        opt.type = "blob";
-
-        workbook.save(opt, (d) => {
-          var fileName = fileNameWithoutExtension + ExcelFileUtilities.getFileExtension(workbook.currentFormat);
-          saveAs(<Blob>d, fileName);
-          resolve(fileName);
-        }, (e) => {
-          reject(e);
-        });
-      });
     }
 }
