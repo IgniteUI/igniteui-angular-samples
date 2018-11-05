87afa9997e7251c67e1255adcbb1d1ddf4ee2356
NikolayAlipiev <nikolay.alipiev@gmail.com>
2018-11-05 16:23:13 +0200

    revert change

--- src/app/grid/grid-batch-editing/grid-batch-editing-sample.component.ts
+++ src/app/grid/grid-batch-editing/grid-batch-editing-sample.component.ts
@@ -95,6 +95,6 @@ export class GridBatchEditingSampleComponent {
     }
 
     public get hasTransactions(): boolean {
-        return (this.gridRowEditTransaction.transactions.getAggregatedChanges(false) || []).length > 0;
+        return (this.gridRowEditTransaction.transactions.aggregatedState(false) || []).length > 0;
     }
 }
