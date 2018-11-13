9651afa78fce0ac2b64c53fafd5fe3491d8000a9
agoldenbaum <andrew.goldenbaum@gmail.com>
2018-11-13 17:32:04 -0500

    Import IgxExcelModule instead of IgxExcelXlsxModule

--- src/app/excel-library/working-with/tables.module.ts
+++ src/app/excel-library/working-with/tables.module.ts
@@ -2,16 +2,9 @@ import { CommonModule } from "@angular/common";
 import { ModuleWithProviders, NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { RouterModule, Routes } from "@angular/router";
-
-// TODO import either CategoryChart, DataChart, or Grid module:
-// import { IgxButtonModule, IgxGridModule } from "igniteui-angular";
 import { IgxCategoryChartModule } from "igniteui-angular-charts/ES5/igx-category-chart-module";
-import { IgxExcelXlsxModule } from "igniteui-angular-excel/ES5/igx-excel-xlsx-module";
-// import { IgxDataChartCategoryModule } from "igniteui-angular-charts/ES5/igx-data-chart-category-module";
-// import { IgxDataChartCoreModule } from "igniteui-angular-charts/ES5/igx-data-chart-core-module";
-// import { IgxExcelModule } from "igniteui-angular-excel/ES5/igx-excel-module";
+import { IgxExcelModule } from "igniteui-angular-excel/ES5/igx-excel-module";
 
-// importing IG sample:
 import { ExcelLibraryWorkingWithTablesComponent } from "./tables.component";
 
 const routes: Routes = [
@@ -25,13 +18,8 @@ const routing: ModuleWithProviders = RouterModule.forChild(routes);
         routing,
         CommonModule,
         FormsModule,
-        // IgxButtonModule,
         IgxCategoryChartModule,
-        IgxExcelXlsxModule
-        // IgxDataChartCoreModule,
-        // IgxDataChartCategoryModule,
-        // IgxExcelModule,
-        // IgxGridModule,
+        IgxExcelModule
     ]
 })
 
