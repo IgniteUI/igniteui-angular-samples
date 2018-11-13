f9ccef9e21c8580fafd0507ea43680dc74a5526c
agoldenbaum <andrew.goldenbaum@gmail.com>
2018-11-13 16:10:53 -0500

    Excel Tables Sample Commit

--- src/app/excel-library/working-with/tables.module.ts
+++ src/app/excel-library/working-with/tables.module.ts
@@ -6,6 +6,7 @@ import { RouterModule, Routes } from "@angular/router";
 // TODO import either CategoryChart, DataChart, or Grid module:
 // import { IgxButtonModule, IgxGridModule } from "igniteui-angular";
 import { IgxCategoryChartModule } from "igniteui-angular-charts/ES5/igx-category-chart-module";
+import { IgxExcelXlsxModule } from "igniteui-angular-excel/ES5/igx-excel-xlsx-module";
 // import { IgxDataChartCategoryModule } from "igniteui-angular-charts/ES5/igx-data-chart-category-module";
 // import { IgxDataChartCoreModule } from "igniteui-angular-charts/ES5/igx-data-chart-core-module";
 // import { IgxExcelModule } from "igniteui-angular-excel/ES5/igx-excel-module";
@@ -25,7 +26,8 @@ const routing: ModuleWithProviders = RouterModule.forChild(routes);
         CommonModule,
         FormsModule,
         // IgxButtonModule,
-        IgxCategoryChartModule
+        IgxCategoryChartModule,
+        IgxExcelXlsxModule
         // IgxDataChartCoreModule,
         // IgxDataChartCategoryModule,
         // IgxExcelModule,
