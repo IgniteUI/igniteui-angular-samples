f404a44961060f4a0f8db9b007027a68aaa4e42d
ViktorSlavov <viktorslavov91@gmail.com>
2018-12-11 17:08:39 +0200

    docs(overlay): change overlay emitter binding

--- src/app/overlay/overlay-positioning-1/overlay-position-sample-1.component.ts
+++ src/app/overlay/overlay-positioning-1/overlay-position-sample-1.component.ts
@@ -1,7 +1,7 @@
 import { Component, ElementRef, Inject, OnDestroy, OnInit, ViewChild } from "@angular/core";
 import {
     AutoPositionStrategy, ConnectedPositioningStrategy, ElasticPositionStrategy, GlobalPositionStrategy,
-    HorizontalAlignment, IgxOverlayService, OverlayEventArgs, VerticalAlignment } from "igniteui-angular";
+    HorizontalAlignment, IgxOverlayService, VerticalAlignment } from "igniteui-angular";
 // tslint:disable:object-literal-sort-keys
 @Component({
     selector: "overlay-sample",
@@ -68,16 +68,13 @@ export class OverlayPositionSample1Component implements OnInit, OnDestroy {
     }
 
     public ngOnInit() {
-        this.overlay.onOpened.subscribe((e: OverlayEventArgs) => {
-            this.overlayDemo.nativeElement.style.overflow = "auto";
-        });
-        this.overlay.onClosed.subscribe((e: OverlayEventArgs) => {
-            this.overlayDemo.nativeElement.style.overflow = "none";
-        });
+        const applyStyle = (overflow) => { this.overlayDemo.nativeElement.style.overflow = overflow; };
+        this.overlay.onOpening.subscribe(() => {applyStyle("auto"); });
+        this.overlay.onClosed.subscribe(() => {applyStyle(""); });
     }
 
     public ngOnDestroy() {
-        this.overlay.onOpened.unsubscribe();
+        this.overlay.onOpening.unsubscribe();
         this.overlay.onClosed.unsubscribe();
     }
 }
