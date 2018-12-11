25057d3097eeda8972172817bf642ba2006d3dee
Stefan Stoyanov <sstoyanov@infragistics.com>
2018-12-11 14:11:30 +0200

    Update samples

--- live-editing/configs/LocalizationConfigGenerator.ts
+++ live-editing/configs/LocalizationConfigGenerator.ts
@@ -3,7 +3,7 @@ import { IgxIconModule, IgxInputGroupModule, IgxTimePickerModule, IgxGridModule}
 import { LocalizationSample1Component } from "../../src/app/localization-samples/localization-sample-1/localization-sample-1.component";
 import { LocalizationSample2Component } from "../../src/app/localization-samples/localization-sample-2/localization-sample-2.component";
 import { LocalizationSample3Component } from "../../src/app/localization-samples/localization-sample-3/localization-sample-3.component";
-import { DATA } from "../../src/app/localization-samples/nwindData";
+import { DATA } from "../../src/app/localization-samples/localization-sample-3/nwindData";
 import { AppModuleConfig } from "./core/AppModuleConfig";
 import { Config } from "./core/Config";
 import { IConfigGenerator } from "./core/IConfigGenerator";
@@ -15,10 +15,11 @@ export class LocalizationConfigGenerator implements IConfigGenerator {
         // localization sample 1
         configs.push(new Config({
             component: LocalizationSample1Component,
+            additionalFiles: ["src/app/localization-samples/localization-sample-1/nwindData.ts"],
             appModuleConfig: new AppModuleConfig({
                 imports: [IgxInputGroupModule, LocalizationSample1Component, IgxGridModule, IgxIconModule, DATA],
                 ngDeclarations: [LocalizationSample1Component],
-                ngImports: [IgxInputGroupModule, IgxGridModule, IgxGridModule.forRoot(), DATA]
+                ngImports: [IgxInputGroupModule, IgxGridModule, IgxGridModule.forRoot()]
             }),
             shortenComponentPathBy: "/localization-samples/"
         }));
@@ -26,10 +27,11 @@ export class LocalizationConfigGenerator implements IConfigGenerator {
         // localization sample 2
         configs.push(new Config({
             component: LocalizationSample2Component,
+            additionalFiles: ["src/app/localization-samples/localization-sample-2/nwindData.ts"],
             appModuleConfig: new AppModuleConfig({
                 imports: [IgxInputGroupModule, LocalizationSample2Component, IgxIconModule, IgxTimePickerModule, IgxGridModule, DATA],
                 ngDeclarations: [LocalizationSample2Component],
-                ngImports: [IgxInputGroupModule, IgxIconModule, IgxTimePickerModule, IgxGridModule.forRoot(), DATA]
+                ngImports: [IgxInputGroupModule, IgxIconModule, IgxTimePickerModule, IgxGridModule.forRoot()]
             }),
             shortenComponentPathBy: "/localization-samples/"
         }));
@@ -37,10 +39,11 @@ export class LocalizationConfigGenerator implements IConfigGenerator {
         // localization sample 3
         configs.push(new Config({
             component: LocalizationSample3Component,
+            additionalFiles: ["src/app/localization-samples/localization-sample-3/nwindData.ts"],
             appModuleConfig: new AppModuleConfig({
                 imports: [IgxInputGroupModule, LocalizationSample3Component, IgxGridModule, IgxIconModule, DATA],
                 ngDeclarations: [LocalizationSample3Component],
-                ngImports: [IgxInputGroupModule, IgxGridModule, IgxGridModule.forRoot(), DATA]
+                ngImports: [IgxInputGroupModule, IgxGridModule, IgxGridModule.forRoot()]
             }),
             shortenComponentPathBy: "/localization-samples/"
         }));
