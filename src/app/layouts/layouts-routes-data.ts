f78a03b7a0a30b059bc50879c9d37ee0cda72359
INFRAGISTICS\tiliev <tiliev@infragistics.com>
2019-01-24 14:26:22 +0200

    Finalized lazy loading.

---
+++ src/app/layouts/layouts-routes-data.ts
@@ -0,0 +1,29 @@
+// tslint:disable:object-literal-sort-keys
+
+export const layoutsRoutesData = {
+    "avatar-sample-1": { displayName: "Avatar with Initials", parentName: "Avatar" },
+    "avatar-sample-2": { displayName: "Avatar with Image", parentName: "Avatar" },
+    "avatar-sample-3": { displayName: "Multiple avatars", parentName: "Avatar" },
+    "card": { displayName: "Card Overview", parentName: "Card" },
+    "carousel-sample-1": { displayName: "Carousel items with text", parentName: "Carousel" },
+    "carousel-sample-2": { displayName: "Slides with ngFor directive", parentName: "Carousel" },
+    "carousel-sample-3": { displayName: "Carousel Navigation", parentName: "Carousel" },
+    "carousel-sample-4": { displayName: "Carousel Router Navigation", parentName: "Carousel" },
+    "carousel": { displayName: "Carousel Image Viewer", parentName: "Carousel" },
+    "expansion-sample-1": { displayName: "Expansion Panel 1", parentName: "Expansion Panel" },
+    "expansion-sample-2": { displayName: "Expansion Panel 2", parentName: "Expansion Panel" },
+    "expansion-sample-3": { displayName: "Expansion Panel 3", parentName: "Expansion Panel" },
+    "expansion-sample-4": { displayName: "Expansion Panel 4", parentName: "Expansion Panel" },
+    "expansion-sample-5": { displayName: "Expansion Panel 5", parentName: "Expansion Panel" },
+    "expansion-sample-6": { displayName: "Expansion Panel 6", parentName: "Expansion Panel" },
+    "expansion-sample-7": { displayName: "Expansion Panel 7", parentName: "Expansion Panel" },
+    "layout": { displayName: "Layout Overview", parentName: "Layout" },
+    "tabbar": { displayName: "Bottom Navigation Overview", parentName: "Bottom Navigation" },
+    "tabbar-sample-1": { displayName: "Bottom Navigation Simple Panels", parentName: "Bottom Navigation" },
+    "tabbar-sample-2": { displayName: "Bottom Navigation Customizing Panels", parentName: "Bottom Navigation" },
+    "tabs-sample-1": { displayName: "Tabs Overview", parentName: "Tabs" },
+    "tabs-sample-2": { displayName: "Tabs Types", parentName: "Tabs" },
+    "tabs-sample-3": { displayName: "Tabs Customizing", parentName: "Tabs" },
+    "card-sample-2": { displayName: "Card with Avatar and Image", parentName: "Card" },
+    "card-sample-3": { displayName: "Card Actions", parentName: "Card" }
+};
