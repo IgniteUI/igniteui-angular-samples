7423ca78287bfaccc63179ff3b9ab77585424e75
Radko Kolev <rkolev@infragistics.com>
2019-02-04 16:56:50 +0200

    samples(tree-grid): adding samples for cvs/excel export #912

---
+++ src/app/services/export-csv/csv-export-tree-grid-sample/csv-export-tree-grid-sample.component.ts
@@ -0,0 +1,44 @@
+import { Component, ViewChild } from "@angular/core";
+
+import { CsvFileTypes,
+         IgxCsvExporterOptions,
+         IgxCsvExporterService,
+         IgxTreeGridComponent } from "igniteui-angular";
+
+import { FOODS_DATA } from "../../../tree-grid/data/foods";
+
+@Component({
+  selector: "app-csv-export-tree-grid-sample",
+  styleUrls: ["./csv-export-tree-grid-sample.component.scss"],
+  templateUrl: "./csv-export-tree-grid-sample.component.html"
+})
+export class TreeGridCsvExportSample1Component {
+
+  @ViewChild("igxTreeGrid1")
+  public igxTreeGrid1: IgxTreeGridComponent;
+
+  public data: any[];
+
+  constructor(private csvExportService: IgxCsvExporterService) {
+    this.data = FOODS_DATA;
+  }
+
+  public exportButtonHandler() {
+    /*
+    The following code demonstrates how to attach event handlers to exporter specific events
+    and also how to customize the column export process.
+    this.csvExportService.onColumnExport.subscribe((args: IColumnExportingEventArgs) => {
+      if (args.header == "Age" && args.columnIndex == 1) {
+        args.cancel = true;
+      }
+    });
+    this.csvExportService.onRowExport.subscribe((args: IRowExportingEventArgs) => {
+    });
+    this.csvExportService.onExportEnded.subscribe((args: ICsvExportEndedEventArgs) => {
+    });
+    */
+    this.csvExportService.export(this.igxTreeGrid1,
+        new IgxCsvExporterOptions("ExportFileFromTreeGrid", CsvFileTypes.CSV));
+  }
+
+}
