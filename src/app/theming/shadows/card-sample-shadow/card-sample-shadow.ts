e3f3db4c8bd3776323ad1b7c4591752cf05b4a3b
MPopov <desig9stein@gmail.com>
2019-02-04 09:25:07 +0200

    Prepare a sample for shadows showcase

---
+++ src/app/theming/shadows/card-sample-shadow/card-sample-shadow.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from "@angular/core";
+
+@Component({
+    selector: "app-card-sample-shadow",
+    styleUrls: ["./card-sample-shadow.scss"],
+    templateUrl: "./card-sample-shadow.html"
+})
+export class CardSampleShadow implements OnInit {
+
+    constructor() {
+    }
+
+    public ngOnInit() {
+    }
+
+}
