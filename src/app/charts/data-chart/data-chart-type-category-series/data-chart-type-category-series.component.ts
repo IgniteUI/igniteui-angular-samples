2bba21c327924ce644bcdecc4df72d1bcc411cde
mddifilippo89 <mddifilippo89@comcast.net>
2019-02-14 13:36:02 -0500

    mdd-260575
    Category-Chart Sampe for DataChart

--- src/app/charts/data-chart/data-chart-type-category-series/data-chart-type-category-series.component.ts
+++ src/app/charts/data-chart/data-chart-type-category-series/data-chart-type-category-series.component.ts
@@ -11,5 +11,12 @@ export class DataChartTypeCategorySeriesComponent implements OnInit {
 
   public ngOnInit() {
   }
-
+  public data = [
+    { label: "Africa", value1: 200, value2: 150 },
+{ label: "Brazil", value1: 500, value2: 500 },
+{ label: "Canada", value1: 300, value2: 300 },
+{ label: "Denmark", value1: 600, value2: 600 },
+{ label: "England", value1: 1000, value2: 1000 },
+{ label: "France", value1: 200, value2: 50 }
+  ]
 }
