7c294a8fd329f54d77cb05e6d1d9add039be0029
Boris <jackofdiamond596@gmail.com>
2019-02-20 10:00:40 +0200

    chore(select): Add dropdown module to imports.

--- live-editing/configs/SelectConfigGenerator.ts
+++ live-editing/configs/SelectConfigGenerator.ts
@@ -1,4 +1,4 @@
-import { IgxButtonModule, IgxInputGroupModule, IgxSelectModule } from "igniteui-angular";
+import { IgxButtonModule, IgxDropDownModule, IgxInputGroupModule, IgxSelectModule } from "igniteui-angular";
 import { SelectSample1Component } from "../../src/app/data-entries/select/select-sample-1/select-sample-1.component";
 import { SelectSample2Component } from "../../src/app/data-entries/select/select-sample-2/select-sample-2.component";
 import { SelectSample3Component } from "../../src/app/data-entries/select/select-sample-3/select-sample-3.component";
@@ -13,9 +13,9 @@ export class SelectConfigGenerator implements IConfigGenerator {
 
         configs.push(new Config({
             appModuleConfig: new AppModuleConfig({
-                imports: [IgxSelectModule, SelectSample1Component, IgxInputGroupModule],
+                imports: [IgxSelectModule, SelectSample1Component, IgxInputGroupModule, IgxDropDownModule],
                 ngDeclarations: [SelectSample1Component],
-                ngImports: [IgxSelectModule, IgxInputGroupModule]
+                ngImports: [IgxSelectModule, IgxInputGroupModule, IgxDropDownModule]
             }),
             component: SelectSample1Component,
             shortenComponentPathBy: "/data-entries/select/"
@@ -23,9 +23,9 @@ export class SelectConfigGenerator implements IConfigGenerator {
 
         configs.push(new Config({
             appModuleConfig: new AppModuleConfig({
-                imports: [IgxSelectModule, SelectSample2Component, IgxInputGroupModule],
+                imports: [IgxSelectModule, SelectSample2Component, IgxInputGroupModule, IgxDropDownModule],
                 ngDeclarations: [SelectSample2Component],
-                ngImports: [IgxSelectModule, IgxInputGroupModule]
+                ngImports: [IgxSelectModule, IgxInputGroupModule, IgxDropDownModule]
             }),
             component: SelectSample2Component,
             shortenComponentPathBy: "/data-entries/select/"
@@ -33,9 +33,9 @@ export class SelectConfigGenerator implements IConfigGenerator {
 
         configs.push(new Config({
             appModuleConfig: new AppModuleConfig({
-                imports: [IgxSelectModule, SelectSample3Component, IgxButtonModule],
+                imports: [IgxSelectModule, SelectSample3Component, IgxButtonModule, IgxDropDownModule],
                 ngDeclarations: [SelectSample3Component],
-                ngImports: [IgxSelectModule, IgxButtonModule]
+                ngImports: [IgxSelectModule, IgxButtonModule, IgxDropDownModule]
             }),
             component: SelectSample3Component,
             shortenComponentPathBy: "/data-entries/select/"
@@ -43,9 +43,9 @@ export class SelectConfigGenerator implements IConfigGenerator {
 
         configs.push(new Config({
             appModuleConfig: new AppModuleConfig({
-                imports: [IgxSelectModule, SelectSample4Component, IgxInputGroupModule],
+                imports: [IgxSelectModule, SelectSample4Component, IgxInputGroupModule, IgxDropDownModule],
                 ngDeclarations: [SelectSample4Component],
-                ngImports: [IgxSelectModule, IgxInputGroupModule]
+                ngImports: [IgxSelectModule, IgxInputGroupModule, IgxDropDownModule]
             }),
             component: SelectSample4Component,
             shortenComponentPathBy: "/data-entries/select/"
