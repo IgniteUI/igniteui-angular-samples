37c46c40aeb1c115ef87058b2191aa9f192ef0d2
Svetla Boykova <sboykova@infragistics.com>
2019-02-27 18:33:47 +0200

    docs(date-picker): Tslint warning fixed #1000

--- live-editing/configs/DatePickerConfigGenerator.ts
+++ live-editing/configs/DatePickerConfigGenerator.ts
@@ -1,13 +1,13 @@
 /* tslint:disable:object-literal-sort-keys */
 // tslint:disable:max-line-length
 import { IgxDatePickerModule, IgxIconModule, IgxInputGroupModule } from "igniteui-angular";
+import { DatepickerDropdownComponent } from "../../src/app/scheduling/datepicker/datepicker-dropdown/datepicker-dropdown.component";
 import { DatepickerSample1Component } from "../../src/app/scheduling/datepicker/datepicker-sample-1/datepicker-sample-1.component";
 import { DatepickerSample2Component } from "../../src/app/scheduling/datepicker/datepicker-sample-2/datepicker-sample-2.component";
 import { DatepickerSample3Component } from "../../src/app/scheduling/datepicker/datepicker-sample-3/datepicker-sample-3.component";
 import { DatepickerSample4Component } from "../../src/app/scheduling/datepicker/datepicker-sample-4/datepicker-sample-4.component";
 import { DatepickerSample5Component } from "../../src/app/scheduling/datepicker/datepicker-sample-5/datepicker-sample-5.component";
 import { DatepickerSample6Component } from "../../src/app/scheduling/datepicker/datepicker-sample-6/datepicker-sample-6.component";
-import { DatepickerDropdownComponent } from "../../src/app/scheduling/datepicker/datepicker-dropdown/datepicker-dropdown.component";
 import { AppModuleConfig } from "./core/AppModuleConfig";
 import { Config } from "./core/Config";
 import { IConfigGenerator } from "./core/IConfigGenerator";
