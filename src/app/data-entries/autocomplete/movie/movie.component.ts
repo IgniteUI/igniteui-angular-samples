c87734734671171461a6d0199805d9fceb0d935c
Svetla Boykova <sboykova@infragistics.com>
2019-03-04 18:14:33 +0200

    fix(pickers): Fixes after interaction mode renaming #1020

--- src/app/data-entries/autocomplete/movie/movie.component.ts
+++ src/app/data-entries/autocomplete/movie/movie.component.ts
@@ -1,7 +1,10 @@
 import { Component, Pipe, PipeTransform, ViewChild } from "@angular/core";
 import { FormBuilder, FormControl, Validators } from "@angular/forms";
-import { DatePickerInteractionMode, DateRangeType,
-        IgxDialogComponent, TimePickerInteractionMode } from "igniteui-angular";
+import {
+    DateRangeType,
+    IgxDialogComponent,
+    InteractionMode
+} from "igniteui-angular";
 @Component({
     selector: "movie-availability",
     styleUrls: ["./movie.component.scss"],
@@ -10,40 +13,46 @@ import { DatePickerInteractionMode, DateRangeType,
 export class MovieComponent {
     @ViewChild("alert", { read: IgxDialogComponent }) public alert: IgxDialogComponent;
     public user;
-    public dateMode = DatePickerInteractionMode.EDITABLE;
-    public timeMode = TimePickerInteractionMode.dropdown;
+    public dateMode = InteractionMode.DROPDOWN;
+    public timeMode = InteractionMode.DROPDOWN;
 
     public towns = [
-        { name: "New York", cinemas: ["Regal Cinemas", "Village East Cinema", "Roxy Cinema", "The Paris Theatre" ]},
+        { name: "New York", cinemas: ["Regal Cinemas", "Village East Cinema", "Roxy Cinema", "The Paris Theatre"] },
         // tslint:disable-next-line:object-literal-sort-keys
-        { name: "Los Angeles", cinemas: ["Arc Light", "Pacific Cinerama Dome",
-            "New Beverly Cinema", "Downtown Independent"]},
+        {
+            name: "Los Angeles", cinemas: ["Arc Light", "Pacific Cinerama Dome",
+                "New Beverly Cinema", "Downtown Independent"]
+        },
         // tslint:disable-next-line:object-literal-sort-keys
-        { name: "Seattle", cinemas: ["Central Cinema", "Grand Illusion Cinema",
-            "Ark Lodge Cinemas", "Skyway Outdoor Cinema"]}
+        {
+            name: "Seattle", cinemas: ["Central Cinema", "Grand Illusion Cinema",
+                "Ark Lodge Cinemas", "Skyway Outdoor Cinema"]
+        }
     ];
 
     public genres = [
-        { type: "Action" , movies: ["The Matrix", "Kill Bill: Vol.1", "The Dark Knight Rises"]},
-        { type: "Adventure" , movies: ["Interstellar", "Inglourious Basterds", "Inception"]},
+        { type: "Action", movies: ["The Matrix", "Kill Bill: Vol.1", "The Dark Knight Rises"] },
+        { type: "Adventure", movies: ["Interstellar", "Inglourious Basterds", "Inception"] },
         // tslint:disable-next-line:object-literal-sort-keys
-        { type: "Comedy" , movies: ["Wild Tales", "In Bruges", "Three Billboards Outside Ebbing, Missouri",
-            "Untouchable", "3 idiots"]},
-        { type: "Crime" , movies: ["Training Day", "Heat", "American Gangster"]},
-        { type: "Drama" , movies: ["Fight Club", "A Beautiful Mind", "Good Will Hunting", "City of God"]},
-        { type: "Biography" , movies: ["Amadeus", "Bohemian Rhapsody"]},
-        { type: "Mystery" , movies: ["The Prestige", "Memento", "Cloud Atlas"]},
-        { type: "Musical" , movies: ["All That Jazz"]},
-        { type: "Romance" , movies: ["Love Actually", "In The Mood for Love"]},
-        { type: "Sci-Fi" , movies: ["The Fifth Element"]},
-        { type: "Thriller" , movies: ["The Usual Suspects"]},
-        { type: "Western" , movies: ["Django Unchained"]}];
+        {
+            type: "Comedy", movies: ["Wild Tales", "In Bruges", "Three Billboards Outside Ebbing, Missouri",
+                "Untouchable", "3 idiots"]
+        },
+        { type: "Crime", movies: ["Training Day", "Heat", "American Gangster"] },
+        { type: "Drama", movies: ["Fight Club", "A Beautiful Mind", "Good Will Hunting", "City of God"] },
+        { type: "Biography", movies: ["Amadeus", "Bohemian Rhapsody"] },
+        { type: "Mystery", movies: ["The Prestige", "Memento", "Cloud Atlas"] },
+        { type: "Musical", movies: ["All That Jazz"] },
+        { type: "Romance", movies: ["Love Actually", "In The Mood for Love"] },
+        { type: "Sci-Fi", movies: ["The Fifth Element"] },
+        { type: "Thriller", movies: ["The Usual Suspects"] },
+        { type: "Western", movies: ["Django Unchained"] }];
 
     public disabledDates = [{
-        dateRange: [ new Date() ],
+        dateRange: [new Date()],
         type: DateRangeType.Before
     }, {
-        dateRange: [ new Date(new Date().getTime() + 14 * 24 * 60 * 60 * 1000) ],
+        dateRange: [new Date(new Date().getTime() + 14 * 24 * 60 * 60 * 1000)],
         type: DateRangeType.After
     }];
 
