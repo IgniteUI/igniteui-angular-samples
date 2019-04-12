7ff2e5f87529c8e4bc2323973e5ca0c1ba5e07e3
MPopov <desig9stein@gmail.com>
2019-04-12 15:08:36 +0300

    Create samples for divider component

---
+++ src/app/layouts/divider/dashed/divider-dashed.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from "@angular/core";
+
+@Component({
+    selector: "app-divider-dashed",
+    styleUrls: ["./divider-dashed.component.scss"],
+    templateUrl: "./divider-dashed.component.html"
+})
+export class DividerDashedComponent implements OnInit {
+
+    constructor() {
+    }
+
+    public ngOnInit() {
+    }
+
+}
