90ba7f2c3a905d220a80870c94bbcd027005ed6c
AMarinov <amarinov@infragistics.com>
2019-04-30 14:31:27 +0300

    Fixing ts lint errors

--- src/app/grid/grid-row-drag/data.ts
+++ src/app/grid/grid-row-drag/data.ts
@@ -1,29 +1,43 @@
 export const DATA: any[] = [
     {
-        name: "Saturn",
-        moonsCount: 62,
         description: "",
         moons: [
-            { name: "Mimas", diameter: "396(12% Moon)", orbitalRadius: "185,539((48% Moon))", orbitalPeriod: "0.9(3% Moon)",  mass: "4×1019(0.05% Moon)" },
-            { name: "Enceladus", diameter: "504(14% Moon)", orbitalRadius: "237,948(62% Moon)", orbitalPeriod: "1.4(5% Moon)",  mass: "1.1×1020(0.2% Moon)" },
-            { name: "Tethys", diameter: "1,062(30% Moon)", orbitalRadius: "294,619(77% Moon)", orbitalPeriod: "1.9(7% Moon)",  mass: "6.2×1020(0.8% Moon)" },
-            { name: "Dione", diameter: "1,123(32% Moon)", orbitalRadius: "377,396(98% Moon)", orbitalPeriod: "2.7(10% Moon)",  mass: "1.1×1021(1.5% Moon)" },
-            { name: "Rhea", diameter: "1,527(44% Moon)", orbitalRadius: "527,108(137% Moon)", orbitalPeriod: "4.5(20% Moon)",  mass: "2.3×1021(3% Moon)" },
-            { name: "Titan", diameter: "5,149(148% Moon)(75% Mars)", orbitalRadius: "1,221,870(318% Moon)", orbitalPeriod: "16(60% Moon)",  mass: "1.35×1023(180% Moon)" },
-            { name: "Iapetus", diameter: "1,470(42% Moon)", orbitalRadius: "3,560,820(926% Moon)", orbitalPeriod: "79(290% Moon)",  mass: "1.8×1021(2.5% Moon)" }
-        ]
+            { diameter: "396(12% Moon)", mass: "4×1019(0.05% Moon)", name: "Mimas",
+              orbitalPeriod: "0.9(3% Moon)", orbitalRadius: "185,539((48% Moon))" },
+            { diameter: "504(14% Moon)", mass: "1.1×1020(0.2% Moon)", name: "Enceladus",
+              orbitalPeriod: "1.4(5% Moon)", orbitalRadius: "237,948(62% Moon)" },
+            { diameter: "1,062(30% Moon)", mass: "6.2×1020(0.8% Moon)", name: "Tethys",
+              orbitalPeriod: "1.9(7% Moon)", orbitalRadius: "294,619(77% Moon)" },
+            { diameter: "1,123(32% Moon)", mass: "1.1×1021(1.5% Moon)", name: "Dione",
+              orbitalPeriod: "2.7(10% Moon)", orbitalRadius: "377,396(98% Moon)" },
+            { diameter: "1,527(44% Moon)", mass: "2.3×1021(3% Moon)", name: "Rhea",
+              orbitalPeriod: "4.5(20% Moon)", orbitalRadius: "527,108(137% Moon)" },
+            { diameter: "5,149(148% Moon)(75% Mars)", mass: "1.35×1023(180% Moon)", name: "Titan",
+              orbitalPeriod: "16(60% Moon)", orbitalRadius: "1,221,870(318% Moon)" },
+            { diameter: "1,470(42% Moon)", mass: "1.8×1021(2.5% Moon)", name: "Iapetus",
+              orbitalPeriod: "79(290% Moon)", orbitalRadius: "3,560,820(926% Moon)" }
+        ],
+        moonsCount: 62,
+        name: "Saturn"
     },
     {
-        name: "Jupiter",
-        moonsCount: 79,
         moons: [
-            { name: "Mimas", diameter: "396(12% Moon)", orbitalRadius: "185,539((48% Moon))", orbitalPeriod: "0.9(3% Moon)",  mass: "4×1019(0.05% Moon)" },
-            { name: "Enceladus", diameter: "504(14% Moon)", orbitalRadius: "237,948(62% Moon)", orbitalPeriod: "1.4(5% Moon)",  mass: "1.1×1020(0.2% Moon)" },
-            { name: "Tethys", diameter: "1,062(30% Moon)", orbitalRadius: "294,619(77% Moon)", orbitalPeriod: "1.9(7% Moon)",  mass: "6.2×1020(0.8% Moon)" },
-            { name: "Dione", diameter: "1,123(32% Moon)", orbitalRadius: "377,396(98% Moon)", orbitalPeriod: "2.7(10% Moon)",  mass: "1.1×1021(1.5% Moon)" },
-            { name: "Rhea", diameter: "1,527(44% Moon)", orbitalRadius: "527,108(137% Moon)", orbitalPeriod: "4.5(20% Moon)",  mass: "2.3×1021(3% Moon)" },
-            { name: "Titan", diameter: "5,149(148% Moon)(75% Mars)", orbitalRadius: "1,221,870(318% Moon)", orbitalPeriod: "16(60% Moon)",  mass: "1.35×1023(180% Moon)" },
-            { name: "Iapetus", diameter: "1,470(42% Moon)", orbitalRadius: "3,560,820(926% Moon)", orbitalPeriod: "79(290% Moon)",  mass: "1.8×1021(2.5% Moon)" }
-        ]
+            { diameter: "396(12% Moon)", mass: "4×1019(0.05% Moon)", name: "Mimas",
+              orbitalPeriod: "0.9(3% Moon)", orbitalRadius: "185,539((48% Moon))" },
+            { diameter: "504(14% Moon)", mass: "1.1×1020(0.2% Moon)", name: "Enceladus",
+              orbitalPeriod: "1.4(5% Moon)", orbitalRadius: "237,948(62% Moon)" },
+            { diameter: "1,062(30% Moon)", mass: "6.2×1020(0.8% Moon)", name: "Tethys",
+              orbitalPeriod: "1.9(7% Moon)", orbitalRadius: "294,619(77% Moon)" },
+            { diameter: "1,123(32% Moon)", mass: "1.1×1021(1.5% Moon)", name: "Dione",
+              orbitalPeriod: "2.7(10% Moon)", orbitalRadius: "377,396(98% Moon)" },
+            { diameter: "1,527(44% Moon)", mass: "2.3×1021(3% Moon)", name: "Rhea",
+              orbitalPeriod: "4.5(20% Moon)", orbitalRadius: "527,108(137% Moon)" },
+            { diameter: "5,149(148% Moon)(75% Mars)", mass: "1.35×1023(180% Moon)", name: "Titan",
+              orbitalPeriod: "16(60% Moon)", orbitalRadius: "1,221,870(318% Moon)" },
+            { diameter: "1,470(42% Moon)", mass: "1.8×1021(2.5% Moon)", name: "Iapetus",
+              orbitalPeriod: "79(290% Moon)", orbitalRadius: "3,560,820(926% Moon)" }
+        ],
+        moonsCount: 79,
+        name: "Jupiter"
     }
 ];
