9157ad4226e1196c2d38e69fbec727d43af74ae0
Diyan Dimitrov <ddimitrov@infragistics.com>
2019-06-04 16:55:53 +0300

    samples(treeGrid): add tree grid remote paging sample #1177

--- live-editing/configs/TreeGridConfigGenerator.ts
+++ live-editing/configs/TreeGridConfigGenerator.ts
@@ -50,6 +50,7 @@ import { TreeGridMultiColumnHeadersSampleComponent } from "../../src/app/tree-gr
 import { TreeGridPagingSampleComponent } from "../../src/app/tree-grid/tree-grid-paging-sample/tree-grid-paging-sample.component";
 import { TreeGridPrimaryforeignkeySampleComponent } from "../../src/app/tree-grid/tree-grid-primaryforeignkey-sample/tree-grid-primaryforeignkey-sample.component";
 import { TreeGridRemoteFilteringSampleComponent } from "../../src/app/tree-grid/tree-grid-remote-filtering-sample/tree-grid-remote-filtering-sample.component";
+import { TreeGridRemotePagingSampleComponent } from "../../src/app/tree-grid/tree-grid-remote-paging-sample/tree-grid-remote-paging-sample.component";
 import { TreeGridRowDragBase } from "../../src/app/tree-grid/tree-grid-row-drag-base/tree-grid-row-drag-base.component";
 import { TreeGridRowDrag } from "../../src/app/tree-grid/tree-grid-row-drag/tree-grid-row-drag.component";
 import { TreeGridRowEditSampleComponent } from "../../src/app/tree-grid/tree-grid-row-edit/tree-grid-row-editing-sample.component";
@@ -510,6 +511,18 @@ export class TreeGridConfigGenerator implements IConfigGenerator {
                 ngImports: [IgxTreeGridModule]
             }),
             component: TreeGridKBNavigationComponent,
+            shortenComponentPathBy: "/tree-grid/"
+        }));
+
+        // Tree Grid Remote Paging Sample
+        configs.push(new Config({
+            additionalFiles: ["/src/app/tree-grid/tree-grid-remote-paging-sample/remotePagingService.ts"],
+            appModuleConfig: new AppModuleConfig({
+                imports: [IgxTreeGridModule, TreeGridRemotePagingSampleComponent],
+                ngDeclarations: [TreeGridRemotePagingSampleComponent],
+                ngImports: [IgxTreeGridModule]
+            }),
+            component: TreeGridRemotePagingSampleComponent,
             shortenComponentPathBy: "/tree-grid/"
         }));
 
---
+++ src/app/tree-grid/tree-grid-remote-paging-sample/tree-grid-remote-paging-sample.component.ts
@@ -0,0 +1,123 @@
+import { formatNumber } from "@angular/common";
+import { AfterViewInit, Component, OnDestroy, OnInit, TemplateRef, ViewChild, ViewEncapsulation } from "@angular/core";
+import { IgxTreeGridComponent } from "igniteui-angular";
+import { Observable } from "rxjs";
+import { RemotePagingService } from "./remotePagingService";
+
+@Component({
+    encapsulation: ViewEncapsulation.None,
+    providers: [RemotePagingService],
+    selector: "app-tree-grid-remote-paging-grid-sample",
+    styleUrls: ["./tree-grid-remote-paging-sample.component.scss"],
+    templateUrl: "./tree-grid-remote-paging-sample.component.html"
+})
+export class TreeGridRemotePagingSampleComponent implements OnInit, AfterViewInit, OnDestroy {
+
+    public page = 0;
+    public lastPage = false;
+    public firstPage = true;
+    public totalPages: number = 1;
+    public totalCount = 0;
+    public maxPerPage = Number.MAX_SAFE_INTEGER;
+
+    @ViewChild("grid1")
+    public grid1: IgxTreeGridComponent;
+    public data: Observable<any[]>;
+
+    public get perPage(): number {
+        return this._perPage;
+    }
+
+    public set perPage(val: number) {
+        this._perPage = val;
+        this.paginate(0, true);
+    }
+
+    private _perPage = 12;
+    private _dataLengthSubscriber;
+
+    constructor(
+        private remoteService: RemotePagingService) {
+    }
+
+    public ngOnInit() {
+        this.data = this.remoteService.remoteData.asObservable();
+
+        this._dataLengthSubscriber = this.remoteService.dataLength.subscribe((data) => {
+            this.totalCount = data;
+            this.totalPages = Math.ceil(data / this.perPage);
+            this.buttonDeselection(this.page, this.totalPages);
+            this.grid1.isLoading = false;
+        });
+    }
+
+    public ngOnDestroy() {
+        if (this._dataLengthSubscriber) {
+            this._dataLengthSubscriber.unsubscribe();
+        }
+    }
+
+    public ngAfterViewInit() {
+        this.grid1.isLoading = true;
+
+        this.remoteService.getData(0, this.perPage);
+        this.remoteService.getDataLength();
+    }
+
+    public nextPage() {
+        this.firstPage = false;
+        this.page++;
+        const skip = this.page * this.perPage;
+        const top = this.perPage;
+        this.remoteService.getData(skip, top);
+        if (this.page + 1 >= this.totalPages) {
+            this.lastPage = true;
+        }
+    }
+
+    public previousPage() {
+        this.lastPage = false;
+        this.page--;
+        const skip = this.page * this.perPage;
+        const top = this.perPage;
+        this.remoteService.getData(skip, top);
+        if (this.page <= 0) {
+            this.firstPage = true;
+        }
+    }
+
+    public paginate(page: number, recalc: true) {
+        this.page = page;
+        const skip = this.page * this.perPage;
+        const top = this.perPage;
+        if (recalc) {
+            this.totalPages = Math.ceil(this.totalCount / this.perPage);
+        }
+        this.remoteService.getData(skip, top);
+        this.buttonDeselection(this.page, this.totalPages);
+    }
+
+    public buttonDeselection(page: number, totalPages: number) {
+        if (totalPages === 1) {
+            this.lastPage = true;
+            this.firstPage = true;
+        } else if (page + 1 >= totalPages) {
+            this.lastPage = true;
+            this.firstPage = false;
+        } else if (page !== 0 && page !== totalPages) {
+            this.lastPage = false;
+            this.firstPage = false;
+        } else {
+            this.lastPage = false;
+            this.firstPage = true;
+        }
+    }
+
+    public parseToInt(val) {
+        return parseInt(val, 10);
+    }
+
+    public formatSize(value: number) {
+        return formatNumber(value, "en") + " KB";
+    }
+}
