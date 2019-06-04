aa5c42e810c2042d04cd03c797dedcdc1351da05
agoldenbaum <agoldenbaum@infragistics.com>
2019-06-04 14:57:50 -0400

    Create sparkline sample "shells"

---
+++ src/app/charts/sparkline/sparkline-display-types/sparkline-display-types.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-sparkline-display-types",
+  styleUrls: ["./sparkline-display-types.component.scss"],
+  templateUrl: "./sparkline-display-types.component.html"
+})
+export class SparklineDisplayTypesComponent implements OnInit {
+
+  constructor() { }
+
+  public ngOnInit() {
+  }
+
+}
---
+++ src/app/charts/sparkline/sparkline-markers/sparkline-markers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-sparkline-markers",
+  styleUrls: ["./sparkline-markers.component.scss"],
+  templateUrl: "./sparkline-markers.component.html"
+})
+export class SparklineMarkersComponent implements OnInit {
+
+  constructor() { }
+
+  public ngOnInit() {
+  }
+
+}
