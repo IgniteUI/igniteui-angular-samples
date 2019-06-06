c502752cb978e1c417ab67aae0240137ef5dadc3
Boris <jackofdiamond596@gmail.com>
2019-06-06 16:05:38 +0300

    chore(grid-row-reorder): fix lint

--- src/app/grid/grid-row-reorder-sample/grid-row-reorder.ts
+++ src/app/grid/grid-row-reorder-sample/grid-row-reorder.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from "@angular/core";
-import { IgxGridComponent } from 'igniteui-angular';
+import { IgxGridComponent } from "igniteui-angular";
 import { DATA } from "../../data/customers";
 
 @Component({
