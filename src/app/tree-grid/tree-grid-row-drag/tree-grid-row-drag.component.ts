c40a0161b2210537a06728e976163553207f8715
ViktorSlavov <viktorslavov91@gmail.com>
2019-06-10 14:51:16 +0300

    Revert "feat(row-drag): add row selection, row reordering to tree-grid-row-drag sample"
    This reverts commit 1886c04ab6ab9aab41410a621646468ad7eea406.

--- src/app/tree-grid/tree-grid-row-drag/tree-grid-row-drag.component.ts
+++ src/app/tree-grid/tree-grid-row-drag/tree-grid-row-drag.component.ts
@@ -7,11 +7,6 @@ enum DragIcon {
     ALLOW = "remove"
 }
 
-interface ICursorPosition {
-    x: number;
-    y: number;
-}
-
 @Component({
     selector: "tree-grid-row-drag",
     styleUrls: ["tree-grid-row-drag.component.scss"],
@@ -26,65 +21,21 @@ export class TreeGridRowDrag {
         this.localData = FULL_EMPLOYEE_DATA();
     }
 
-    public rowDragStart(args: any): void {
-        const targetRow: IgxTreeGridRowComponent = args.dragData;
-        if (targetRow.expanded) {
-            const grid = targetRow.grid;
-            grid.expansionStates = grid.expansionStates.set(targetRow.rowID, false);
-        }
-    }
-
-    public dropInGrid(args: IgxDropEventArgs): void {
-        args.cancel = true;
-        const draggedRow: IgxTreeGridRowComponent = args.dragData;
-        const event = args.originalEvent;
-        const cursorPosition: ICursorPosition = { x: event.pageX, y: event.pageY };
-        this.moveRow(draggedRow, cursorPosition);
-    }
-
-    public dropInArea(args: IgxDropEventArgs): void {
+    public onDropAllowed(args: IgxDropEventArgs) {
         args.cancel = true;
         const draggedRow: IgxTreeGridRowComponent = args.dragData;
         draggedRow.delete();
     }
 
-    public onEnterAllowed(args: any): void {
+    public onEnterAllowed(args) {
         this.changeGhostIcon(args.drag.dragGhost, DragIcon.ALLOW);
     }
 
-    public onLeaveAllowed(args: any): void {
+    public onLeaveAllowed(args) {
         this.changeGhostIcon(args.drag.dragGhost, DragIcon.DEFAULT);
     }
 
-    private moveRow(draggedRow: IgxTreeGridRowComponent, cursorPosition: ICursorPosition): void {
-        const parent: IgxTreeGridComponent = draggedRow.grid;
-        const rowIndex: number = this.getTargetRowIndex(parent.rowList.toArray(), cursorPosition);
-        draggedRow.delete();
-        parent.data.splice(rowIndex, 0, draggedRow.rowData);
-        if (draggedRow.isSelected) {
-            parent.selectRows([parent.rowList.toArray()
-                .find((r) => r.rowData.ID === draggedRow.rowData.ID).rowID], false);
-        }
-    }
-
-    private getTargetRowIndex(rowListArr: IgxTreeGridRowComponent[], cursorPosition: ICursorPosition): number {
-        const targetElem: IgxTreeGridRowComponent = this.catchCursorPosOnElem(rowListArr, cursorPosition);
-        console.log(targetElem);
-        return rowListArr.indexOf(rowListArr.find((r) => r.rowData.ID === targetElem.rowData.ID));
-    }
-
-    private catchCursorPosOnElem(rowListArr: IgxTreeGridRowComponent[], cursorPosition: ICursorPosition)
-        : IgxTreeGridRowComponent {
-        for (const row of rowListArr) {
-            const rowRect = row.nativeElement.getBoundingClientRect();
-            if (cursorPosition.y > rowRect.top && cursorPosition.y < rowRect.bottom &&
-                cursorPosition.x > rowRect.left && cursorPosition.x < rowRect.right) {
-                return row;
-            }
-        }
-    }
-
-    private changeGhostIcon(ghost, icon: string): void {
+    private changeGhostIcon(ghost, icon: string) {
         if (ghost) {
             const currentIcon = ghost.querySelector(".igx-grid__drag-indicator > igx-icon");
             if (currentIcon) {
