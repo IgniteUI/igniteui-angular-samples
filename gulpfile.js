026109d7fa0c250decb9d447ca9833fd7d214e80
NikolayAlipiev <nikolay.alipiev@gmail.com>
2019-06-26 15:44:46 +0300

    chore(*): create gul task to overwrite failing package.json files

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 const gulp = require("gulp");
+const fs = require("fs");
 const tsNode = require('ts-node').register({
     transpileOnly: true,
     ignore: [/\/node_modules\/(?!igniteui-angular)/],
@@ -18,6 +19,15 @@ function requireFile(path) {
 
 gulp.task("generate-live-editing", (done) => {
     requireFile("./live-editing/LiveEditingManager.ts");
+    done();
+});
+
+gulp.task("overwrite-package-json", (done) => {
+    const packagesPaths = ["./node_modules/igniteui-angular-charts/package.json", "./node_modules/igniteui-angular-core/package.json", "./node_modules/igniteui-angular-excel/package.json", "./node_modules/igniteui-angular-gauges/package.json", "./node_modules/igniteui-angular-spreadsheet/package.json"];
+    packagesPaths.forEach((packagePath) => {
+        const package = require(packagePath);
+        fs.writeFileSync(packagePath, JSON.stringify(package));
+    });
     done();
 });
 
