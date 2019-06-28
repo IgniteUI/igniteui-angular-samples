275b200c66692c0b263da08b9f41d328dc5daf2f
HristoP96 <HPopov@infragistics.com>
2019-06-28 16:16:43 +0300

    Add polyfill template for IE

--- live-editing/generators/SharedAssetsGenerator.ts
+++ live-editing/generators/SharedAssetsGenerator.ts
@@ -17,7 +17,7 @@ const ANGULAR_JSON_TEMPLATE_CSS_SUPPORT_PATH =
 const MAIN_TS_FILE_PATH = path.join(__dirname, "../templates/main.ts.template");
 const APP_COMPONENT_SCSS_PATH = path.join(__dirname, "../../src/app/app.component.scss");
 const APP_COMPONENT_TS_PATH = path.join(__dirname, "../../src/app/app.component.ts");
-
+const IE_POLYFILLS_TS__PATH = path.join(__dirname, "../templates/polyfills.ts.template");
 export class SharedAssetsGenerator extends Generator {
     private _showLogs: boolean;
 
@@ -69,15 +69,21 @@ export class SharedAssetsGenerator extends Generator {
         let args = new SharedAssetsGeneratorArgs("styles.css", styles,
             ANGULAR_JSON_TEMPLATE_CSS_SUPPORT_PATH, "app.component.css",
             appComponentStylesFileContent, appComponentTsFileContnet);
-        this._generateSharedAssets(args);
+        this._generateSharedAssets(args, true);
     }
 
-    private _generateSharedAssets(args: SharedAssetsGeneratorArgs) {
+    private _generateSharedAssets(args: SharedAssetsGeneratorArgs, isIE?: boolean) {
         let indexFile = fs.readFileSync(INDEX_FILE_PATH, "utf8");
-        let polyfillsFile = fs.readFileSync(POLYPFILLS_FILE_PATH, "utf8");
         let angularJsonFile = fs.readFileSync(args.angularJsonFilePath, "utf8");
         let mainTsFile = fs.readFileSync(MAIN_TS_FILE_PATH, "utf8");
         let files = new Array<LiveEditingFile>();
+        let polyfillsFile;
+
+        if (isIE) {
+            polyfillsFile = fs.readFileSync(IE_POLYFILLS_TS__PATH, "utf8");
+        } else {
+            polyfillsFile = fs.readFileSync(POLYPFILLS_FILE_PATH, "utf8");
+        }
 
         files.push(new LiveEditingFile(SAMPLE_SRC_FOLDER + "index.html", indexFile));
         files.push(new LiveEditingFile(SAMPLE_SRC_FOLDER + "polyfills.ts", polyfillsFile));
