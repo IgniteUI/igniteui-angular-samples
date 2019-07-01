46c89b58c72eedb9407c603e32517634e69925a9
Radko Kolev <radko.kolev@gmail.com>
2019-07-01 15:03:01 +0300

    feat(bottomNav): adding bottomNav routing sample

---
+++ src/app/layouts/tabbar/tabbar-sample-3/tabbar-sample-3.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+selector: "app-tabbar-sample-3",
+styleUrls: ["tabbar-sample-3.component.scss"],
+templateUrl: "tabbar-sample-3.component.html"
+})
+export class TabbarSample3Component {
+
+    constructor() {
+    }
+
+}
