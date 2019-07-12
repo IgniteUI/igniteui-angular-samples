414ad406d8b84946506cac53b725cc868ed15936
PavlovVasil <vasil@vasilpavlov.com>
2019-07-12 14:56:41 +0300

    Add overlay to dialog

--- src/app/interactions/drag-drop/dialog-sample/drag-dialog-sample.component.ts
+++ src/app/interactions/drag-drop/dialog-sample/drag-dialog-sample.component.ts
@@ -1,12 +1,19 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { IgxToggleDirective, IgxDragDirective, OverlaySettings, GlobalPositionStrategy, NoOpScrollStrategy } from 'igniteui-angular';
+import { Component, ViewChild, ElementRef, Inject } from '@angular/core';
+import { 
+    IgxToggleDirective, 
+    IgxDragDirective, 
+    OverlaySettings, 
+    GlobalPositionStrategy, 
+    NoOpScrollStrategy, 
+    IgxOverlayService, 
+    ConnectedPositioningStrategy } from 'igniteui-angular';
 
 @Component({
   selector: 'drag-dialog-sample',
   templateUrl: './drag-dialog-sample.component.html',
   styleUrls: ['./drag-dialog-sample.component.scss']
 })
-export class DragDialogSampleComponent implements OnInit {
+export class DragDialogSampleComponent {
     public toggleStartPageX;
     public toggleStartPageY;
     private overlaySettings: OverlaySettings = {
@@ -17,21 +24,30 @@ export class DragDialogSampleComponent implements OnInit {
     };
   @ViewChild('toggleForm', { static: true })
   public toggleForm: IgxToggleDirective;
+  
+  @ViewChild('toggleFormDiv', {static: true})
+  public toggleFormDiv: ElementRef
 
   @ViewChild('toggleForm', { read: IgxDragDirective, static: true })
   public toggleFormDrag: IgxDragDirective;
-  constructor() { }
 
-  ngOnInit() {
-  }
+  @ViewChild("dialogButton", { static: true })
+  private buttonElement: ElementRef;
+
+  constructor( @Inject(IgxOverlayService) private overlayService: IgxOverlayService ) {}
 
   public openDialog() {
+    const overlayId = this.overlayService.attach(this.toggleFormDiv, {
+        positionStrategy: new ConnectedPositioningStrategy({
+            target: this.buttonElement.nativeElement
+        })
+    });
+    this.overlayService.show(overlayId);
     this.toggleForm.open(this.overlaySettings);
-
     if (!this.toggleStartPageX && !this.toggleStartPageY) {
         this.toggleStartPageX = this.toggleFormDrag.pageX;
         this.toggleStartPageY = this.toggleFormDrag.pageY;
     }
     this.toggleFormDrag.setPageXY(this.toggleStartPageX, this.toggleStartPageY);
-}
+  }
 }
