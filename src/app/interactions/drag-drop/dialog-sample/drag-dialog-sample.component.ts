bb3b1b19b4824ac27f10892272ff166437425f1e
PavlovVasil <vasil@vasilpavlov.com>
2019-07-12 14:39:48 +0300

    Add drag dialog styling

--- src/app/interactions/drag-drop/dialog-sample/drag-dialog-sample.component.ts
+++ src/app/interactions/drag-drop/dialog-sample/drag-dialog-sample.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IgxToggleDirective, IgxDragDirective, OverlaySettings, GlobalPositionStrategy, NoOpScrollStrategy } from 'igniteui-angular';
 
 @Component({
   selector: 'drag-dialog-sample',
@@ -6,13 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./drag-dialog-sample.component.scss']
 })
 export class DragDialogSampleComponent implements OnInit {
+    public toggleStartPageX;
+    public toggleStartPageY;
+    private overlaySettings: OverlaySettings = {
+        positionStrategy: new GlobalPositionStrategy(),
+        scrollStrategy: new NoOpScrollStrategy(),
+        modal: false,
+        closeOnOutsideClick: true
+    };
+  @ViewChild('toggleForm', { static: true })
+  public toggleForm: IgxToggleDirective;
 
+  @ViewChild('toggleForm', { read: IgxDragDirective, static: true })
+  public toggleFormDrag: IgxDragDirective;
   constructor() { }
 
   ngOnInit() {
   }
-  private showDialog() {
 
-  }
+  public openDialog() {
+    this.toggleForm.open(this.overlaySettings);
 
+    if (!this.toggleStartPageX && !this.toggleStartPageY) {
+        this.toggleStartPageX = this.toggleFormDrag.pageX;
+        this.toggleStartPageY = this.toggleFormDrag.pageY;
+    }
+    this.toggleFormDrag.setPageXY(this.toggleStartPageX, this.toggleStartPageY);
+}
 }
