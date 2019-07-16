f2595d96555853cd7780d91418b1d8dd6f2af5b2
PavlovVasil <vasil@vasilpavlov.com>
2019-07-16 15:24:22 +0300

    Add StackBlitz config

--- live-editing/configs/DragAndDropConfigGenerator.ts
+++ live-editing/configs/DragAndDropConfigGenerator.ts
@@ -1,6 +1,14 @@
 /* tslint:disable:object-literal-sort-keys */
-import { IgxDialogModule, IgxDragDirective, IgxDragDropModule, IgxDropDirective } from "igniteui-angular";
+import {
+    IgxDialogModule,
+    IgxDragDirective,
+    IgxDragDropModule,
+    IgxDropDirective,
+    NoOpScrollStrategy,
+    ConnectedPositioningStrategy,
+    GlobalPositionStrategy } from "igniteui-angular";
 import { DragAndDropSampleComponent } from "../../src/app/interactions/drag-drop/drag-drop.component";
+import { DragDialogSampleComponent } from "../../src/app/interactions/drag-drop/dialog-sample/drag-dialog-sample.component";
 import { AppModuleConfig } from "./core/AppModuleConfig";
 import { Config } from "./core/Config";
 import { IConfigGenerator } from "./core/IConfigGenerator";
@@ -15,6 +23,19 @@ export class DragAndDropConfigGenerator implements IConfigGenerator {
                 imports: [IgxDragDirective, IgxDropDirective,
                     IgxDragDropModule, IgxDialogModule, DragAndDropSampleComponent],
                 ngDeclarations: [DragAndDropSampleComponent],
+                ngImports: [IgxDragDropModule, IgxDialogModule]
+            }),
+            shortenComponentPathBy: "/interactions/"
+        }));
+
+        configs.push(new Config({
+            component: DragDialogSampleComponent,
+            appModuleConfig: new AppModuleConfig({
+                imports: [IgxDragDirective, IgxDropDirective,
+                    IgxDragDropModule, IgxDialogModule, DragAndDropSampleComponent, 
+                    NoOpScrollStrategy, ConnectedPositioningStrategy, 
+                    GlobalPositionStrategy,],
+                ngDeclarations: [DragDialogSampleComponent],
                 ngImports: [IgxDragDropModule, IgxDialogModule]
             }),
             shortenComponentPathBy: "/interactions/"
