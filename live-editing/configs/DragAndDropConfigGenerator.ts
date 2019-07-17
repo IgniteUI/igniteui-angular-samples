022e117abcd61099f8f7bcd68191f7215e2a4bae
PavlovVasil <vasil@vasilpavlov.com>
2019-07-17 16:23:52 +0300

    Add StackBlitz config

--- live-editing/configs/DragAndDropConfigGenerator.ts
+++ live-editing/configs/DragAndDropConfigGenerator.ts
@@ -1,6 +1,7 @@
 /* tslint:disable:object-literal-sort-keys */
 import { IgxDialogModule, IgxDragDirective, IgxDragDropModule, IgxDropDirective } from "igniteui-angular";
 import { DragAndDropSampleComponent } from "../../src/app/interactions/drag-drop/drag-drop.component";
+import { ListReorderSampleComponent } from "../../src/app/interactions/drag-drop/list-reorder-sample/list-reorder-sample.component"
 import { AppModuleConfig } from "./core/AppModuleConfig";
 import { Config } from "./core/Config";
 import { IConfigGenerator } from "./core/IConfigGenerator";
@@ -16,6 +17,17 @@ export class DragAndDropConfigGenerator implements IConfigGenerator {
                     IgxDragDropModule, IgxDialogModule, DragAndDropSampleComponent],
                 ngDeclarations: [DragAndDropSampleComponent],
                 ngImports: [IgxDragDropModule, IgxDialogModule]
+            }),
+            shortenComponentPathBy: "/interactions/"
+        }));
+
+        configs.push(new Config({
+            component: ListReorderSampleComponent,
+            appModuleConfig: new AppModuleConfig({
+                imports: [IgxDragDirective, IgxDropDirective,
+                    IgxDragDropModule, ListReorderSampleComponent],
+                ngDeclarations: [ListReorderSampleComponent],
+                ngImports: [IgxDragDropModule]
             }),
             shortenComponentPathBy: "/interactions/"
         }));
