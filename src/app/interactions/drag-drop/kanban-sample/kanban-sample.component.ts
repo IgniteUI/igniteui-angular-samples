ed455ba5e8af1c0924c25d67eb2eddb7b965ebe8
PavlovVasil <vasil@vasilpavlov.com>
2019-07-18 17:00:35 +0300

    Fix lint errors

--- src/app/interactions/drag-drop/kanban-sample/kanban-sample.component.ts
+++ src/app/interactions/drag-drop/kanban-sample/kanban-sample.component.ts
@@ -1,5 +1,5 @@
 import { Component, Renderer2 } from "@angular/core";
-import { IgxDropEventArgs } from 'igniteui-angular';
+import { IgxDropEventArgs } from "igniteui-angular";
 enum state {
     ToDo = "To Do",
     InProgress = "In Progress",
@@ -11,28 +11,26 @@ enum state {
     styleUrls: ["./kanban-sample.component.scss"]
 })
 export class KanbanSampleComponent {
-    constructor(private renderer: Renderer2) { };
-    
     public toDoList = [
-        { id: "STR-000132", text:"Implement chat bubble", state: state.ToDo },
-        { id: "STR-000097", text:"Implement sticky header", state: state.ToDo },
-        { id: "STR-000191", text:"Change trial days to credit", state: state.ToDo },
-        { id: "STR-000121", text:"Add analytics", state: state.ToDo }
+        { id: "STR-000132", text: "Implement chat bubble", state: state.ToDo },
+        { id: "STR-000097", text: "Implement sticky header", state: state.ToDo },
+        { id: "STR-000191", text: "Change trial days to credit", state: state.ToDo },
+        { id: "STR-000121", text: "Add analytics", state: state.ToDo }
     ];
     public inProgressList = [
-        { id: "STR-000124", text:"Implement fback widget", state: state.InProgress }
+        { id: "STR-000124", text: "Implement fback widget", state: state.InProgress }
     ];
     public doneList = [
-        { id: "STR-000129", text:"Add SSL to account pages", state: state.Done }
-    ]
+        { id: "STR-000129", text: "Add SSL to account pages", state: state.Done }
+    ];
+
+    constructor(private renderer: Renderer2) { }
 
     public onStateContainerEnter(event: IgxDropEventArgs) {
-        console.log(event.owner.element.nativeElement)
-        this.renderer.setStyle(event.owner.element.nativeElement, "background-color", "#A7D8F9")
-
+        this.renderer.setStyle(event.owner.element.nativeElement, "background-color", "#A7D8F9");
     }
+
     public onStateContainerLeave(event: IgxDropEventArgs) {
-        console.log(event.owner.element.nativeElement)
-        this.renderer.setStyle(event.owner.element.nativeElement, "background-color", "white")
+        this.renderer.setStyle(event.owner.element.nativeElement, "background-color", "white");
     }
 }
