4006436563210c648da397ac626241f9a8be51a8
PavlovVasil <vasil@vasilpavlov.com>
2019-07-18 16:05:44 +0300

    Add initial styling and implementation

--- src/app/interactions/drag-drop/kanban-sample/kanban-sample.component.ts
+++ src/app/interactions/drag-drop/kanban-sample/kanban-sample.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from "@angular/core";
-
+import { Component, Renderer2 } from "@angular/core";
+import { IgxDropEventArgs } from 'igniteui-angular';
+enum state {
+    ToDo = "To Do",
+    InProgress = "In Progress",
+    Done = "Done"
+}
 @Component({
     selector: "app-kanban-sample",
     templateUrl: "./kanban-sample.component.html",
     styleUrls: ["./kanban-sample.component.scss"]
 })
-export class KanbanSampleComponent implements OnInit {
+export class KanbanSampleComponent {
+    constructor(private renderer: Renderer2) { };
+    
+    public toDoList = [
+        { id: "STR-000132", text:"Implement chat bubble", state: state.ToDo },
+        { id: "STR-000097", text:"Implement sticky header", state: state.ToDo },
+        { id: "STR-000191", text:"Change trial days to credit", state: state.ToDo },
+        { id: "STR-000121", text:"Add analytics", state: state.ToDo }
+    ];
+    public inProgressList = [
+        { id: "STR-000124", text:"Implement fback widget", state: state.InProgress }
+    ];
+    public doneList = [
+        { id: "STR-000129", text:"Add SSL to account pages", state: state.Done }
+    ]
 
-    constructor() { }
+    public onStateContainerEnter(event: IgxDropEventArgs) {
+        console.log(event.owner.element.nativeElement)
+        this.renderer.setStyle(event.owner.element.nativeElement, "background-color", "#A7D8F9")
 
-    ngOnInit() {
     }
-
+    public onStateContainerLeave(event: IgxDropEventArgs) {
+        console.log(event.owner.element.nativeElement)
+        this.renderer.setStyle(event.owner.element.nativeElement, "background-color", "white")
+    }
 }
