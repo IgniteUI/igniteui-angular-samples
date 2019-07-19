e9d92e20aa8395b2e948958298a0941b135cb637
PavlovVasil <vasil@vasilpavlov.com>
2019-07-19 14:57:15 +0300

    Add blue highlight in title

--- src/app/interactions/drag-drop/kanban-sample/kanban-sample.component.ts
+++ src/app/interactions/drag-drop/kanban-sample/kanban-sample.component.ts
@@ -14,23 +14,35 @@ export class KanbanSampleComponent {
     public toDoList = [
         { id: "STR-000132", text: "Implement chat bubble", state: state.ToDo },
         { id: "STR-000097", text: "Implement sticky header", state: state.ToDo },
-        { id: "STR-000191", text: "Change trial days to credit", state: state.ToDo },
-        { id: "STR-000121", text: "Add analytics", state: state.ToDo }
+        { id: "STR-000191", text: "Change trial days to credit", state: state.ToDo }
+
     ];
     public inProgressList = [
-        { id: "STR-000124", text: "Implement fback widget", state: state.InProgress }
+        { id: "STR-000124", text: "Implement fback widget", state: state.InProgress },
+        { id: "STR-000121", text: "Add analytics", state: state.InProgress }
     ];
     public doneList = [
         { id: "STR-000129", text: "Add SSL to account pages", state: state.Done }
     ];
+    public tempObj;
+    public lastEnteredList;
 
     constructor(private renderer: Renderer2) { }
 
     public onStateContainerEnter(event: IgxDropEventArgs) {
-        this.renderer.setStyle(event.owner.element.nativeElement, "background-color", "#A7D8F9");
+        //console.log(event.owner.element.nativeElement)
+        this.renderer.addClass(event.owner.element.nativeElement, "dragHovered");
     }
 
     public onStateContainerLeave(event: IgxDropEventArgs) {
-        this.renderer.setStyle(event.owner.element.nativeElement, "background-color", "white");
+        //console.log(event.owner.element.nativeElement)
+        this.renderer.removeClass(event.owner.element.nativeElement,  "dragHovered");
     }
+    public dragStartHandler(event) {
+        //this.renderer.setStyle(event.owner.element.nativeElement, "display", "none");
+        this.tempObj = this.toDoList.filter((elem) => { return elem.id === event.owner.element.nativeElement.id })[0]
+        console.log(this.tempObj)   
+        this.toDoList = this.toDoList.filter((elem) => { return elem.id !== event.owner.element.nativeElement.id});
+        console.log(this.toDoList)
+    } 
 }
