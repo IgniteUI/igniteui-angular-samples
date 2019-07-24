ff72e46086dd01cf1e35bb9f52a777c263dda1b0
HUSSAR-mtrela <martin.trela@gmail.com>
2019-07-24 14:07:54 -0400

    added SparklineGrid sample

--- src/app/charts/sparkline/sparkline-samples.module.ts
+++ src/app/charts/sparkline/sparkline-samples.module.ts
@@ -1,8 +1,13 @@
+/* tslint:disable */
+
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { IgxGridModule } from "igniteui-angular";
+
 import { IgxSparklineModule} from "igniteui-angular-charts/ES5/igx-sparkline-module";
 import { SparklineDisplayTypesComponent } from "./sparkline-display-types/sparkline-display-types.component";
+import { SparklineGridComponent } from "./sparkline-grid/sparkline-grid.component";
 import { SparklineMarkersComponent } from "./sparkline-markers/sparkline-markers.component";
 import { SparklineNormalRangeComponent } from "./sparkline-normal-range/sparkline-normal-range.component";
 import { SparklineTrendlinesComponent } from "./sparkline-trendlines/sparkline-trendlines.component";
@@ -14,12 +19,14 @@ import { SparklineUnknownValuesComponent } from "./sparkline-unknown-values/spar
         SparklineNormalRangeComponent,
         SparklineTrendlinesComponent,
         SparklineUnknownValuesComponent,
-        SparklineDisplayTypesComponent
+        SparklineDisplayTypesComponent,
+        SparklineGridComponent
     ],
     imports: [
         CommonModule,
         FormsModule,
-        IgxSparklineModule
+        IgxSparklineModule,
+        IgxGridModule
     ]
 })
 export class SparklineSamplesModule { }
