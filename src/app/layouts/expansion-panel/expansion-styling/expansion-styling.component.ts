fb848efcad944c774f390010854e5803ef07eff1
IvayloG <inj.iganchev@gmail.com>
2019-07-31 12:37:48 +0300

    chore: Sample cleanup #1360

--- src/app/layouts/expansion-panel/expansion-styling/expansion-styling.component.ts
+++ src/app/layouts/expansion-panel/expansion-styling/expansion-styling.component.ts
@@ -1,11 +1,4 @@
-import { useAnimation } from "@angular/animations";
-import { Component, ViewChild } from "@angular/core";
-
-import {
-    IgxExpansionPanelComponent,
-    slideInLeft,
-    slideOutRight
-} from "igniteui-angular";
+import { Component } from "@angular/core";
 
 @Component({
     // tslint:disable-next-line:component-selector
@@ -15,32 +8,9 @@ import {
 })
 export class ExpansionPanelStylingComponent {
 
-    @ViewChild(IgxExpansionPanelComponent, { static: true })
-    public panel: IgxExpansionPanelComponent;
-
-    public animationSettingsCustom = {
-        closeAnimation: useAnimation(slideOutRight, {
-                params: {
-                    duration: "100ms",
-                    toPosition: "translateX(25px)"
-            }
-        }),
-        openAnimation: useAnimation(slideInLeft, {
-                params: {
-                    duration: "500ms",
-                    fromPosition: "translateX(-15px)",
-                    startOpacity: 0.1
-                }
-            })
-    };
-
     public user = {
         email: "",
         fullName: "",
         phone: undefined
     };
-
-    public collapsed() {
-        return this.panel && this.panel.collapsed;
-    }
 }
