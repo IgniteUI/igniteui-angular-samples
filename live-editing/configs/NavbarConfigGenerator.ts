08dfab06016c87a6aa2d3e152562b560eaa16ff8
Tacho <tachojelev@gmail.com>
2019-08-15 14:36:56 +0300

    Add navbar style sample IgniteUI/igniteui-docfx#1448

--- live-editing/configs/NavbarConfigGenerator.ts
+++ live-editing/configs/NavbarConfigGenerator.ts
@@ -2,6 +2,7 @@
 import { IgxIconModule, IgxNavbarModule } from "igniteui-angular";
 import { NavbarSample1Component } from "../../src/app/menus/navbar/navbar-sample-1/navbar-sample-1.component";
 import { NavbarSample2Component } from "../../src/app/menus/navbar/navbar-sample-2/navbar-sample-2.component";
+import { NavbarStyleComponent } from "../../src/app/menus/navbar/navbar-style/navbar-style.component";
 import { NavbarComponent } from "../../src/app/menus/navbar/navbar.component";
 import { AppModuleConfig } from "./core/AppModuleConfig";
 import { Config } from "./core/Config";
@@ -36,6 +37,16 @@ export class NavbarConfigGenerator implements IConfigGenerator {
             appModuleConfig: new AppModuleConfig({
                 imports: [IgxIconModule, IgxNavbarModule, NavbarSample2Component],
                 ngDeclarations: [NavbarSample2Component],
+                ngImports: [IgxIconModule, IgxNavbarModule]
+            }),
+            shortenComponentPathBy: "/menus/navbar/"
+        }));
+
+        configs.push(new Config({
+            component: NavbarStyleComponent,
+            appModuleConfig: new AppModuleConfig({
+                imports: [IgxIconModule, IgxNavbarModule, NavbarStyleComponent],
+                ngDeclarations: [NavbarStyleComponent],
                 ngImports: [IgxIconModule, IgxNavbarModule]
             }),
             shortenComponentPathBy: "/menus/navbar/"
