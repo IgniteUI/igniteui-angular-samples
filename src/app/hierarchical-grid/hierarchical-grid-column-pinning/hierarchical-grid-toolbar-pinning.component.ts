1aaf4d53effdefa032cdfa954d7b8b0b4d3ae5c4
mpavlov <mpavlov@infragistics.com>
2019-08-20 18:21:04 +0300

    fix(samples): Reworking HGrid pinning samples

--- src/app/hierarchical-grid/hierarchical-grid-column-pinning/hierarchical-grid-toolbar-pinning.component.ts
+++ src/app/hierarchical-grid/hierarchical-grid-column-pinning/hierarchical-grid-toolbar-pinning.component.ts
@@ -1,16 +1,15 @@
 import { Component } from "@angular/core";
-import { SINGERS } from "../data";
+import { CUSTOMERS } from "../data";
 
 @Component({
     selector: "hierarchical-grid-toolbar-pinning",
     styleUrls: ["./hierarchical-grid-toolbar-pinning.component.scss"],
     templateUrl: "hierarchical-grid-toolbar-pinning.component.html"
 })
-
 export class HGridToolbarPinningComponent {
     public localdata;
 
     constructor() {
-        this.localdata = SINGERS;
+        this.localdata = CUSTOMERS;
     }
 }
