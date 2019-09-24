424867699cc81e511eb1be7550de5b56cf1a8491
Hristo Anastasov <HAnastasov@infragistics.com>
2019-09-24 11:31:06 +0300

    merge vNext

--- src/app/treegrid-finjs/treegrid-finjs.module.ts
+++ src/app/treegrid-finjs/treegrid-finjs.module.ts
@@ -1,8 +1,8 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
-import { IgxButtonModule, IgxDialogModule, IgxExcelExporterService,
-    IgxSliderModule, IgxSwitchModule, IgxTreeGridModule } from "igniteui-angular";
+import { IgxButtonModule, IgxExcelExporterService, IgxSliderModule,
+    IgxSwitchModule, IgxTreeGridModule } from "igniteui-angular";
 import { LocalDataService } from "../grid-finjs/localData.service";
 import { TreeGridFinJSComponent } from "./tree-grid-finjs-sample.component";
 import { TreeGridGroupingPipe } from "./tree-grid-grouping.pipe";
@@ -20,8 +20,7 @@ import { TreeGridFinjsRoutingModule } from "./treegrid-finjs-routing.module";
         IgxSwitchModule,
         IgxSliderModule,
         TreeGridFinjsRoutingModule,
-        IgxTreeGridModule,
-        IgxDialogModule
+        IgxTreeGridModule
     ],
     providers: [LocalDataService, IgxExcelExporterService]
 })
