393a95d242dd583c090c79cea3de335896c40cc9
HristoP96 <HPopov@infragistics.com>
2019-09-25 17:07:44 +0300

    Initial commit

---
+++ src/app/grid/grid-dynamic-chart-data/grid-dynamic-chart-data.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grid-dynamic-chart-data',
+  templateUrl: './grid-dynamic-chart-data.component.html',
+  styleUrls: ['./grid-dynamic-chart-data.component.scss']
+})
+export class GridDynamicChartDataComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
