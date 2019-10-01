bdb3ac840171c8c352c2e385a5f78b93c72e2858
3phase <pbozhinow@gmail.com>
2019-10-01 16:24:02 +0300

    Updates all records similar to customers.ts and their dependencies

--- src/app/grid/grid-column-hiding-sample/grid-column-hiding-sample.component.ts
+++ src/app/grid/grid-column-hiding-sample/grid-column-hiding-sample.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { DATA } from "./data";
+import { DATA } from "../../data/customers";
 
 @Component({
     selector: "grid-column-hiding-sample",
