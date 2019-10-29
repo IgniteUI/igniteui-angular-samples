4c863f3c140986f862d560c0920eae6ed22845a0
MonikaKirkova <monika_kirkova@abv.bg>
2019-10-29 16:52:23 +0200

    feat(grid): added multi row drag sample

---
+++ src/app/grid/grid-multiple-row-drag/grid-multiple-row-drag.component.ts
@@ -0,0 +1,67 @@
+import { Component, ViewChild } from '@angular/core';
+import { IgxGridComponent, IRowDragStartEventArgs } from "igniteui-angular";
+import { DATA } from "../../data/customers";
+
+enum DragIcon {
+    DEFAULT = "drag_indicator",
+    ALLOW = "allow-drop"
+}
+
+@Component({
+    selector: 'grid-multiple-row-drag',
+    styleUrls: ['./grid-multiple-row-drag.component.scss'],
+    templateUrl: './grid-multiple-row-drag.component.html'
+})
+export class GridMultipleRowDragComponent  {
+    public data1: any[];
+    public data2: any[];
+    @ViewChild("sourceGrid", { read: IgxGridComponent, static: true }) 
+    public sourceGrid: IgxGridComponent;
+    @ViewChild("targetGrid", { read: IgxGridComponent, static: true }) 
+    public targetGrid: IgxGridComponent;
+    public selectionMode = "multiple";
+    constructor() {
+        this.data1 = DATA;
+        this.data2 = [];
+        
+    }
+
+    public onRowDragEnd(args) {
+        args.animation = true;
+    }
+    
+    public onRowDragStart(args) {
+        args.drag.ghostElement.classList.add(DragIcon.ALLOW);
+    }
+    
+    public onDropAllowed(args, draggedElements) {
+        let selected = false;
+        const ids = this.sourceGrid.selectedRows();
+        const selectedRowData = this.sourceGrid.data.filter((record) => {
+            return ids.includes(record.ID);
+        });
+        selectedRowData.forEach((rowData) => {
+            draggedElements++;
+            selected = true;
+            this.targetGrid.addRow(rowData);
+            this.sourceGrid.deleteRow(rowData.ID);
+        });
+        if(selected === false){
+            this.targetGrid.addRow(args.dragData.rowData);
+            this.sourceGrid.deleteRow(args.dragData.rowID);
+        }
+    }
+
+    public onEnter(args) {
+        args.drag.ghostElement.classList.add(DragIcon.ALLOW);
+        
+    }
+
+    public onLeave(args) {
+        const dragGhost: HTMLElement =  args.drag.ghostElement;
+        if (dragGhost) {
+            dragGhost.classList.remove(DragIcon.ALLOW);
+        }
+    }
+
+}
