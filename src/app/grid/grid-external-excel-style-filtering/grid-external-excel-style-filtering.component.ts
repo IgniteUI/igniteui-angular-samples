a12b5e2c658e1a6850e7e3559bdab57c429ddcd5
astaev <astaev@infragistics.com>
2019-11-04 15:44:39 +0200

    samples(grid): Updating the ESF external filtering #1526

--- src/app/grid/grid-external-excel-style-filtering/grid-external-excel-style-filtering.component.ts
+++ src/app/grid/grid-external-excel-style-filtering/grid-external-excel-style-filtering.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit, ViewChild } from "@angular/core";
+import { ChangeDetectionStrategy, Component } from "@angular/core";
 import { IgxGridComponent } from "igniteui-angular";
 import { DATA } from "../../data/nwindData";
 
 @Component({
+    changeDetection: ChangeDetectionStrategy.OnPush,
     selector: "grid-external-excel-style-filtering",
     templateUrl: "./grid-external-excel-style-filtering.component.html",
     styleUrls: ["./grid-external-excel-style-filtering.component.scss"]
