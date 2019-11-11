31b9a24b0164a73c34db12f3331c0e9e8d728b79
HristoP96 <HPopov@infragistics.com>
2019-11-11 21:38:36 +0200

    add new carousel base sample

--- src/app/layouts/carousel/carousel-sample-4/carousel-sample-4.component.ts
+++ src/app/layouts/carousel/carousel-sample-4/carousel-sample-4.component.ts
@@ -1,54 +1,70 @@
-import { Component, OnInit, ViewChild, ViewChildren, ViewEncapsulation } from "@angular/core";
+import { Component, OnInit, ViewChild, ViewChildren, ViewEncapsulation, AfterViewInit } from "@angular/core";
 import { Router } from "@angular/router";
-import { Direction, IgxButtonModule, IgxCarouselComponent, IgxLinearProgressBarComponent } from "igniteui-angular";
+import {IgxButtonModule, IgxCarouselComponent, IgxLinearProgressBarComponent } from "igniteui-angular";
 
 @Component({
-  encapsulation: ViewEncapsulation.None,
   selector: "app-carousel",
   styleUrls: ["./carousel-sample-4.component.scss"],
   templateUrl: "./carousel-sample-4.component.html"
 })
-export class CarouselSample4Component implements OnInit {
+export class CarouselSample4Component implements OnInit, AfterViewInit {
   @ViewChild("carousel", { static: true }) public carousel: IgxCarouselComponent;
-  @ViewChild("linearbar", { static: true }) public linearbar: IgxLinearProgressBarComponent;
 
   public slides: any[] = [];
-  public loop = true;
+  public loop = false;
   public total: number;
   public current: number;
+  private _prevArrow: HTMLElement;
+  private _nextArrow: HTMLElement;
 
   constructor(private router: Router) { }
 
   public ngOnInit() {
-    this.addNewSlide();
-    this.carousel.stop();
-    this.total = this.slides.length;
-    this.current = this.carousel.current;
+      this.addNewSlide();
+      this.carousel.stop();
+      this.total = this.slides.length;
+      this.current = this.carousel.current;
+      this.carousel.onSlideChanged.subscribe((evt) => {
+        this.current = evt.carousel.current + 1;
+        if (this.current === 1) {
+            this._prevArrow.style.opacity = ".5";
+            this._prevArrow.style.pointerEvents = "none";
+            this._nextArrow.style.opacity = "";
+            this._nextArrow.style.pointerEvents = "all";
+
+        } else if (this.current === this.carousel.slides.length) {
+            this._nextArrow.style.opacity = ".5";
+            this._nextArrow.style.pointerEvents = "none";
+            this._prevArrow.style.opacity = "";
+            this._prevArrow.style.pointerEvents = "all";
+
+        } else {
+            this._prevArrow.style.opacity = "";
+            this._nextArrow.style.opacity = "";
+            this._prevArrow.style.pointerEvents = "all";
+            this._nextArrow.style.pointerEvents = "all";
+
+        }
+      });
+  }
+
+  public ngAfterViewInit() {
+    this._nextArrow = (document.getElementsByClassName("igx-carousel__arrow--next")[0] as HTMLElement);
+    this._prevArrow = (document.getElementsByClassName("igx-carousel__arrow--prev")[0] as HTMLElement);
   }
 
   public addNewSlide() {
     this.slides.push(
       {
-        description: "30+ Material-based Angular components to code speedy web apps faster.",
-        heading: "Ignite UI for Angular",
-        image: "assets/images/carousel/slide1-angular.png"
+        src: "assets/images/svg/carousel/Onboarding1.svg"
       },
       {
-        description: "A complete JavaScript UI library empowering you to build data-rich responsive web apps.",
-        heading: "Ignite UI for Javascript",
-        image: "assets/images/carousel/slide2-ignite.png"
+        src: "assets/images/svg/carousel/Onboarding2.svg"
       },
       {
-        description: "Build full-featured business apps with the most versatile set of ASP.NET AJAX UI controls",
-        heading: "Ultimate UI for ASP.NET",
-        image: "assets/images/carousel/slide3-aspnet.png"
+        src: "assets/images/svg/carousel/Onboarding3.svg"
       }
     );
-  }
-
-  public onSlideChanged(carousel: IgxCarouselComponent) {
-    this.current = carousel.current + 1;
-    this.linearbar.value = carousel.current + 1;
   }
 
   public goTo(index: number) {
