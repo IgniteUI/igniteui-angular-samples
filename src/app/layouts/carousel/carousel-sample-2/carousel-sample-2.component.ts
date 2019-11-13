9082c470491f64db2d293c00b59b7701faddbbb5
HristoP96 <HPopov@infragistics.com>
2019-11-13 17:01:11 +0200

    add carousel no-nav sample

--- src/app/layouts/carousel/carousel-sample-2/carousel-sample-2.component.ts
+++ src/app/layouts/carousel/carousel-sample-2/carousel-sample-2.component.ts
@@ -1,39 +1,63 @@
 import { Component, OnInit, ViewChild, ViewEncapsulation } from "@angular/core";
-import { IgxCarouselComponent } from "igniteui-angular";
+import { IgxCarouselComponent, IgxListComponent, ISlideEventArgs } from "igniteui-angular";
 
 @Component({
-  encapsulation: ViewEncapsulation.None,
   selector: "app-carousel",
   styleUrls: ["./carousel-sample-2.component.scss"],
   templateUrl: "./carousel-sample-2.component.html"
 })
 export class CarouselSample2Component implements OnInit {
-  @ViewChild("carousel", { static: true }) public carousel: IgxCarouselComponent;
+  @ViewChild(IgxCarouselComponent, { static: true }) public carousel: IgxCarouselComponent;
+  @ViewChild(IgxListComponent, {static: true})
+  public list: IgxListComponent;
 
   public slides: any[] = [];
-
+  public currentIndex = 0;
   constructor() { }
 
   public ngOnInit() {
-    this.addNewSlide();
+    this.addSlides();
+
+    this.carousel.onSlideChanged.subscribe((args: ISlideEventArgs) => {
+        this.currentIndex = args.slide.index;
+    });
   }
 
-  public addNewSlide() {
+  public addSlides() {
     this.slides.push(
       {
-        description: "30+ Material-based Angular components to code speedy web apps faster.",
-        heading: "Ignite UI for Angular",
-        image: "assets/images/carousel/slide1-angular.png"
+        title: "Wonderful Coast",
+        subTitle: "May to October",
+        image: "assets/images/carousel/WonderfulCoast",
+        price: 1299,
+        description: "Delicious seafood and wines along the coasts of beautiful Italy"
       },
       {
-        description: "A complete JavaScript UI library empowering you to build data-rich responsive web apps.",
-        heading: "Ignite UI for Javascript",
-        image: "assets/images/carousel/slide2-ignite.png"
+        title: "Cultural Dip",
+        subTitle: "July and August",
+        image: "assets/images/carousel/CulturalDip",
+        price: 1949,
+        description: "Immerse yourself in the history and culture of the far East"
       },
       {
-        description: "Build full-featured business apps with the most versatile set of ASP.NET AJAX UI controls",
-        heading: "Ultimate UI for ASP.NET",
-        image: "assets/images/carousel/slide3-aspnet.png"
+        title: "Golden Beaches",
+        subTitle: "October to March",
+        image: "assets/images/carousel/GoldenBeaches",
+        price: 2799,
+        description: "Endless beachfronts, crystal blue water and the finest grains of sand"
+      },
+      { title: "Island Of History",
+        subTitle: "May to October",
+        image: "assets/images/carousel/IslandOfHistory",
+        price: 1419,
+        description: "Discover this hidden gem of the Mediterranean rich of antiquity"
+      },
+      {
+        title: "Amazing Bridge",
+        subTitle: "Spring and Autumn",
+        image: "assets/images/carousel/AmazingBridge",
+        price: 1049,
+        description: "Walk along one of the engineering wonders of the twentieth century"
       }
     );
   }
