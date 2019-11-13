875cb938c033dc9eb42b55d4d5c6ad0fd9f0d97d
HristoP96 <HPopov@infragistics.com>
2019-11-13 11:00:29 +0200

    add card to carousel animation sample

--- src/app/layouts/carousel/carousel-sample-3/carousel-sample-3.component.ts
+++ src/app/layouts/carousel/carousel-sample-3/carousel-sample-3.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, ViewChild, ViewChildren, ViewEncapsulation, HostListener } from "@angular/core";
-import { IgxCarouselComponent, IgxLinearProgressBarComponent } from "igniteui-angular";
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { IgxCarouselComponent } from "igniteui-angular";
 
 @Component({
   selector: "app-carousel",
@@ -15,9 +15,6 @@ export class CarouselSample3Component implements OnInit {
   public total: number;
   public current: number;
   public animations = ["slide", "fade", "none"];
-  constructor() { 
-
-  }
 
   public ngOnInit() {
 
@@ -39,26 +36,33 @@ export class CarouselSample3Component implements OnInit {
         this.carousel.play();
       } else {
           this.carousel.navigation = false;
-          this.carousel.pause = true;
           this.carousel.stop();
       }
   }
+
+  public visitPage(location) {
+      window.location.href = location;
+  }
+
   public addSlides() {
     this.slides.push(
       {
         description: "30+ Material-based Angular components to code speedy web apps faster.",
         heading: "Ignite UI for Angular",
-        image: "assets/images/carousel/slide1-angular.png"
+        image: "assets/images/carousel/slide1-angular.png",
+        link: "https://www.infragistics.com/products/ignite-ui-angular"
       },
       {
         description: "A complete JavaScript UI library empowering you to build data-rich responsive web apps.",
         heading: "Ignite UI for Javascript",
-        image: "assets/images/carousel/slide2-ignite.png"
+        image: "assets/images/carousel/slide2-ignite.png",
+        link: "https://www.infragistics.com/products/ignite-ui"
       },
       {
         description: "Build full-featured business apps with the most versatile set of ASP.NET AJAX UI controls",
         heading: "Ultimate UI for ASP.NET",
-        image: "assets/images/carousel/slide3-aspnet.png"
+        image: "assets/images/carousel/slide3-aspnet.png",
+        link: "https://www.infragistics.com/products/aspnet"
       }
     );
   }
