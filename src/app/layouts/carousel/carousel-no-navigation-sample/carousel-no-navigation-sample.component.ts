96e5280e4355cd6f70e5df772c7f27f8b567d0e7
HristoP96 <HPopov@infragistics.com>
2019-11-22 16:30:19 +0200

    mass refactoring for no-navigation sample

--- src/app/layouts/carousel/carousel-no-navigation-sample/carousel-no-navigation-sample.component.ts
+++ src/app/layouts/carousel/carousel-no-navigation-sample/carousel-no-navigation-sample.component.ts
@@ -28,39 +28,51 @@ export class CarouselNoNavigationSampleComponent implements OnInit {
     });
   }
 
+  public slideStyle(image: string) {
+    return  {
+        background: `url(${image})`,
+        backgroundSize: "cover"
+    };
+  }
+
   public addSlides() {
     this.slides.push(
       {
         title: "Wonderful Coast",
         subTitle: "May to October",
-        image: "assets/images/carousel/WonderfulCoast",
+        image: "assets/images/carousel/WonderfulCoast.png",
+        thumb: "assets/images/carousel/WonderfulCoastThumb.png",
         price: 1299,
         description: "Delicious seafood and wines along the coasts of beautiful Italy"
       },
       {
         title: "Cultural Dip",
         subTitle: "July and August",
-        image: "assets/images/carousel/CulturalDip",
+        image: "assets/images/carousel/CulturalDip.png",
+        thumb: "assets/images/carousel/CulturalDipThumb.png",
         price: 1949,
         description: "Immerse yourself in the history and culture of the far East"
       },
       {
         title: "Golden Beaches",
         subTitle: "October to March",
-        image: "assets/images/carousel/GoldenBeaches",
+        image: "assets/images/carousel/GoldenBeaches.png",
+        thumb: "assets/images/carousel/GoldenBeachesThumb.png",
         price: 2799,
         description: "Endless beachfronts, crystal blue water and the finest grains of sand"
       },
       { title: "Island Of History",
         subTitle: "May to October",
-        image: "assets/images/carousel/IslandOfHistory",
+        image: "assets/images/carousel/IslandOfHistory.png",
+        thumb: "assets/images/carousel/IslandOfHistoryThumb.png",
         price: 1419,
         description: "Discover this hidden gem of the Mediterranean rich of antiquity"
       },
       {
         title: "Amazing Bridge",
         subTitle: "Spring and Autumn",
-        image: "assets/images/carousel/AmazingBridge",
+        image: "assets/images/carousel/AmazingBridge.png",
+        thumb: "assets/images/carousel/AmazingBridgeThumb.png",
         price: 1049,
         description: "Walk along one of the engineering wonders of the twentieth century"
       }
