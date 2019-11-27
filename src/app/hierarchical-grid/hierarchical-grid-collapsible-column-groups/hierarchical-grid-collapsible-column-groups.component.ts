72fe9c61268160e728131a6c17687e4db94f5358
ddincheva <desislava9dincheva@gmail.com>
2019-11-27 16:42:45 +0200

    chore(*): update hirarchical grid collapsible coulumn groups sample

---
+++ src/app/hierarchical-grid/hierarchical-grid-collapsible-column-groups/hierarchical-grid-collapsible-column-groups.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { IgxHierarchicalGridComponent } from "igniteui-angular";
+import { CUSTOMERS } from "../data";
+
+@Component({
+    selector: "hierarchical-grid-collapsible-column-groups",
+    styleUrls: ["./hierarchical-grid-collapsible-column-groups.component.scss"],
+    templateUrl: "hierarchical-grid-collapsible-column-groups.component.html"
+})
+
+export class HGridCollapsibleColumnGroupComponent implements OnInit {
+    public localdata;
+
+    @ViewChild("hierarchicalGrid", { static: true })
+    private hierarchicalGrid: IgxHierarchicalGridComponent;
+
+    constructor() {
+
+    }
+    public ngOnInit(): void {
+        this.localdata = CUSTOMERS;
+        for (const item of this.localdata) {
+            const names = item.ContactName.split(" ");
+            item.FirstName = names[0];
+            item.LastName = names[names.length - 1];
+            item.FullAddress = `${item.Address}, ${item.City}, ${item.Country}`;
+        }
+    }
+
+    public getTooltipText(expanded) {
+        return expanded ?
+            "The column is expanded! Click here to collapse." : "The column is collapsed! Click here to expand";
+    }
+
+}
