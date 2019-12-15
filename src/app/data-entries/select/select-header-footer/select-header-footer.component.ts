76964d23d62dcd2217b184955901cdd1be93ef14
IvayloG <inj.iganchev@gmail.com>
2019-12-15 23:32:21 +0200

    chore(select): Update functionality #1571

--- src/app/data-entries/select/select-header-footer/select-header-footer.component.ts
+++ src/app/data-entries/select/select-header-footer/select-header-footer.component.ts
@@ -1,65 +1,48 @@
-import { ChangeDetectorRef, Component, OnInit, ViewChild } from "@angular/core";
-import { IgxButtonGroupComponent, IgxSelectComponent } from "igniteui-angular";
-
+import { ChangeDetectorRef, Component, OnInit } from "@angular/core";
 @Component({
     selector: "select-header-footer",
     styleUrls: ["select-header-footer.component.scss"],
     templateUrl: "select-header-footer.component.html"
 })
 export class SelectHeaderFooterComponent implements OnInit {
-    @ViewChild(IgxSelectComponent) public select: IgxSelectComponent;
-    @ViewChild(IgxButtonGroupComponent) public buttons: IgxButtonGroupComponent;
-
     public flightCount: number;
     public trainCount: number;
     public boatCount: number;
     public selected: string;
-    public fruitsDS: Array<{ type: string, delivery: string }>;
+    public fruits: Array<{ type: string, delivery: string }>;
 
-    public filteredFlight2: any;
-    public filteredFlight3: any;
-
-    // TODO add interface for Fruit/Vegetable
-    // TODO extend data source
-    public fruits: Array<{ type: string, delivery: string }> = [
+    public fruitsDS: Array<{ type: string, delivery: string }> = [
             { type: "Apple", delivery: "boat" },
-            { type: "Orange", delivery: "boat" },
+            { type: "Banana", delivery: "flight"},
+            { type: "Blackberry", delivery: "train"},
+            { type: "Blueberry", delivery: "train"},
+            { type: "Coconut", delivery: "flight"},
             { type: "Cherry", delivery: "boat"},
-            { type: "Banana", delivery: "airplane"},
-            { type: "Pineapple", delivery: "airplane"},
-            { type: "Melon", delivery: "airplane"},
-            { type: "Pear", delivery: "train"},
-            { type: "Watermelon", delivery: "train"},
             { type: "Grape", delivery: "train"},
-            { type: "Pomegranate", delivery: "train"}
+            { type: "Japanese plum", delivery: "flight"},
+            { type: "Pineapple", delivery: "flight"},
+            { type: "Melon", delivery: "flight"},
+            { type: "Orange", delivery: "boat" },
+            { type: "Pear", delivery: "train"},
+            { type: "Peach", delivery: "boat"},
+            { type: "Pineapple", delivery: "flight"},
+            { type: "Pomegranate", delivery: "flight"},
+            { type: "Watermelon", delivery: "train"}
         ];
 
     constructor(public cdr: ChangeDetectorRef) {
-
     }
 
     public ngOnInit() {
-        this.flightCount = this.fruits.filter(items => items.delivery === "airplane").length;
-        this.trainCount = this.fruits.filter(items => items.delivery === "train").length;
-        this.boatCount = this.fruits.filter(items => items.delivery === "boat").length;
-        this.fruitsDS = this.fruits;
+        this.flightCount = this.fruitsDS.filter(items => items.delivery === "flight").length;
+        this.trainCount = this.fruitsDS.filter(items => items.delivery === "train").length;
+        this.boatCount = this.fruitsDS.filter(items => items.delivery === "boat").length;
+        this.fruits = this.fruitsDS;
     }
 
-    public filterFlight() {
-        this.fruitsDS = this.fruits.filter(fruit => fruit.delivery === "airplane");
-        this.selected = "airplane";
-        this.cdr.detectChanges();
-    }
-
-    public filterTrain() {
-        this.fruitsDS = this.fruits.filter(fruit => fruit.delivery === "train");
-        this.selected = "train";
-        this.cdr.detectChanges();
-    }
-
-    public filterBoat() {
-        this.fruitsDS = this.fruits.filter(fruit => fruit.delivery === "boat");
-        this.selected = "boat";
+    public filter(value) {
+        this.selected = value;
+        this.fruits = this.fruitsDS.filter(fruit => fruit.delivery === value);
         this.cdr.detectChanges();
     }
 }
