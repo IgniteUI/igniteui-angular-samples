c0b1205b774aac43a7b93acf83332754bf68d703
ddincheva <desislava9dincheva@gmail.com>
2020-02-06 15:04:42 +0200

    chore(DataAnalysis): update dataAnalysis sample module with newly added components

--- projects/app-lob/src/app/grid-dynamic-chart-data/grid-dynamic-chart-data.module.ts
+++ projects/app-lob/src/app/grid-dynamic-chart-data/grid-dynamic-chart-data.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from "@angular/core";
-import { IgxButtonModule, IgxDialogModule, IgxDividerModule,
-            IgxGridModule, IgxTabsModule, IgxToggleModule } from "igniteui-angular";
+import { IgxButtonModule, IgxDividerModule, IgxGridModule, IgxTabsModule, IgxToggleModule } from "igniteui-angular";
 import { IgxBarSeriesModule, IgxCategoryChartModule, IgxCategoryXAxisModule,
     IgxDataChartCategoryModule, IgxDataChartComponent, IgxDataChartCoreModule,
     IgxDataChartInteractivityModule, IgxDataChartScatterModule, IgxDataChartStackedModule,
     IgxItemLegendComponent, IgxItemLegendModule, IgxLegendComponent, IgxLegendModule,
     IgxNumericXAxisModule, IgxNumericYAxisModule, IgxPieChartComponent } from "igniteui-angular-charts";
 import { IgxPieChartModule } from "igniteui-angular-charts/";
+import { IgxChartMenuComponent } from "./context-menu/chart-dialog/chart-dialog.component";
 import { IgxContextMenuComponent } from "./context-menu/context-menu.component";
 import { ChartHostDirective,
     ChartIntegrationDirective } from "./directives/chart-integration/chart-integration.directive";
 import { ConditionalFormattingDirective } from "./directives/conditional-formatting/conditional-formatting.directive";
 import { GridDynamicChartDataRoutingModule } from "./grid-dynamic-chart-data-routing.module";
-import { FilterTypePipe, GridDynamicChartDataComponent, NamePipe } from "./grid-dynamic-chart-data.component";
+import { GridDynamicChartDataComponent } from "./grid-dynamic-chart-data.component";
 
 @NgModule({
     declarations: [
@@ -20,13 +20,11 @@ import { FilterTypePipe, GridDynamicChartDataComponent, NamePipe } from "./grid-
         ChartHostDirective,
         ConditionalFormattingDirective,
         ChartIntegrationDirective,
-        NamePipe,
-        FilterTypePipe,
-        IgxContextMenuComponent
+        IgxContextMenuComponent,
+        IgxChartMenuComponent
     ],
     imports: [
         IgxButtonModule,
-        IgxDialogModule,
         IgxDividerModule,
         IgxGridModule,
         IgxTabsModule,
@@ -52,7 +50,8 @@ import { FilterTypePipe, GridDynamicChartDataComponent, NamePipe } from "./grid-
         IgxDataChartComponent,
         IgxItemLegendComponent,
         IgxLegendComponent,
-        IgxPieChartComponent
+        IgxPieChartComponent,
+        IgxChartMenuComponent
     ]
 })
 export class GridDynamicChartDataModule {}
