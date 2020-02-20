d7fd26c0994031ebae5c04d97c7dd0a93ab0e04b
MPopov <desig9stein@gmail.com>
2020-02-20 18:16:51 +0200

    fix(style): Fix layout styles for all grids

--- projects/app-lob/src/app/grid-taskplanner/taskplanner.module.ts
+++ projects/app-lob/src/app/grid-taskplanner/taskplanner.module.ts
@@ -1,8 +1,19 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
-import { IgxAvatarModule, IgxButtonGroupModule, IgxDialogModule, IgxGridModule,
-    IgxInputGroupModule, IgxMaskModule, IgxSwitchModule, IgxToastModule } from "igniteui-angular";
+import {
+    IgxAvatarModule,
+    IgxButtonGroupModule,
+    IgxCardModule,
+    IgxDialogModule,
+    IgxGridModule,
+    IgxInputGroupModule,
+    IgxListModule,
+    IgxMaskModule,
+    IgxSwitchModule,
+    IgxToastModule
+} from "igniteui-angular";
+
 import { TasksDataService } from "../services/tasks.service";
 import { TaskPlannerComponent } from "./task-planner/taskplanner.component";
 import { GridWithTransactionsComponent } from "./task-planner/transaction.component";
@@ -24,8 +35,11 @@ import { TaskPlannerRoutingModule } from "./taskplanner-routing.module";
         IgxInputGroupModule,
         IgxButtonGroupModule,
         IgxSwitchModule,
+        IgxCardModule,
+        IgxListModule,
         TaskPlannerRoutingModule
     ],
     providers: [TasksDataService]
 })
 export class TaskPlannerModule {}
+
