071dbd73f27c5dd133f859446c8c97b810d3ed94
didimmova <d.dimova11@gmail.com>
2020-02-25 16:04:40 +0200

    refactor(layout manager): making new samples for layout manager
    Closes #1807, #1767, #1794

---
+++ src/app/layouts/layout/layout-sample/layout-sample.component.ts
@@ -0,0 +1,11 @@
+import { Component, ViewEncapsulation } from "@angular/core";
+
+@Component({
+  encapsulation: ViewEncapsulation.None,
+  selector: "app-layout-sample",
+  styleUrls: ["./layout-sample.component.scss"],
+  templateUrl: "./layout-sample.component.html"
+})
+export class LayoutSampleComponent {
+  constructor() { }
+}
