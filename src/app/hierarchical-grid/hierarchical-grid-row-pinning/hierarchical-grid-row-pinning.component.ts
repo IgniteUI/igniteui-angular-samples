c9ad416abc25b789f20a935d6f7efb3abd341eba
Martin Dragnev <MDragnev@infragistics.com>
2020-04-24 18:48:08 +0300

    Changing pinning icon

--- src/app/hierarchical-grid/hierarchical-grid-row-pinning/hierarchical-grid-row-pinning.component.ts
+++ src/app/hierarchical-grid/hierarchical-grid-row-pinning/hierarchical-grid-row-pinning.component.ts
@@ -1,8 +1,11 @@
 import { Component, ViewChild } from "@angular/core";
-import { IgxHierarchicalGridComponent, IgxHierarchicalRowComponent, RowPinningPosition } from "igniteui-angular";
+import { IgxHierarchicalGridComponent, IgxHierarchicalRowComponent,
+        IgxIconService, RowPinningPosition} from "igniteui-angular";
 import { IPinningConfig } from "igniteui-angular/lib/grids/common/grid.interface";
+import { icons } from "../../grid/services/svgIcons";
 import { SINGERS } from "../data";
 
+const FILTERING_ICONS_FONT_SET = "filtering-icons";
 @Component({
     selector: "hierarchical-grid-row-pinning",
     styleUrls: ["./hierarchical-grid-row-pinning.component.scss"],
@@ -12,12 +15,20 @@ import { SINGERS } from "../data";
 export class HGridRowPinningSampleComponent {
     public localdata;
     public pinningConfig: IPinningConfig = {rows: RowPinningPosition.Top};
-
     @ViewChild("hierarchicalGrid", { static: true })
     private hierarchicalGrid: IgxHierarchicalGridComponent;
 
-    constructor() {
+    constructor(private iconService: IgxIconService) {
         this.localdata = SINGERS;
+    }
+
+    public ngAfterViewInit() {
+        const pinnedIcons = icons.filter(icon => icon.name === "pin" || icon.name === "unpin");
+        pinnedIcons.forEach(icon => {
+            if (!this.iconService.isSvgIconCached(icon.name, FILTERING_ICONS_FONT_SET)) {
+                this.iconService.addSvgIconFromText(icon.name, icon.value, FILTERING_ICONS_FONT_SET);
+            }
+        });
     }
 
     public togglePinning(row: IgxHierarchicalRowComponent, event) {
