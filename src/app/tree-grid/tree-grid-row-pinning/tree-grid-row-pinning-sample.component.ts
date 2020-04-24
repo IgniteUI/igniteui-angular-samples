747d8ade38e3305fce90e0a2f940766efd8c0e3d
Martin Dragnev <MDragnev@infragistics.com>
2020-04-24 19:12:41 +0300

    Changing pinning icons

--- src/app/tree-grid/tree-grid-row-pinning/tree-grid-row-pinning-sample.component.ts
+++ src/app/tree-grid/tree-grid-row-pinning/tree-grid-row-pinning-sample.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit, ViewChild } from "@angular/core";
-import { IgxGridRowComponent, IgxTreeGridComponent, RowPinningPosition } from "igniteui-angular";
+import {IgxGridRowComponent, IgxIconService, IgxTreeGridComponent, RowPinningPosition } from "igniteui-angular";
 import { IPinningConfig } from "igniteui-angular/lib/grids/common/grid.interface";
+import { icons } from "../../grid/services/svgIcons";
 import { generateEmployeeFlatData, IEmployee } from "../data/employees-flat";
 
+const FILTERING_ICONS_FONT_SET = "filtering-icons";
 @Component({
     selector: "app-tree-grid-row-pinning",
     styleUrls: ["tree-grid-row-pinning-sample.component.scss"],
@@ -14,6 +16,9 @@ export class TreeGridRowPinningSampleComponent implements OnInit {
     public columns: any[];
     public pinningConfig: IPinningConfig = { rows: RowPinningPosition.Top };
     @ViewChild("treeGrid", { static: true }) public treeGrid1: IgxTreeGridComponent;
+
+    constructor(private iconService: IgxIconService) {}
+
     public ngOnInit(): void {
         this.data = generateEmployeeFlatData();
 
@@ -24,6 +29,15 @@ export class TreeGridRowPinningSampleComponent implements OnInit {
             { field: "Title", label: "Title", resizable: true, movable: true, sortable: true, filterable: true, editable: true, dataType: "string" },
             { field: "HireDate", label: "Hire Date", resizable: true, movable: true, sortable: true, filterable: true, editable: true, dataType: "date" }
         ];
+    }
+
+    public ngAfterViewInit() {
+        const pinnedIcons = icons.filter(icon => icon.name === "pin" || icon.name === "unpin");
+        pinnedIcons.forEach(icon => {
+            if (!this.iconService.isSvgIconCached(icon.name, FILTERING_ICONS_FONT_SET)) {
+                this.iconService.addSvgIconFromText(icon.name, icon.value, FILTERING_ICONS_FONT_SET);
+            }
+        });
     }
 
     public togglePinning(row: IgxGridRowComponent, event) {
