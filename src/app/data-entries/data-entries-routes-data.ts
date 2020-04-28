8510bee6d588a2a3801dff425f55d41adb037726
Silvia Ivanova <SIvanova@infragistics.com>
2020-04-28 12:01:33 +0300

    refactor(radio): improve radio & radio group samples

--- src/app/data-entries/data-entries-routes-data.ts
+++ src/app/data-entries/data-entries-routes-data.ts
@@ -44,6 +44,7 @@ export const dataEntriesRoutesData = {
     "radio-sample-1": { displayName: "Simple Radio Buttons", parentName: "Radio" },
     "radio-sample-2": { displayName: "Radio Button Binding", parentName: "Radio" },
     "radio-group-sample": { displayName: "Radio Group", parentName: "Radio" },
+    "radio-styling-sample": { displayName: "Styling Radio Buttons", parentName: "Radio" },
     "switch-sample-1": { displayName: "Switch Overview", parentName: "Switch" },
     "switch-sample-2": { displayName: "Multiple Switches", parentName: "Switch" },
     "select-sample-1": { displayName: "Simple Select", parentName: "Select" },
