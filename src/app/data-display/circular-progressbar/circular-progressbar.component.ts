c9f11c244de5681f21762ef6ae6653c4e68fe45b
didimmova <d.dimova11@gmail.com>
2020-05-12 13:24:59 +0300

    refactor(circular progress): remove unnecessary methods

--- src/app/data-display/circular-progressbar/circular-progressbar.component.ts
+++ src/app/data-display/circular-progressbar/circular-progressbar.component.ts
@@ -1,26 +1,8 @@
-import { Component, OnInit, ViewChild, ViewEncapsulation } from "@angular/core";
-import { IgxCircularProgressBarComponent } from "igniteui-angular";
+import { Component } from "@angular/core";
 
 @Component({
-  encapsulation: ViewEncapsulation.None,
   selector: "app-circular-progressbar",
   styleUrls: ["./circular-progressbar.component.scss"],
   templateUrl: "./circular-progressbar.component.html"
 })
-export class CircularProgressbarComponent implements OnInit {
-
-  public interval: any;
-
-  @ViewChild(IgxCircularProgressBarComponent, { static: true }) public circularBar: IgxCircularProgressBarComponent;
-
-  public ngOnInit() {
-    this.interval = setInterval(this.updateValue.bind(this), 60);
-  }
-
-  public updateValue() {
-     this.circularBar.value += 1;
-     if (this.circularBar.value === this.circularBar.max) {
-       this.interval = clearInterval(this.interval);
-     }
-  }
-}
+export class CircularProgressbarComponent { }
--- src/app/data-display/circular-progressbar/circular-styling-sample/circular-styling-sample.component.ts
+++ src/app/data-display/circular-progressbar/circular-styling-sample/circular-styling-sample.component.ts
@@ -5,24 +5,4 @@ import { Component } from "@angular/core";
     styleUrls: ["./circular-styling-sample.component.scss"],
     templateUrl: "./circular-styling-sample.component.html"
 })
-export class CircularStylingSampleComponent {
-    public currentValue: number;
-
-    public ngOnInit() {
-        this.currentValue = 0;
-    }
-
-    public addProgress() {
-        this.currentValue += 10;
-        if (this.currentValue > 100) {
-            this.currentValue = 100;
-        }
-    }
-
-    public removeProgress() {
-        this.currentValue -= 10;
-        if (this.currentValue < 0) {
-            this.currentValue = 0;
-        }
-    }
-}
+export class CircularStylingSampleComponent { }
