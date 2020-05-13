e179a8547a6e24e7868e8ebde1d7acd2d37f98ae
HristoP96 <HPopov@infragistics.com>
2020-05-13 16:08:41 +0300

    initial implementation

---
+++ projects/app-lob/src/app/grid-dynamic-chart-data/data-analysis-dock-manager/data-analysis-dock-manager.component.ts
@@ -0,0 +1,221 @@
+// tslint:disable: max-line-length
+import { ChangeDetectorRef, Component, HostListener, OnInit, ViewChild } from "@angular/core";
+import { AutoPositionStrategy, CloseScrollStrategy, HorizontalAlignment, IgxDialogComponent, IgxGridComponent, IgxOverlayOutletDirective, IgxTabsComponent, VerticalAlignment, IgxButtonDirective } from "igniteui-angular";
+import { noop, Subject } from "rxjs";
+import { debounceTime, takeUntil, tap } from "rxjs/operators";
+import { FinancialData } from "../../services/financialData";
+import { ChartHostDirective, ChartIntegrationDirective, IDeterminedChartTypesArgs } from "../directives/chart-integration/chart-integration.directive";
+import { CHART_TYPE } from "../directives/chart-integration/chart-types";
+import { ConditionalFormattingDirective } from "../directives/conditional-formatting/conditional-formatting.directive";
+@Component({
+  selector: "app-data-analysis-dock-manager",
+  templateUrl: "./data-analysis-dock-manager.component.html",
+  styleUrls: ["./data-analysis-dock-manager.component.scss"]
+})
+export class DataAnalysisDockManagerComponent implements OnInit {
+
+    public data;
+    @ViewChild(ConditionalFormattingDirective, { read: ConditionalFormattingDirective, static: true })
+    public formatting: ConditionalFormattingDirective;
+
+    @ViewChild(ChartIntegrationDirective, {read: ChartIntegrationDirective, static: true})
+    public chartIntegration: ChartIntegrationDirective;
+
+    @ViewChild("grid", { read: IgxGridComponent, static: true })
+    public grid: IgxGridComponent;
+
+    @ViewChild("chart", { read: ChartHostDirective, static: true })
+    public chartHost: ChartHostDirective;
+
+    @ViewChild(IgxOverlayOutletDirective, { static: true })
+    public outlet: IgxOverlayOutletDirective;
+
+    @ViewChild("contextDialog", { static: true })
+    public contextDialog: IgxDialogComponent;
+
+    @ViewChild(IgxTabsComponent, { static: true })
+    public tabs: IgxTabsComponent;
+
+    public chartData = [];
+    public contextmenu = false;
+    public contextmenuX = 0;
+    public contextmenuY = 0;
+    public currentChartType: CHART_TYPE = CHART_TYPE.COLUMN_GROUPED;
+    public range;
+    public currentFormatter;
+
+    protected destroy$ = new Subject<any>();
+    private _contextDilogOverlaySettings = {
+        closeOnOutsideClick: true,
+        modal: false,
+        outlet: null,
+        scrollStrategy: new CloseScrollStrategy(),
+        positionStrategy: null
+    };
+
+    private rowIndex;
+    private colIndex;
+
+    constructor(private cdr: ChangeDetectorRef) {
+    }
+
+    public ngOnInit() {
+        (this.tabs.headerContainer.nativeElement as HTMLElement).onpointerdown = event => event.stopPropagation();
+
+        this.contextDialog.onOpen.subscribe(() => {
+            this.currentChartType = CHART_TYPE.COLUMN_GROUPED;
+        });
+
+        this.data = new FinancialData().generateData(1000);
+
+        this.grid.onRangeSelection.pipe(tap(() => this.contextmenu ? this.disableContextMenu() : noop()), debounceTime(200))
+            .subscribe(range => {
+                this.chartData = this.grid.getSelectedData();
+                this.range = range;
+                this.tabs.tabs.first.isSelected = true;
+                this.renderButton();
+                const btn = document.getElementsByClassName('analytics-btn')[0];
+                setTimeout(() => {
+                    this.toggleContextDialog(btn);
+                });
+            });
+    }
+
+    public ngAfterViewInit(): void {
+        this.chartIntegration.onChartTypesDetermined.subscribe((args: IDeterminedChartTypesArgs) => {
+            if (args.chartsAvailabilty.size === 0 || args.chartsForCreation.length === 0) {
+            } else {
+                args.chartsAvailabilty.forEach((isAvailable, chart) => {
+                    if (args.chartsForCreation.indexOf(chart) === -1) {
+                        this.chartIntegration.disableCharts([chart]);
+                    } else {
+                        this.chartIntegration.enableCharts([chart]);
+                    }
+                });
+                this.availableCharts = this.chartIntegration.getAvailableCharts();
+            }
+        });
+        this.formatting.onFormattersReady.pipe(takeUntil(this.destroy$)).subscribe(names => this.formattersNames = names);
+        this.grid.onCellClick.pipe(takeUntil(this.destroy$)).subscribe(() => this.range = undefined);
+        this.grid.onDataPreLoad.pipe(
+            tap(() => this.contextmenu ? this.disableContextMenu() : noop()),
+            debounceTime(250),
+            takeUntil(this.destroy$))
+            .subscribe(() => this.range && !this.contextmenu ? this.renderButton() : noop());
+        this.grid.parentVirtDir.onChunkLoad.pipe(
+            tap(() => this.contextmenu ? this.disableContextMenu() : noop()),
+            debounceTime(250),
+            takeUntil(this.destroy$))
+            .subscribe(() => {
+                if (this.range) { this.renderButton(); }
+            });
+    }
+
+    public ngOnDestroy(): void {
+        this.destroy$.next(true);
+        this.destroy$.complete();
+    }
+
+    public formatCurrency(value: number) {
+        return "$" + value.toFixed(3);
+    }
+
+    // tslint:disable: member-ordering
+    public chartTypesMenuX;
+    public chartTypesMenuY;
+
+    public availableCharts: CHART_TYPE[] = [];
+    public chartTypes = ["Column", "Area", "Bar", "Line", "Scatter", "Pie"];
+
+    public toggleContextDialog(btn) {
+        if (!this.contextDialog.isOpen) {
+            this._contextDilogOverlaySettings.outlet = this.outlet;
+            const positionStrategy = {
+                verticalStartPoint: VerticalAlignment.Bottom,
+                target: btn
+            };
+
+            if (((this.grid.visibleColumns.length - 1) - this.colIndex) < 2 || !this.grid.navigation.isColumnFullyVisible(this.colIndex + 1)) {
+                positionStrategy["horizontalDirection"] = HorizontalAlignment.Left;
+                positionStrategy["horizontalStartPoint"] = HorizontalAlignment.Right;
+            } else {
+                positionStrategy["horizontalDirection"] = HorizontalAlignment.Center;
+                positionStrategy["horizontalStartPoint"] = HorizontalAlignment.Center;
+            }
+            this._contextDilogOverlaySettings.positionStrategy = new AutoPositionStrategy({ ...positionStrategy });
+            this.contextDialog.open(this._contextDilogOverlaySettings);
+        } else {
+            this.contextDialog.close();
+        }
+    }
+
+    public formattersNames = [];
+
+    public createChart(type: CHART_TYPE, host: ChartHostDirective, dialog: IgxDialogComponent, overlaySettings: any) {
+        const chartHost = host;
+        const dialogToOpen = dialog;
+        const dialogOverlaySettings = overlaySettings;
+        this.currentChartType = type;
+        chartHost.viewContainerRef.clear();
+        this.chartIntegration.chartFactory(type, chartHost.viewContainerRef);
+        if (dialogToOpen.isCollapsed) {
+            dialogOverlaySettings.outlet = this.outlet;
+            dialogToOpen.open(overlaySettings);
+        }
+    }
+
+    public disableContextMenu() {
+        this.contextmenu = false;
+        this.contextDialog.close();
+    }
+    // What we check here and why we need a lister on host level
+    @HostListener("pointerdown", ["$event"])
+    public onPointerDown(event) {
+        if (!event.target.parentElement.classList.contains("analytics-btn") &&
+            event.target.className.indexOf("btn") === -1 &&
+            event.target.className.indexOf("action") === -1 &&
+            event.target.className.indexOf("tab-option") === -1) {
+            this.disableContextMenu();
+        }
+    }
+
+    public analyse(condition) {
+        this.currentFormatter = condition;
+        this.formatting.formatCells(condition);
+    }
+
+    public clearFormatting() {
+        this.formatting.clearFormatting();
+    }
+
+    private renderButton() {
+        this.rowIndex = this.range.rowEnd;
+        this.colIndex = this.range.columnEnd;
+
+        while (this.colIndex >= 0 && !this.grid.navigation.isColumnFullyVisible(this.colIndex)) {
+            this.colIndex--;
+        }
+
+        if (this.colIndex < 0) {
+            return;
+        }
+
+        let cell;
+        if ((!this.grid.getRowByIndex(this.rowIndex) || (this.grid.rowList.toArray().indexOf(this.grid.getRowByIndex(this.rowIndex)) >= this.grid.rowList.length - 2) && this.rowIndex + 2 < this.grid.dataLength)) {
+            const lastFullyVisibleRowIndex = this.grid.rowList.toArray()[this.grid.rowList.length - 3].index;
+            const field = this.grid.visibleColumns[this.colIndex].field;
+            cell = this.grid.getCellByColumn(lastFullyVisibleRowIndex, field);
+        } else {
+            cell = this.grid.getCellByColumn(this.rowIndex, this.grid.visibleColumns[this.colIndex].field);
+        }
+        this.contextmenuX = cell.element.nativeElement.getClientRects()[0].right;
+        this.contextmenuY = cell.element.nativeElement.getClientRects()[0].bottom;
+        this.contextmenu = this.isWithInRange(cell.rowIndex, cell.visibleColumnIndex);
+        this.cdr.detectChanges();
+    }
+
+    public isWithInRange(rowIndex, colIndex) {
+        return rowIndex >= this.range.rowStart && rowIndex <= this.range.rowEnd
+        && colIndex >= this.range.columnStart && colIndex <= this.range.columnEnd;
+    }
+}
--- projects/app-lob/src/app/grid-dynamic-chart-data/grid-dynamic-chart-data.module.ts
+++ projects/app-lob/src/app/grid-dynamic-chart-data/grid-dynamic-chart-data.module.ts
@@ -1,14 +1,16 @@
-import { NgModule } from "@angular/core";
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from "@angular/core";
 import { IgxButtonModule, IgxDialogModule, IgxDividerModule, IgxGridModule, IgxTabsModule } from "igniteui-angular";
 import { IgxBarSeriesModule, IgxCategoryChartModule, IgxCategoryXAxisModule,
-    IgxDataChartCategoryModule, IgxDataChartComponent, IgxDataChartCoreModule,
-    IgxDataChartInteractivityModule, IgxDataChartScatterModule, IgxDataChartStackedModule,
-    IgxItemLegendComponent, IgxItemLegendModule, IgxLegendComponent, IgxLegendModule,
-    IgxNumericXAxisModule, IgxNumericYAxisModule, IgxPieChartComponent } from "igniteui-angular-charts";
+         IgxDataChartCategoryModule, IgxDataChartComponent, IgxDataChartCoreModule,
+         IgxDataChartInteractivityModule, IgxDataChartScatterModule,
+         IgxDataChartStackedModule, IgxItemLegendComponent, IgxItemLegendModule,
+         IgxLegendComponent, IgxLegendModule, IgxNumericXAxisModule,
+         IgxNumericYAxisModule, IgxPieChartComponent } from "igniteui-angular-charts";
 import { IgxPieChartModule } from "igniteui-angular-charts/";
 import { IgxPreventDocumentScrollModule } from "../directives/prevent-scroll.directive";
-import { ChartHostDirective,
-    ChartIntegrationDirective } from "./directives/chart-integration/chart-integration.directive";
+import { DataAnalysisDockManagerComponent } from "./data-analysis-dock-manager/data-analysis-dock-manager.component";
+// tslint:disable-next-line: max-line-length
+import { ChartHostDirective, ChartIntegrationDirective } from "./directives/chart-integration/chart-integration.directive";
 import { ConditionalFormattingDirective } from "./directives/conditional-formatting/conditional-formatting.directive";
 import { GridDynamicChartDataRoutingModule } from "./grid-dynamic-chart-data-routing.module";
 import { FilterTypePipe, GridDynamicChartDataComponent, NamePipe } from "./grid-dynamic-chart-data.component";
@@ -20,7 +22,8 @@ import { FilterTypePipe, GridDynamicChartDataComponent, NamePipe } from "./grid-
         ConditionalFormattingDirective,
         ChartIntegrationDirective,
         NamePipe,
-        FilterTypePipe
+        FilterTypePipe,
+        DataAnalysisDockManagerComponent
     ],
     imports: [
         IgxButtonModule,
@@ -46,6 +49,7 @@ import { FilterTypePipe, GridDynamicChartDataComponent, NamePipe } from "./grid-
         GridDynamicChartDataRoutingModule
     ],
     providers: [],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA],
     entryComponents: [
         IgxDataChartComponent,
         IgxItemLegendComponent,
