7fcdff7dff95b02527656b4051f4e4fffea5ed40
ViktorSlavov <viktorslavov91@gmail.com>
2020-06-11 16:10:06 +0300

    fix(select): adjust select item content display to account for lib change

--- src/app/data-entries/select/select-sample-2/select-sample-2.component.ts
+++ src/app/data-entries/select/select-sample-2/select-sample-2.component.ts
@@ -1,6 +1,6 @@
 
-import { Component, ViewChild } from "@angular/core";
-import { IgxSelectComponent, IgxSelectItemComponent } from "igniteui-angular";
+import { Component, ElementRef, ViewChild } from "@angular/core";
+import { IgxSelectComponent } from "igniteui-angular";
 
 @Component({
     selector: "select-sample-2",
@@ -10,28 +10,31 @@ import { IgxSelectComponent, IgxSelectItemComponent } from "igniteui-angular";
 export class SelectSample2Component {
     @ViewChild(IgxSelectComponent, { static: true })
     public select: IgxSelectComponent;
-
+    
     public greengrocery: Array<{
         label: string,
         items: Array<{ type: string, origin: string }>
     }> = [
-            {
-                label: "Fruits",
-                items:
-                    [
-                        { type: "Apple", origin: "local" },
-                        { type: "Orange", origin: "import" },
-                        { type: "Banana", origin: "import"}
-                    ]
-            },
-            {
-                label: "Vegetables",
-                items:
-                    [
-                        { type: "Cucumber", origin: "local" },
-                        { type: "Potato", origin: "import" },
-                        { type: "Pepper", origin: "local" }
-                    ]
-            }
-        ];
+        {
+            label: "Fruits",
+            items:
+            [
+                { type: "Apple", origin: "local" },
+                { type: "Orange", origin: "import" },
+                { type: "Banana", origin: "import"}
+            ]
+        },
+        {
+            label: "Vegetables",
+            items:
+            [
+                { type: "Cucumber", origin: "local" },
+                { type: "Potato", origin: "import" },
+                { type: "Pepper", origin: "local" }
+            ]
+        }
+    ];
+
+    constructor(public element: ElementRef) {
+    }
 }
