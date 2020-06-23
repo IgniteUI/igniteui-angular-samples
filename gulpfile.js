4fbf9d66e7b3e8aed3066f7f9f5c5e0270d77700
HristoP96 <HPopov@infragistics.com>
2020-06-23 19:04:17 +0300

    add app lob repositoryfy task

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,7 @@ const tsNode = require('ts-node').register({
 });
 const argv = require("yargs").argv;
 
+const submodule = "test-angular-samples";
 
 const window = domino.createWindow('<!doctype html><html><body></body></html>');
 global.HTMLElement = window.HTMLElement;
@@ -59,9 +60,106 @@ gulp.task("sass-js-compile-check", async() => {
     var checker = requireFile("./live-editing/services/SassJsCompileChecker.ts");
     await checker.sassJsCompileChecker.run();
 });
-const excludedDirectories = ["data", "index", "assets", "environment"];
-const repositoryfy = (projectPath) => {
+
+const excludedDirectories = ["index", "assets", "environment"];
+
+const getSampleBaseDir = (directories, fileName) =>  directories.find(d => {
+    return fileName.indexOf(d + "-") !== -1 && fileName.indexOf(d + "-") === 0;
+});
+
+const getSampleNameFromFileName = (fileName, sampleBaseDir) => fileName.replace(sampleBaseDir + "-", "");
+var assetsRegex = new RegExp("\/?assets\/", "g");
+
+const repositoryfyAppLob = (projectPath) => {
+    if(!fs.existsSync(submodule)) {
+        return console.error("No submodule found");
+    }
+    const appLobExplicitSampleBasesStructure = {
+        "finjs-sample": "",
+        "grid-crm": "",
+        "grid-grid": "grid",
+        "grid-grid-master-detail": "grid",
+        "treegrid-finjs-sample": ""
+    }
+    
     const directories = [];
+    const appLobexcludedDirectories = excludedDirectories.concat("services");
+    const appDir = fs.readdirSync(projectPath + "/app", "utf-8");
+    appDir.filter(dir => appLobexcludedDirectories.indexOf(dir) === -1 )
+          .forEach(child => {
+            if(fs.lstatSync(`${projectPath + "/app"}/${child}`).isDirectory()) {
+                directories.push(child);
+            }
+    });
+    const jsonSamplesPath = path.join(__dirname, `${projectPath}/assets/samples`);
+    const sharedJson = JSON.parse(fs.readFileSync(jsonSamplesPath + "/shared.json"));
+    const devDependencies = JSON.parse(fs.readFileSync("package.json")).devDependencies;
+    const submoduleAppDest = submodule + "/angular-demos-lob/";
+    if(!fs.existsSync(submoduleAppDest)) {
+        fs.mkdirSync(submoduleAppDest);
+    }
+
+    return gulp.src([`${jsonSamplesPath}/*.json`,`!${jsonSamplesPath}/shared.json`, `!${jsonSamplesPath}/meta.json`])
+               .pipe(es.map((file, cb) => {
+                   console.log(file.path);
+                    fs.readFile(file.path, 'utf-8', (err, content) => {
+                        // Adjust sample application bundle files
+                        const jsonObj = JSON.parse(content);
+                        const packageJson = {
+                            "path": "package.json",
+                            "hasRelativeAssetsUrls": false,
+                            "content": JSON.stringify({"dependencies": JSON.parse(jsonObj.sampleDependencies),
+                                                       "devDependencies": devDependencies})
+                        }
+                        jsonObj.sampleFiles = jsonObj.sampleFiles.concat(sharedJson.files).concat(packageJson);
+
+                        // Configure sample application file structure
+                        const fileName = file.path.substring(file.base.length + 1).replace(".json", "");
+                        const sampleBaseDir = fileName in appLobExplicitSampleBasesStructure ?
+                                              appLobExplicitSampleBasesStructure[fileName] : getSampleBaseDir(directories, fileName);
+
+                        if(sampleBaseDir && !fs.existsSync(submoduleAppDest + sampleBaseDir)) {
+                            fs.mkdirSync(submoduleAppDest + sampleBaseDir);
+                        }
+                        const sampleName = sampleBaseDir ? getSampleNameFromFileName(fileName, sampleBaseDir) : fileName;
+                        const sampleAppPath = submoduleAppDest + sampleBaseDir + "/" + sampleName;
+                        if(!fs.existsSync(sampleAppPath)) {
+                            fs.mkdirSync(sampleAppPath);
+                        }
+
+                        // Distribute Sample Files
+                        jsonObj.sampleFiles.forEach(sampleFile => {
+                            let sampleContent;
+                            const assetsUrl = "https://www.infragistics.com/angular-demos-lob/assets/";
+                            if(sampleFile.hasRelativeAssetsUrls) {
+                                sampleContent = sampleFile.content.replace(assetsRegex, assetsUrl);
+                            } else {
+                                sampleContent = sampleFile.content;
+                            }
+                            const paths = sampleFile.path.replace("./", "").split("/");
+                            let tempPath = "";
+                            paths.forEach(p => {
+                                tempPath += p + "/";
+                                if(p.indexOf(".") !== -1) {
+                                    fs.writeFileSync(sampleAppPath + "/" + tempPath, sampleContent);
+                                } else
+                                if(!fs.existsSync(sampleAppPath + "/" + tempPath)) {
+                                    fs.mkdirSync(sampleAppPath + "/" + tempPath)
+                                }
+                            })
+                        });
+                        cb(null, file);
+                    })
+               }))
+               .on("error", () => console.log(err));
+}
+
+const repositoryfy = (projectPath) => {
+    if(!fs.existsSync(submodule)) {
+        return console.error("No submodule found");
+    }
+    const directories = [];
+    excludedDirectories.push("data");
     const appDir = fs.readdirSync(projectPath + "/app", "utf-8");
     appDir.filter(dir => excludedDirectories.indexOf(dir) === -1 )
           .forEach(child => {
@@ -72,44 +170,58 @@ const repositoryfy = (projectPath) => {
     const jsonSamplesPath = path.join(__dirname, `${projectPath}/assets/samples`);
     const sharedJson = JSON.parse(fs.readFileSync(jsonSamplesPath + "/shared.json"));
     const devDependencies = JSON.parse(fs.readFileSync("package.json")).devDependencies;
+    const submoduleAppDest = submodule + "/angular-demos/";
+    if(!fs.existsSync(submoduleAppDest)) {
+        fs.mkdirSync(submoduleAppDest);
+    }
+
     return gulp.src([`${jsonSamplesPath}/*.json`,`!${jsonSamplesPath}/shared.json`, `!${jsonSamplesPath}/meta.json`])
                .pipe(es.map((file, cb) => {
                    console.log(file.path);
                     fs.readFile(file.path, 'utf-8', (err, content) => {
+                        // Adjust sample application bundle files
                         const jsonObj = JSON.parse(content);
                         const packageJson = {
                             "path": "package.json",
                             "hasRelativeAssetsUrls": false,
-                            "content": JSON.stringify({"dependencies": JSON.parse(jsonObj.sampleDependencies), "devDependencies": devDependencies})
+                            "content": JSON.stringify({"dependencies": JSON.parse(jsonObj.sampleDependencies),
+                                                       "devDependencies": devDependencies})
                         }
                         jsonObj.sampleFiles = jsonObj.sampleFiles.concat(sharedJson.files).concat(packageJson);
-                        const fileName = file.path.substring(file.base.length + 1);
-                        const sampleBase = directories.find(d => {
-                            return fileName.indexOf(d + "-") !== -1 && fileName.indexOf(d + "-") === 0;
-                        })
-                        if(!fs.existsSync("test-angular-samples/samples/" + sampleBase)) {
-                            fs.mkdirSync("test-angular-samples/samples/" + sampleBase);
+
+                        // Configure sample application file structure
+                        const fileName = file.path.substring(file.base.length + 1).replace(".json", "");
+                        const sampleBaseDir = getSampleBaseDir(directories, fileName);
+                        if(!fs.existsSync(submoduleAppDest + sampleBaseDir)) {
+                            fs.mkdirSync(submoduleAppDest + sampleBaseDir);
                         }
-                        const sampleName = fileName.replace(sampleBase + "-", "").replace(".json", "");
-                        const sampleAppPath = "test-angular-samples/samples/" + sampleBase + "/" + sampleName;
+                        const sampleName = getSampleNameFromFileName(fileName, sampleBaseDir);
+                        const sampleAppPath = submoduleAppDest + sampleBaseDir + "/" + sampleName;
                         if(!fs.existsSync(sampleAppPath)) {
                             fs.mkdirSync(sampleAppPath);
                         }
 
+                        // Distribute Sample Files
                         jsonObj.sampleFiles.forEach(sampleFile => {
+                            let sampleContent;
+                            const assetsUrl = "https://www.infragistics.com/angular-demos/assets/";
+                            if(sampleFile.hasRelativeAssetsUrls) {
+                                sampleContent = sampleFile.content.replace(assetsRegex, assetsUrl);
+                            } else {
+                                sampleContent = sampleFile.content;
+                            }
                             const paths = sampleFile.path.replace("./", "").split("/");
                             let tempPath = "";
                             paths.forEach(p => {
                                 tempPath += p + "/";
                                 if(p.indexOf(".") !== -1) {
-                                    fs.writeFileSync(sampleAppPath + "/" + tempPath, sampleFile.content);
+                                    fs.writeFileSync(sampleAppPath + "/" + tempPath, sampleContent);
                                 } else
                                 if(!fs.existsSync(sampleAppPath + "/" + tempPath)) {
                                     fs.mkdirSync(sampleAppPath + "/" + tempPath)
                                 }
                             })
                         });
-
                         cb(null, file);
                     })
                }))
@@ -118,4 +230,8 @@ const repositoryfy = (projectPath) => {
 
 gulp.task("repositoryfy", () => {
     return repositoryfy("src")
+})
+
+gulp.task("repositoryfyAppLob", () => {
+    return repositoryfyAppLob("projects/app-lob/src")
 })
