4113d2dbc94fef326041f6c225b781c086fdf6d4
HristoP96 <HPopov@infragistics.com>
2020-07-28 13:49:16 +0300

    Adding sorting indicators sample

--- src/app/data/utils.ts
+++ src/app/data/utils.ts
@@ -4,5 +4,6 @@
  * @param end upper range (inclusive)
  */
 const generateRandomInteger = (start: number, end: number) => Math.floor(Math.random() * (end - start + 1)) + start;
+const generateRandomFloat = (start: number, end: number) => Math.random() * (end - start) + start;
 
-export { generateRandomInteger };
+export { generateRandomInteger, generateRandomFloat };
---
+++ src/app/grid/grid-sorting-indicators/grid-sorting-indicators.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { IgxGridComponent, DefaultSortingStrategy } from 'igniteui-angular';
+import { FinancialData } from '../services/financialData';
+import  {generateRandomInteger, generateRandomFloat} from '../../data/utils';
+@Component({
+  selector: 'app-grid-sorting-indicators',
+  templateUrl: './grid-sorting-indicators.component.html',
+  styleUrls: ['./grid-sorting-indicators.component.scss']
+})
+export class GridSortingIndicatorsComponent implements OnInit, AfterViewInit {
+
+    @ViewChild("grid1", { static: true }) public grid: IgxGridComponent;
+    public data;
+
+    public ngOnInit(): void {
+        const typeArr = ["Gold", "Silver", "Coal"];
+        this.data = new FinancialData().generateData(1000).map(dataObj => {
+                const type = typeArr[generateRandomInteger(0, 2)];
+                switch (type) {
+                    case "Gold":
+                        dataObj["Type"] = "Gold";
+                        dataObj["Price"] = generateRandomFloat(1261.78, 1302.76);
+                        dataObj["Buy"] = generateRandomFloat(1261.78, 1280.73);
+                        break;
+                    case "Silver":
+                        dataObj["Type"] = "Silver";
+                        dataObj["Price"] = generateRandomFloat(17.12, 17.73);
+                        dataObj["Buy"] = generateRandomFloat(17.12, 17.43);
+                        break;
+                    case "Coal":
+                        dataObj["Type"] = "Coal";
+                        dataObj["Price"] = generateRandomFloat(0.40, 0.42);
+                        dataObj["Buy"] = generateRandomFloat(0.42, 0.46);
+                        break;
+                }
+                return dataObj;
+        });
+    }
+
+    public ngAfterViewInit() {
+        this.grid.columns.forEach(c => {
+            const sortExpr =
+            {
+                dir: generateRandomInteger(1, 2), fieldName: c.field,
+                ignoreCase: true, strategy: DefaultSortingStrategy.instance()
+            }
+            this.grid.sortingExpressions.push(sortExpr)
+        });
+        this.grid.cdr.detectChanges();
+    }
+    public formatCurrency(value: number) {
+        return "$" + value.toFixed(2);
+    }
+
+}
