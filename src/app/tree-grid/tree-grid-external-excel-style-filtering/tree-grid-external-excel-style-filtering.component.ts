5bbdfde21bc058158bdc0c18c58e8919f8b01406
IBarakov <ibarakov@icloud.com>
2020-08-05 16:03:34 +0300

    fix(tree-grid-external-esf-sample): fix ExpressionChangedAfterItHasBeenCheckedError

--- src/app/tree-grid/tree-grid-external-excel-style-filtering/tree-grid-external-excel-style-filtering.component.ts
+++ src/app/tree-grid/tree-grid-external-excel-style-filtering/tree-grid-external-excel-style-filtering.component.ts
@@ -1,15 +1,26 @@
-import { Component } from "@angular/core";
+import { Component, ViewChild, ChangeDetectionStrategy } from "@angular/core";
 import { FOODS_DATA } from "../data/foods";
+import { IgxTreeGridComponent } from 'igniteui-angular';
 
 @Component({
+    changeDetection: ChangeDetectionStrategy.OnPush,
     selector: "tree-grid-external-excel-style-filtering",
     templateUrl: "./tree-grid-external-excel-style-filtering.component.html",
     styleUrls: ["./tree-grid-external-excel-style-filtering.component.scss"]
 })
 export class TreeGridExternalExcelStyleFilteringComponent {
 
-    public data: any;
+    @ViewChild("tGrid", { read: IgxTreeGridComponent, static: true })
+    public tGrid: IgxTreeGridComponent;
+
+    public columns: any[];
+    public data: any[];
+
     constructor() {
         this.data = FOODS_DATA();
+    }
+
+    public ngAfterViewInit() {
+        this.columns = this.tGrid.columns.filter(c => c.filterable);
     }
 }
