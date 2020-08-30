ce6058dd3368505e2e0dd19e823b3f8128ca3e64
IvayloG <inj.iganchev@gmail.com>
2020-08-30 13:33:55 +0300

    feat(input-group): Update sample to use token
     - InputGroupToken

--- src/app/data-entries/input-group/input-group-sample-5/input-group-sample-5.component.ts
+++ src/app/data-entries/input-group/input-group-sample-5/input-group-sample-5.component.ts
@@ -1,9 +1,11 @@
 import { Component } from "@angular/core";
 import { BaseInputGroupSampleComponent } from "../base-input.component";
+import { InputGroupToken } from 'igniteui-angular';
 
 @Component({
     selector: "app-input-group-sample-5",
     styleUrls: ["./input-group-sample-5.component.scss"],
-    templateUrl: "./input-group-sample-5.component.html"
+    templateUrl: "./input-group-sample-5.component.html",
+    providers: [{ provide: InputGroupToken, useValue: 'box' }]
 })
 export class InputGroupSample5Component extends BaseInputGroupSampleComponent { }
