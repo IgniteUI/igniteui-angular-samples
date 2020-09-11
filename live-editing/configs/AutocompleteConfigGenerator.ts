79938ace45ee501cb5dd76154fb15b1810e71245
hpopov <Hpopov@infragistics.com>
2020-09-11 14:21:21 +0300

    resolve conflicts vol 2

--- live-editing/configs/AutocompleteConfigGenerator.ts
+++ live-editing/configs/AutocompleteConfigGenerator.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import {IgxAutocompleteModule,
 IgxDropDownModule,
 IgxInputGroupModule} from 'igniteui-angular';
@@ -8,14 +7,6 @@ import {IConfigGenerator} from './core/IConfigGenerator';
 export class AutocompleteConfigGenerator implements IConfigGenerator {
     public additionalImports = {
         AutocompletePipeStartsWith: '../../src/app/data-entries/autocomplete/autocomplete/autocomplete.component'
-=======
-import { AppModuleConfig } from "./core/AppModuleConfig";
-import { Config } from "./core/Config";
-import { ConfigGenerator} from "./core/IConfigGenerator";
-export class AutocompleteConfigGenerator extends ConfigGenerator{
-    public additionalImports = {
-        'AutocompletePipeStartsWith': '../../src/app/data-entries/autocomplete/autocomplete/autocomplete.component'
->>>>>>> 5835f0692b85fd2fa8a274f2a973ea5b958cb772
 };
     public generateConfigs(): Config[] {
         const configs = new Array<Config>();
