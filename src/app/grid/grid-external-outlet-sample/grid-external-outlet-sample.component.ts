be48f2db55baef06b11b26a68d19775a0c7771b8
IBarakov <ibarakov@infragistics.com>
2020-09-14 12:23:38 +0300

    sample(external-outlet): use input binding instead of setting it in the code

--- src/app/grid/grid-external-outlet-sample/grid-external-outlet-sample.component.ts
+++ src/app/grid/grid-external-outlet-sample/grid-external-outlet-sample.component.ts
@@ -9,23 +9,14 @@ import { DATA } from "../../data/nwindData";
 })
 
 export class GridExternalOutletComponent implements OnInit {
-    @ViewChild("grid1", { read: IgxGridComponent, static: true })
-    public grid1: IgxGridComponent;
-
-    @ViewChild("grid2", { read: IgxGridComponent, static: true })
-    public grid2: IgxGridComponent;
-
     @ViewChild('igxFilteringOverlayOutlet', { read: IgxOverlayOutletDirective, static: true })
-    public _outletDirective: IgxOverlayOutletDirective;
+    public outletDirective: IgxOverlayOutletDirective;
 
     public data: any[];
 
     constructor() {
     }
     public ngOnInit(): void {
-        this.grid1.outlet = this._outletDirective;
-        this.grid2.outlet = this._outletDirective;
-
         this.data = DATA;
     }
 }
