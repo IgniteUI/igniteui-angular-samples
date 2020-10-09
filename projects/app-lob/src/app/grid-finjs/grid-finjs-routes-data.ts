795d237a751f3892d97365fa5e725e18cd1f65e2
hpopov <Hpopov@infragistics.com>
2020-10-09 16:50:51 +0300

    Adding new finjs sample (master)

--- projects/app-lob/src/app/grid-finjs/grid-finjs-routes-data.ts
+++ projects/app-lob/src/app/grid-finjs/grid-finjs-routes-data.ts
@@ -1,3 +1,4 @@
 export const gridfinjsRoutesData = {
-    "empty-path": { displayName: "Grid Finance demo sample", parentName: "Grid" }
+    "empty-path": { displayName: "Grid Finance demo sample", parentName: "Grid"},
+    "grid-finjs-partial": { displayName: "Grid Finance partial demo sample", parentName: "Grid" }
 };
