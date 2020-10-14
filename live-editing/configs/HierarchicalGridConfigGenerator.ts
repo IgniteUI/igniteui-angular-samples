0dda7a2cbf51351a50c430d5ed3bb99e555801d5
Zdravko Kolev <ZKolev@infragistics.com>
2020-10-14 12:14:59 +0300

    chore(*): Use hGrid module instead of Grid

--- live-editing/configs/HierarchicalGridConfigGenerator.ts
+++ live-editing/configs/HierarchicalGridConfigGenerator.ts
@@ -753,9 +753,9 @@ export class HierarchicalGridConfigGenerator implements IConfigGenerator {
         configs.push(new Config({
             additionalFiles: ["/src/app/directives/prevent-scroll.directive.ts", "/src/app/hierarchical-grid/data.ts"],
             appModuleConfig: new AppModuleConfig({
-                imports: ['IgxGridModule', 'IgxPreventDocumentScrollModule', 'HGridEditingLifecycleComponent', 'IgxSwitchModule'],
+                imports: ['IgxHierarchicalGridModule', 'IgxPreventDocumentScrollModule', 'HGridEditingLifecycleComponent', 'IgxSwitchModule'],
                 ngDeclarations: ['HGridEditingLifecycleComponent'],
-                ngImports: ['IgxPreventDocumentScrollModule', 'IgxGridModule', 'IgxSwitchModule'],
+                ngImports: ['IgxPreventDocumentScrollModule', 'IgxHierarchicalGridModule', 'IgxSwitchModule'],
                 ngProviders: []
             }),
             component: 'HGridEditingLifecycleComponent'
