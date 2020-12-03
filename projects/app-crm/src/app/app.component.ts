507ca594de9f31f60b07cf5e914dee247d7b2f8a
dobromirts <dobromir.ts@gmail.com>
2020-12-03 14:08:16 +0200

    Add page title and meta description -vnext

--- projects/app-crm/src/app/app.component.ts
+++ projects/app-crm/src/app/app.component.ts
@@ -1,9 +1,18 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { SEOService } from '../../../app-lob/src/app/seo.service';
 @Component({
     selector: "app-root",
     styleUrls: ["./app.component.scss"],
     templateUrl: "./app.component.html"
 })
-export class AppComponent {
-    constructor() {}
+export class AppComponent implements OnInit{
+    private title: string = "Business propeller - Angular Data Grid Example";
+    private metaDesc: string = "In this angular grid example, you can see how users can customize their data view by leveraging the various features built into the grid.";
+
+    constructor(private seoService: SEOService) {}
+
+    public ngOnInit() {
+        this.seoService.updateTitle(this.title);
+        this.seoService.updateDescription(this.metaDesc);
+    }
 }
