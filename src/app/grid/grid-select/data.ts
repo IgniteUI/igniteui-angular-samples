9e90be2a13173954c1d163529d40a26bcab7b105
valadzhov <valadzhov@infragistics.com>
2021-01-14 17:24:24 +0200

    Adding the sample.

---
+++ src/app/grid/grid-select/data.ts
@@ -0,0 +1,159 @@
+export const DATA: any[] = [{
+    name: "Stredo",
+    class: "Ranger",
+    age: "244",
+},
+{
+    name: "Haluun",
+    class: "Bard",
+    age: 40,
+},
+{
+    name: "Ivellios",
+    class: "Monk",
+    age: "244",
+},
+{
+    name: "Tes",
+    class: "Rogue",
+    age: 35,
+},
+{
+    name: "Kalla",
+    class: "Barbarian",
+    age: "47",
+},
+{
+    name: "Halimath Dundragon",
+    class: "Monk",
+    age: "149",
+},
+{
+    name: "Iriphawa",
+    class: "Fighter",
+    age: "39",
+},
+{
+    name: "Quaf",
+    class: "Wizard",
+    age: "25",
+},
+{
+    name: "Rat Scratch",
+    class: "Druid",
+    age: "15",
+},
+{
+    name: "Slicer",
+    class: "Cleric",
+    age: "57",
+},
+{
+    name: "Nereones Ahlorsath",
+    class: "Barbarian",
+    age: "95",
+},
+{
+    name: "Nalvarti Stonecutter",
+    class: "Monk",
+    age: "118",
+},
+{
+    name: "Errk",
+    class: "Monk",
+    age: "22",
+},
+{
+    name: "Seven Thundercloud",
+    class: "Paladin",
+    age: "43",
+},
+{
+    name: "Navarra Chergoba",
+    class: "Bard",
+    age: "16",
+},
+{
+    name: "Sail Snap",
+    class: "Sorcerer",
+    age: "56",
+},
+{
+    name: "Urreek",
+    class: "Paladin",
+    age: "17",
+},
+{
+    name: "Morkral Firetamer",
+    class: "Druid",
+    age: "24",
+},
+{
+    name: "Vithka",
+    class: "Rogue",
+    age: "53",
+},
+{
+    name: "Sandrue Avhoste",
+    class: "Bard",
+    age: "19",
+},
+{
+    name: "Hapah Moq",
+    class: "Paladin",
+    age: "34",
+},
+{
+    name: "Kothar ",
+    class: "Cleric",
+    age: "55",
+},
+{
+    name: "Senen",
+    class: "Rogue",
+    age: "40",
+},
+];
+
+export const ALIGNMENTS = [
+    "💖 Lawful good",
+    "❤️ Neutral good",
+    "🧡 Chaotic good",
+    "💛 Lawful neutral",
+    "💚 Neutral",
+    "💙 Chaotic Neutral",
+    "💜 Lawful evil",
+    "🤎 Neutral evil",
+    "🖤 Chaotic evil",
+    "🤍 Unaligned"
+]
+
+export const RACES = [
+    "🐲 Dragonborn",
+    "🎅🏽 Dwarf",
+    "🧝‍♀️ Elf",
+    "🧝🏻‍♂️ Half-Elf",
+    "🧒🏻 Hafling",
+    "🐗 Orc",
+    "👩 Human",
+    "🐢 Tortle",
+    "🤖 Warforged",
+    "🐡 Locathah",
+    "🎭 Changeling",
+    "🐯 Tabaxi"
+]
+
+export const CLASSES = [
+    "💪 Barbarian",
+    "🎻 Bard",
+    "❤️‍ Cleric",
+    "🍁 Druid",
+    "🥊 Fighter",
+    "🙏🏻 Monk",
+    "⚔️ Paladin",
+    "🏹 Ranger",
+    "🗡️ Rogue",
+    "🦹‍♂️ Sorcerer",
+    "🐉 Warlock",
+    "🧙‍♂️ Wizard"
+]
--- src/app/grid/grids.module.ts
+++ src/app/grid/grids.module.ts
@@ -120,6 +120,7 @@ import { GridMultiColumnHeadersStylingComponent } from "./multi-column-headers-s
 import { GridMultiColumnHeadersComponent } from "./multi-column-headers/multi-column-headers";
 import { GridSortingIndicatorsComponent } from './grid-sorting-indicators/grid-sorting-indicators.component';
 import { GridExportVisualizationComponent } from './grid-export-visualization/grid-export-visualization.component';
+import {GridSelectComponent} from './grid-select/grid-editing-sample.component';
 
 @NgModule({
     declarations: [
@@ -230,7 +231,8 @@ import { GridExportVisualizationComponent } from './grid-export-visualization/gr
         RemotePagingBatchEditingComponent,
         GridSortingIndicatorsComponent,
         GridExportVisualizationComponent,
-        GridEditingLifecycleComponent
+        GridEditingLifecycleComponent,
+        GridSelectComponent
     ],
     imports: [
         CommonModule,
