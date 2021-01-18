d4ee781a3da781aabbcdc96b7e7cf599231a9c22
valadzhov <valadzhov@infragistics.com>
2021-01-18 16:04:56 +0200

    Added configuration for live editing.

--- live-editing/configs/GridConfigGenerator.ts
+++ live-editing/configs/GridConfigGenerator.ts
@@ -1224,6 +1224,16 @@ export class GridConfigGenerator implements IConfigGenerator {
             })
         }));
 
+        configs.push(new Config({
+            component: 'GridSelectComponent',
+            additionalFiles: ["/src/app/directives/prevent-scroll.directive.ts", "/src/app/grid/grid-select/data.ts", "/src/app/grid/grid-select/characters.ts"],
+            appModuleConfig: new AppModuleConfig({
+                imports: ['IgxPreventDocumentScrollModule', 'IgxGridModule', 'IgxSelectModule', 'IgxDialogModule', 'GridSelectComponent' ],
+                ngDeclarations: ['GridSelectComponent'],
+                ngImports: ['IgxPreventDocumentScrollModule', 'IgxGridModule', 'IgxSelectModule', 'IgxDialogModule'],
+            })
+        }));
+
         return configs;
     }
 }
