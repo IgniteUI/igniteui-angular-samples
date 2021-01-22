d4618461c31fdc3cce9d22b64ca0b97cccdd37c5
valadzhov <valadzhov@infragistics.com>
2021-01-22 13:15:34 +0200

    Addressing the comments.

--- src/app/grid/grid-select/grid-select-sample.component.ts
+++ src/app/grid/grid-select/grid-select-sample.component.ts
@@ -1,6 +1,6 @@
 import { Character } from './characters';
 import { Component, OnInit, ViewChild } from "@angular/core";
-import { IgxDialogComponent, IgxGridComponent, IGridCellEventArgs } from "igniteui-angular";
+import { IgxGridComponent } from "igniteui-angular";
 import { DATA, ALIGNMENTS, RACES, CLASSES } from "./data";
 
 @Component({
@@ -12,16 +12,15 @@ export class GridSelectComponent implements OnInit {
 
     @ViewChild("grid1", { read: IgxGridComponent, static: true })
     public grid1: IgxGridComponent;
-    @ViewChild("dialogAdd", { read: IgxDialogComponent, static: true })
-    public dialog: IgxDialogComponent;
     public data;
     public alignments;
     public races;
     public classes;
     public character;
 
-    public ngOnInit() {
-        this.data = DATA.map((e) => {
+    public generateRandomData(data) {
+
+        return data.map((e) => {
             const indexAlignments = Math.floor(Math.random() * ALIGNMENTS.length);
             e.alignment = ALIGNMENTS[indexAlignments];
             const indexRaces = Math.floor(Math.random() * RACES.length);
@@ -29,25 +28,14 @@ export class GridSelectComponent implements OnInit {
             const indexClasses = Math.floor(Math.random() * CLASSES.length);
             e.class = CLASSES[indexClasses];
             return e;
-        });
+        })
+    }
+
+    public ngOnInit() {
+        this.data = this.generateRandomData(DATA);
         this.character = new Character();
         this.alignments = ALIGNMENTS;
         this.races = RACES;
         this.classes = CLASSES;
-    }
-
-    public removeRow(rowIndex) {
-       const row = this.grid1.getRowByIndex(rowIndex);
-       row.delete();
-    }
-
-    public addRow() {
-        this.grid1.addRow(this.character);
-        this.cancel();
-    }
-
-    public cancel() {
-        this.dialog.close();
-        this.character = new Character();
     }
 }
