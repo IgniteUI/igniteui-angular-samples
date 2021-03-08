53a5a32cf8225a495592994d9c2210d043e0f3e3
dobromirts <dobromir.ts@gmail.com>
2021-03-08 16:39:07 +0200

    chore(*): fix crm lint errors

--- projects/app-crm/e2e/src/app.po.ts
+++ projects/app-crm/e2e/src/app.po.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { browser, by, element } from "protractor";
+import { browser, by, element } from 'protractor';
 
 export class AppPage {
   public navigateTo() {
@@ -7,6 +7,6 @@ export class AppPage {
   }
 
   public getTitleText() {
-    return element(by.css("app-root .content span")).getText() as Promise<string>;
+    return element(by.css('app-root .content span')).getText() as Promise<string>;
   }
 }
