c974c43350a0af73feb9a83e2d8422751e953420
dobromirts <dobromir.ts@gmail.com>
2021-03-10 14:51:14 +0200

    chore(*): refactor rtl sample

--- src/app/scheduling/calendar/calendar-rtl-sample/calendar-rtl-sample.component.ts
+++ src/app/scheduling/calendar/calendar-rtl-sample/calendar-rtl-sample.component.ts
@@ -9,18 +9,9 @@ import { IgxCalendarComponent } from "igniteui-angular";
 export class CalendarRtlSampleComponent {
     @ViewChild("calendar", { read: IgxCalendarComponent }) public calendar: IgxCalendarComponent;
 
-    public formatOptions: any;
-    public formatViews: any;
-    public locales = ["EN", "DE", "FR", "AR", "ZH"];
-    public locale = "EN";
     public directionality = 'rtl'
 
     changeDirectionality(){
         this.directionality === 'rtl' ? this.directionality = 'ltr' : this.directionality = 'rtl';
-    }
-
-    public ngOnInit() {
-        this.formatOptions = { day: "2-digit", month: "long", weekday: "long", year: "numeric" };
-        this.formatViews = { day: true, month: true, year: true };
     }
 }
