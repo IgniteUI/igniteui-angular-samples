8eeaf852dd1edcbfbbf1b64e7bf53ba95b783176
Anastas Staev <astaev@infragistics.com>
2021-04-08 15:22:57 +0300

    samples(tabs): Make tabs closable

--- src/app/layouts/tabs/tabs-header-prefix-suffix/tabs-header-prefix-suffix.ts
+++ src/app/layouts/tabs/tabs-header-prefix-suffix/tabs-header-prefix-suffix.ts
@@ -5,5 +5,21 @@ import { Component } from '@angular/core';
     templateUrl: './tabs-header-prefix-suffix.html'
 })
 export class TabsHeaderPrefixSuffixComponent {
+    public tabs = [{
+        image: 'https://static.infragistics.com/marketing/Website/products/ignite-ui-landing/ignite-ui-logo.svg',
+        header: 'Tab1',
+        content: 'Content1'
+    }, {
+        image: 'https://static.infragistics.com/marketing/Website/products/ignite-ui-landing/ignite-ui-logo.svg',
+        header: 'Tab2',
+        content: 'Content2'
+    }, {
+        image: 'https://static.infragistics.com/marketing/Website/products/ignite-ui-landing/ignite-ui-logo.svg',
+        header: 'Tab3',
+        content: 'Content3'
+    }];
 
+    closeTab(index: number) {
+        this.tabs.splice(index, 1);
+    }
 }
