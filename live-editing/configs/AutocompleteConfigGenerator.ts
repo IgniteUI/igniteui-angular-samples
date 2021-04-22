6e102c1c8cba7ea0487ddc997743fd7670b5266c
viktorkombov <viktorkombov@gmail.com>
2021-04-22 11:50:47 +0300

    fix(live-editing): sample not loading in codesandbox and stackblitz - vNext #2598

--- live-editing/configs/AutocompleteConfigGenerator.ts
+++ live-editing/configs/AutocompleteConfigGenerator.ts
@@ -1,45 +1,49 @@
 import {IgxAutocompleteModule,
-IgxDropDownModule,
-IgxInputGroupModule} from 'igniteui-angular';
-
-import { Config, IConfigGenerator, AppModuleConfig } from 'igniteui-live-editing';
-
-export class AutocompleteConfigGenerator implements IConfigGenerator {
-    public additionalImports = {
-        AutocompletePipeStartsWith: '../../src/app/data-entries/autocomplete/autocomplete/autocomplete.component'
-};
-    public generateConfigs(): Config[] {
-        const configs = new Array<Config>();
-
-        configs.push(new Config({
-            appModuleConfig: new AppModuleConfig({
-                imports: ['IgxAutocompleteModule', 'IgxDropDownModule', 'IgxInputGroupModule',
-                    'AutocompleteBasicComponent', 'AutocompletePipeStartsWith'],
-                ngDeclarations: ['AutocompleteBasicComponent', 'AutocompletePipeStartsWith'],
-                ngImports: ['IgxAutocompleteModule', 'IgxDropDownModule', 'IgxInputGroupModule']
-            }),
-            component: 'AutocompleteBasicComponent',
-            shortenComponentPathBy: "/data-entries/autocomplete/"
-        }));
-
-        configs.push(new Config({
-            appModuleConfig: new AppModuleConfig({
-                imports: [
-                    'IgxAutocompleteModule',
-                    'IgxDropDownModule',
-                    'IgxInputGroupModule',
-                    'MovieComponent'
-                ],
-                ngDeclarations: ['MovieComponent' ],
-                ngImports: [
-                    'IgxAutocompleteModule',
-                    'IgxDropDownModule',
-                    'IgxInputGroupModule'
-                ]
-            }),
-            component: 'MovieComponent'
-        }));
-
-        return configs;
+    IgxDropDownModule,
+    IgxInputGroupModule} from 'igniteui-angular';
+    
+    import { Config, IConfigGenerator, AppModuleConfig } from 'igniteui-live-editing';
+    
+    export class AutocompleteConfigGenerator implements IConfigGenerator {
+        public additionalImports = {
+            AutocompletePipeStartsWith: '../../src/app/data-entries/autocomplete/autocomplete/autocomplete.component',
+            AutocompletePipeStartsWith2: '../../src/app/data-entries/autocomplete/movie/movie.component'
+    
+    };
+        public generateConfigs(): Config[] {
+            const configs = new Array<Config>();
+    
+            configs.push(new Config({
+                appModuleConfig: new AppModuleConfig({
+                    imports: ['IgxAutocompleteModule', 'IgxDropDownModule', 'IgxInputGroupModule',
+                        'AutocompleteBasicComponent', 'AutocompletePipeStartsWith'],
+                    ngDeclarations: ['AutocompleteBasicComponent', 'AutocompletePipeStartsWith'],
+                    ngImports: ['IgxAutocompleteModule', 'IgxDropDownModule', 'IgxInputGroupModule']
+                }),
+                component: 'AutocompleteBasicComponent',
+                shortenComponentPathBy: "/data-entries/autocomplete/"
+            }));
+    
+            configs.push(new Config({
+                appModuleConfig: new AppModuleConfig({
+                    imports: [
+                        'IgxAutocompleteModule',
+                        'IgxDropDownModule',
+                        'IgxInputGroupModule',
+                        'MovieComponent',
+                        'AutocompletePipeStartsWith2'
+                    ],
+                    ngDeclarations: ['MovieComponent', 'AutocompletePipeStartsWith2'],
+                    ngImports: [
+                        'IgxAutocompleteModule',
+                        'IgxDropDownModule',
+                        'IgxInputGroupModule'
+                    ]
+                }),
+                component: 'MovieComponent'
+            }));
+    
+            return configs;
+        }
     }
-}
+    
