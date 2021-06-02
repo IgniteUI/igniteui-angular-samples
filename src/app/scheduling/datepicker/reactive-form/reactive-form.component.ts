68dadc8b3de6e0b24c0a4aee7033258f632428eb
plamenamiteva <plamenamiteva@abv.bg>
2021-06-02 14:37:59 +0300

    sample(pickers): addressing comments

--- src/app/scheduling/datepicker/reactive-form/reactive-form.component.ts
+++ src/app/scheduling/datepicker/reactive-form/reactive-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -6,11 +6,11 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
     styleUrls: ['./reactive-form.component.scss'],
     templateUrl: './reactive-form.component.html'
 })
-export class DateTimePickerRFSampleComponent implements OnInit {
+export class DateTimePickerRFSampleComponent {
     public dateTimeForm: FormGroup;
     public date = new Date();
-    public minDate = new Date(new Date(this.date.getFullYear(), this.date.getMonth(), this.date.getDate() - 10));
-    public maxDate = new Date(new Date(this.date.getFullYear(), this.date.getMonth(), this.date.getDate() + 15));
+    public minDate = new Date(this.date.getFullYear(), this.date.getMonth(), this.date.getDate() - 10);
+    public maxDate = new Date(this.date.getFullYear(), this.date.getMonth(), this.date.getDate() + 15);
     public minTime = '06:15:30';
     public maxTime = '15:15:30';
 
@@ -18,16 +18,6 @@ export class DateTimePickerRFSampleComponent implements OnInit {
         this.dateTimeForm = fb.group({
             date: [this.date, Validators.required],
             time: [this.date, Validators.required]
-        });
-    }
-
-    ngOnInit() {
-        this.dateTimeForm.get('date').valueChanges.subscribe(selectedDate => {
-            this.dateTimeForm.get('time').setValue(selectedDate, { emitEvent: false });
-        });
-
-        this.dateTimeForm.get('time').valueChanges.subscribe(selectedTime => {
-            this.dateTimeForm.get('date').setValue(selectedTime, { emitEvent: false });
         });
     }
 }
