f70360a3185bc583a7e5c13c88156a1ce14091a6
Galina Edinakova <gedinakova@infragistics.com>
2021-07-19 09:18:49 +0300

    feat(TreeGrid): Update to new GroupByArea. (#2717)
    * feat(TreeGrid): Update to new GroupByArea.
    Co-authored-by: Dimitar Dimitrov <dmdimitrov@infragistics.com>
    Co-authored-by: Nikolay Alipiev <nikolay.alipiev@gmail.com>

--- live-editing/configs/TreeGridConfigGenerator.ts
+++ live-editing/configs/TreeGridConfigGenerator.ts
@@ -35,8 +35,7 @@ export class TreeGridConfigGenerator implements IConfigGenerator {
         IgxPreventDocumentScrollModule: '../../src/app/directives/prevent-scroll.directive',
         TreeGridWithTransactionsComponent: '../../src/app/tree-grid/tree-grid-batch-editing/tree-grid-with-transactions.component',
         RemoteValuesService: '../../src/app/tree-grid/tree-grid-excel-style-filtering-load-on-demand/remoteValues.service',
-        TreeGridContextmenuComponent: '../../src/app/tree-grid/tree-grid-sorting-sample/tree-grid-contextmenu/tree-grid-contextmenu.component',
-        IgxTreeGridGroupAreaComponent: '../../src/app/tree-grid/tree-grid-group-by-load-on-demand-sample/tree-grid-group-area.component'
+        TreeGridContextmenuComponent: '../../src/app/tree-grid/tree-grid-sorting-sample/tree-grid-contextmenu/tree-grid-contextmenu.component'
 };
     public generateConfigs(): Config[] {
         const configs = new Array<Config>();
@@ -639,14 +638,11 @@ export class TreeGridConfigGenerator implements IConfigGenerator {
             additionalFiles: [
                 '/src/app/directives/prevent-scroll.directive.ts',
                 '/src/app/tree-grid/tree-grid-group-by-load-on-demand-sample/remoteService.ts',
-                '/src/app/tree-grid/tree-grid-group-by-load-on-demand-sample/tree-grid-group-area.component.ts',
-                '/src/app/tree-grid/tree-grid-group-by-load-on-demand-sample/tree-grid-group-area.component.html',
-                '/src/app/tree-grid/tree-grid-group-by-load-on-demand-sample/tree-grid-group-area.component.scss',
                 '/src/app/data/invoiceData.ts'
             ],
             appModuleConfig: new AppModuleConfig({
-                imports: ['IgxPreventDocumentScrollModule', 'IgxTreeGridModule', 'TreeGridGroupByLoadOnDemandComponent', 'IgxTreeGridGroupAreaComponent'],
-                ngDeclarations: ['TreeGridGroupByLoadOnDemandComponent', 'IgxTreeGridGroupAreaComponent'],
+                imports: ['IgxPreventDocumentScrollModule', 'IgxTreeGridModule', 'TreeGridGroupByLoadOnDemandComponent'],
+                ngDeclarations: ['TreeGridGroupByLoadOnDemandComponent'],
                 ngImports: ['IgxPreventDocumentScrollModule', 'IgxTreeGridModule']
             }),
             component: 'TreeGridGroupByLoadOnDemandComponent'
