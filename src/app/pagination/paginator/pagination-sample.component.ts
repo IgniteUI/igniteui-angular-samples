19f80c0c684661e5da8df2d45908ce9fa2bfb29c
ddincheva <desislava9dincheva@gmail.com>
2021-08-02 09:23:21 +0300

    chore(*): add separate pagination sample

---
+++ src/app/pagination/paginator/pagination-sample.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IgxPaginatorComponent } from 'igniteui-angular';
+import { DATA } from '../../data/product';
+
+@Component({
+    selector: 'app-pagination-sample',
+    styleUrls: ['./pagination-sample.component.scss'],
+    templateUrl: './pagination-sample.component.html'
+})
+export class PaginationSampleComponent implements OnInit{
+
+    @ViewChild('paginator', { static: true }) public paginator!: IgxPaginatorComponent;
+    public productData = DATA;
+    public itemsPerPage = [3,4,5,6];
+
+    public ngOnInit() {
+        this.productData.forEach((prod) => prod.icons = ['favorite', 'share']);
+    }
+
+    public get data() {
+        let products = this.productData;
+        products = this.paginator ?
+        this.productData.slice(this.paginator.page * this.paginator.perPage,
+            ((this.paginator.page * this.paginator.perPage) + this.paginator.perPage)) : products;
+        return products;
+    }
+
+    public navigateToFirstPage() {
+        this.paginator.page = 0;
+    }
+
+}
