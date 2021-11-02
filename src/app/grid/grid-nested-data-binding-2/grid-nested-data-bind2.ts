489874754c3fc49df82485c022c6dea3dc85baf8
Konstantin Dinev <kdinev@mail.bw.edu>
2021-11-02 09:22:37 +0200

    chore(*): moving another data file

--- src/app/grid/grid-nested-data-binding-2/grid-nested-data-bind2.ts
+++ src/app/grid/grid-nested-data-binding-2/grid-nested-data-bind2.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { GridColumnDataType } from 'igniteui-angular';
 import { AMINO_DATA } from '../../data/amino-data';
 
