9a00b5f973124c1adf44ad1e36a8909f34dafb8a
Konstantin Dinev <kdinev@mail.bw.edu>
2021-11-03 12:07:20 +0200

    fix(lint): removing empty lifecycle methods

--- src/app/layouts/avatar/avatar-sample-2/avatar-sample-2.component.ts
+++ src/app/layouts/avatar/avatar-sample-2/avatar-sample-2.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-avatar-sample-2',
   styleUrls: ['./avatar-sample-2.component.scss'],
   templateUrl: './avatar-sample-2.component.html'
 })
-export class AvatarSample2Component implements OnInit {
+export class AvatarSample2Component {
 
   constructor() { }
-
-  public ngOnInit() {
-  }
 
 }
