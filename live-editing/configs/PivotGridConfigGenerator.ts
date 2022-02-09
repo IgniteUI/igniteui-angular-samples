bd01744f1516c5e017b10494fe278c93b6deee58
Deyan Kamburov <dkamburov@infragistics.com>
2022-02-09 20:28:05 +0200

    chore(pivot): Add live-editing config and height to pivot grid sample

---
+++ live-editing/configs/PivotGridConfigGenerator.ts
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+/* eslint-disable max-len */
+import {
+    IgxPivotGridModule
+} from 'igniteui-angular';
+import { AppModuleConfig, Config, IConfigGenerator } from 'igniteui-live-editing';
+export class PivotGridConfigGenerator implements IConfigGenerator {
+    public additionalImports = {
+        IgxPreventDocumentScrollModule: '../../src/app/directives/prevent-scroll.directive'
+    };
+    public generateConfigs(): Config[] {
+        const configs = new Array<Config>();
+
+        configs.push(new Config({
+            additionalFiles: ['/src/app/directives/prevent-scroll.directive.ts', '/src/app/data/pivot-data.ts'],
+            appModuleConfig: new AppModuleConfig({
+                imports: ['IgxPivotGridModule', 'PivotGridBasicSampleComponent', 'IgxPreventDocumentScrollModule'],
+                ngDeclarations: ['PivotGridBasicSampleComponent'],
+                ngImports: ['IgxPreventDocumentScrollModule', 'IgxPivotGridModule']
+            }),
+            component: 'PivotGridBasicSampleComponent'
+        }));
+
+        return configs;
+    }
+}
