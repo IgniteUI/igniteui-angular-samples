22ec54d1435150a0bcae3809241c54c764437b96
3phase <pbozhinow@gmail.com>
2022-02-09 16:20:03 +0200

    Pushes new datasource

---
+++ src/app/data/dataToAnalyze.ts
@@ -0,0 +1,390 @@
+export const DATA_TO_ANALYZE = [
+    /*eslint-disable*/
+    {
+        Date: "1/6/08",
+        Region: "Quebec",
+        Rep: "Jones",
+        Item: "Pencil",
+        Units: "95",
+        Cost: "$1.99 ",
+        Total: "$189.05 "
+    },
+    {
+        Date: "1/23/08",
+        Region: "Ontario",
+        Rep: "Kivell",
+        Item: "Binder",
+        Units: "50",
+        Cost: "$19.99 ",
+        Total: "$999.50 "
+    },
+    {
+        Date: "2/9/08",
+        Region: "Ontario",
+        Rep: "Jardine",
+        Item: "Pencil",
+        Units: "36",
+        Cost: "$4.99 ",
+        Total: "$179.64 "
+    },
+    {
+        Date: "2/26/08",
+        Region: "Ontario",
+        Rep: "Gill",
+        Item: "Pen",
+        Units: "27",
+        Cost: "$19.99 ",
+        Total: "$539.73 "
+    },
+    {
+        Date: "3/15/08",
+        Region: "Alberta",
+        Rep: "Sorvino",
+        Item: "Pencil",
+        Units: "56",
+        Cost: "$2.99 ",
+        Total: "$167.44 "
+    },
+    {
+        Date: "4/1/08",
+        Region: "Quebec",
+        Rep: "Jones",
+        Item: "Binder",
+        Units: "60",
+        Cost: "$4.99 ",
+        Total: "$299.40 "
+    },
+    {
+        Date: "4/18/08",
+        Region: "Ontario",
+        Rep: "Andrews",
+        Item: "Pencil",
+        Units: "75",
+        Cost: "$1.99 ",
+        Total: "$149.25 "
+    },
+    {
+        Date: "5/5/08",
+        Region: "Ontario",
+        Rep: "Jardine",
+        Item: "Pencil",
+        Units: "90",
+        Cost: "$4.99 ",
+        Total: "$449.10 "
+    },
+    {
+        Date: "5/22/08",
+        Region: "Alberta",
+        Rep: "Thompson",
+        Item: "Pencil",
+        Units: "32",
+        Cost: "$1.99 ",
+        Total: "$63.68 "
+    },
+    {
+        Date: "6/8/08",
+        Region: "Quebec",
+        Rep: "Jones",
+        Item: "Binder",
+        Units: "60",
+        Cost: "$8.99 ",
+        Total: "$539.40 "
+    },
+    {
+        Date: "6/25/08",
+        Region: "Ontario",
+        Rep: "Morgan",
+        Item: "Pencil",
+        Units: "90",
+        Cost: "$4.99 ",
+        Total: "$449.10 "
+    },
+    {
+        Date: "7/12/08",
+        Region: "Quebec",
+        Rep: "Howard",
+        Item: "Binder",
+        Units: "29",
+        Cost: "$1.99 ",
+        Total: "$57.71 "
+    },
+    {
+        Date: "7/29/08",
+        Region: "Quebec",
+        Rep: "Parent",
+        Item: "Binder",
+        Units: "81",
+        Cost: "$19.99 ",
+        Total: "$1,619.19 "
+    },
+    {
+        Date: "8/15/08",
+        Region: "Quebec",
+        Rep: "Jones",
+        Item: "Pencil",
+        Units: "35",
+        Cost: "$4.99 ",
+        Total: "$174.65 "
+    },
+    {
+        Date: "9/1/08",
+        Region: "Ontario",
+        Rep: "Smith",
+        Item: "Desk",
+        Units: "2",
+        Cost: "$125.00 ",
+        Total: "$250.00 "
+    },
+    {
+        Date: "9/18/08",
+        Region: "Quebec",
+        Rep: "Jones",
+        Item: "Pen Set",
+        Units: "16",
+        Cost: "$15.99 ",
+        Total: "$255.84 "
+    },
+    {
+        Date: "10/5/08",
+        Region: "Ontario",
+        Rep: "Morgan",
+        Item: "Binder",
+        Units: "28",
+        Cost: "$8.99 ",
+        Total: "$251.72 "
+    },
+    {
+        Date: "10/22/08",
+        Region: "Quebec",
+        Rep: "Jones",
+        Item: "Pen",
+        Units: "64",
+        Cost: "$8.99 ",
+        Total: "$575.36 "
+    },
+    {
+        Date: "11/8/08",
+        Region: "Quebec",
+        Rep: "Parent",
+        Item: "Pen",
+        Units: "15",
+        Cost: "$19.99 ",
+        Total: "$299.85 "
+    },
+    {
+        Date: "11/25/08",
+        Region: "Ontario",
+        Rep: "Kivell",
+        Item: "Pen Set",
+        Units: "96",
+        Cost: "$4.99 ",
+        Total: "$479.04 "
+    },
+    {
+        Date: "12/12/08",
+        Region: "Ontario",
+        Rep: "Smith",
+        Item: "Pencil",
+        Units: "67",
+        Cost: "$1.29 ",
+        Total: "$86.43 "
+    },
+    {
+        Date: "12/29/08",
+        Region: "Quebec",
+        Rep: "Parent",
+        Item: "Pen Set",
+        Units: "74",
+        Cost: "$15.99 ",
+        Total: "$1,183.26 "
+    },
+    {
+        Date: "1/15/09",
+        Region: "Ontario",
+        Rep: "Gill",
+        Item: "Binder",
+        Units: "46",
+        Cost: "$8.99 ",
+        Total: "$413.54 "
+    },
+    {
+        Date: "2/1/09",
+        Region: "Ontario",
+        Rep: "Smith",
+        Item: "Binder",
+        Units: "87",
+        Cost: "$15.00 ",
+        Total: "$1,305.00 "
+    },
+    {
+        Date: "2/18/09",
+        Region: "Quebec",
+        Rep: "Jones",
+        Item: "Binder",
+        Units: "4",
+        Cost: "$4.99 ",
+        Total: "$19.96 "
+    },
+    {
+        Date: "3/7/09",
+        Region: "Alberta",
+        Rep: "Sorvino",
+        Item: "Binder",
+        Units: "7",
+        Cost: "$19.99 ",
+        Total: "$139.93 "
+    },
+    {
+        Date: "3/24/09",
+        Region: "Ontario",
+        Rep: "Jardine",
+        Item: "Pen Set",
+        Units: "50",
+        Cost: "$4.99 ",
+        Total: "$249.50 "
+    },
+    {
+        Date: "4/10/09",
+        Region: "Ontario",
+        Rep: "Andrews",
+        Item: "Pencil",
+        Units: "66",
+        Cost: "$1.99 ",
+        Total: "$131.34 "
+    },
+    {
+        Date: "4/27/09",
+        Region: "Quebec",
+        Rep: "Howard",
+        Item: "Pen",
+        Units: "96",
+        Cost: "$4.99 ",
+        Total: "$479.04 "
+    },
+    {
+        Date: "5/14/09",
+        Region: "Ontario",
+        Rep: "Gill",
+        Item: "Pencil",
+        Units: "53",
+        Cost: "$1.29 ",
+        Total: "$68.37 "
+    },
+    {
+        Date: "5/31/09",
+        Region: "Ontario",
+        Rep: "Gill",
+        Item: "Binder",
+        Units: "80",
+        Cost: "$8.99 ",
+        Total: "$719.20 "
+    },
+    {
+        Date: "6/17/09",
+        Region: "Ontario",
+        Rep: "Kivell",
+        Item: "Desk",
+        Units: "5",
+        Cost: "$125.00 ",
+        Total: "$625.00 "
+    },
+    {
+        Date: "7/4/09",
+        Region: "Quebec",
+        Rep: "Jones",
+        Item: "Pen Set",
+        Units: "62",
+        Cost: "$4.99 ",
+        Total: "$309.38 "
+    },
+    {
+        Date: "7/21/09",
+        Region: "Ontario",
+        Rep: "Morgan",
+        Item: "Pen Set",
+        Units: "55",
+        Cost: "$12.49 ",
+        Total: "$686.95 "
+    },
+    {
+        Date: "8/7/09",
+        Region: "Ontario",
+        Rep: "Kivell",
+        Item: "Pen Set",
+        Units: "42",
+        Cost: "$23.95 ",
+        Total: "$1,005.90 "
+    },
+    {
+        Date: "8/24/09",
+        Region: "Alberta",
+        Rep: "Sorvino",
+        Item: "Desk",
+        Units: "3",
+        Cost: "$275.00 ",
+        Total: "$825.00 "
+    },
+    {
+        Date: "9/10/09",
+        Region: "Ontario",
+        Rep: "Gill",
+        Item: "Pencil",
+        Units: "7",
+        Cost: "$1.29 ",
+        Total: "$9.03 "
+    },
+    {
+        Date: "9/27/09",
+        Region: "Alberta",
+        Rep: "Sorvino",
+        Item: "Pen",
+        Units: "76",
+        Cost: "$1.99 ",
+        Total: "$151.24 "
+    },
+    {
+        Date: "10/14/09",
+        Region: "Alberta",
+        Rep: "Thompson",
+        Item: "Binder",
+        Units: "57",
+        Cost: "$19.99 ",
+        Total: "$1,139.43 "
+    },
+    {
+        Date: "10/31/09",
+        Region: "Ontario",
+        Rep: "Andrews",
+        Item: "Pencil",
+        Units: "14",
+        Cost: "$1.29 ",
+        Total: "$18.06 "
+    },
+    {
+        Date: "11/17/09",
+        Region: "Ontario",
+        Rep: "Jardine",
+        Item: "Binder",
+        Units: "11",
+        Cost: "$4.99 ",
+        Total: "$54.89 "
+    },
+    {
+        Date: "12/4/09",
+        Region: "Ontario",
+        Rep: "Jardine",
+        Item: "Binder",
+        Units: "94",
+        Cost: "$19.99 ",
+        Total: "$1,879.06 "
+    },
+    {
+        Date: "12/21/09",
+        Region: "Ontario",
+        Rep: "Andrews",
+        Item: "Binder",
+        Units: "28",
+        Cost: "$4.99 ",
+        Total: "$139.72 "
+    }
+];
