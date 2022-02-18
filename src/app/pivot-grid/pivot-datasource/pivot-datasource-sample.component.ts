5b5579705db91ee4d942a708e2beb39d6a4a771f
3phase <pbozhinow@gmail.com>
2022-02-18 19:51:37 +0200

    chore(pivot): Updates datasource

--- src/app/pivot-grid/pivot-datasource/pivot-datasource-sample.component.ts
+++ src/app/pivot-grid/pivot-datasource/pivot-datasource-sample.component.ts
@@ -1,55 +1,60 @@
 import { Component } from "@angular/core";
-import { IgxPivotDateAggregate, IgxPivotNumericAggregate, IPivotConfiguration } from "igniteui-angular";
-import { DATA_TO_ANALYZE } from "../../data/dataToAnalyze";
-
-
+import { GridColumnDataType, IgxPivotNumericAggregate, IPivotConfiguration } from "igniteui-angular";
+import { SALES_DATA_NEW } from '../../data/salesDataNew';
 @Component({
     selector: 'app-pivot-datasource-sample',
     styleUrls: ['./pivot-datasource-sample.component.scss'],
     templateUrl: './pivot-datasource-sample.component.html'
 })
 export class PivotDatasourceSampleComponent {
-    public data = DATA_TO_ANALYZE;
+    public data = SALES_DATA_NEW;
     public pivotConfigHierarchy: IPivotConfiguration = {
         columns: [
-
             {
-                memberName: 'Region',
+                memberName: 'Year',
+                enabled: true
+            },
+            {
+
+                memberName: 'Product',
                 enabled: true
             }
         ],
         rows: [
             {
-
-                memberName: 'Item',
-                enabled: true
+                memberName: 'Country',
+                enabled: true,
+                childLevel: {
+                    memberName: 'Date',
+                    enabled: true
+                }
             }
         ],
         values: [
             {
-                member: 'Units',
+                member: 'Sales',
                 aggregate: {
                     aggregator: IgxPivotNumericAggregate.sum,
-                    key: 'Amount',
-                    label: 'Total'
+                    key: 'Sum Of Sales',
+                    label: 'Sum'
                 },
-                enabled: true
-
+                enabled: true,
+                dataType: GridColumnDataType.Currency
             },
             {
-                member: 'Date',
+                member: 'Profit',
                 aggregate: {
-                    aggregator: IgxPivotDateAggregate.latest,
-                    key: 'As Of',
-                    label: 'Latest'
+                    aggregator: IgxPivotNumericAggregate.sum,
+                    key: 'Sum Of Profit',
+                    label: 'Sum'
                 },
-                enabled: true
-
+                enabled: true,
+                dataType: GridColumnDataType.Currency
             }
         ],
         filters: [
             {
-                memberName: 'Rep',
+                memberName: 'Month Name',
                 enabled: true
             }
         ]
