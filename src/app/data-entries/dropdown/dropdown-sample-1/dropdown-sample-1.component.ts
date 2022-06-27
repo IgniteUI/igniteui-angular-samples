e688587bccc8c4bdf640ff919a9684f624b1f0cc
mmart1n <mevtimov@infragistics.com>
2022-06-27 12:10:40 +0300

    feat(*): initial impl

--- src/app/data-entries/dropdown/dropdown-sample-1/dropdown-sample-1.component.ts
+++ src/app/data-entries/dropdown/dropdown-sample-1/dropdown-sample-1.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { CloseScrollStrategy, ConnectedPositioningStrategy, IgxDropDownComponent, OverlaySettings } from 'igniteui-angular';
+import { DropDownSample2Component } from '../dropdown-sample-2/dropdown-sample-2.component';
 @Component({
     // tslint:disable-next-line:component-selector
     selector: 'app-dropdown-sample-1',
@@ -6,9 +8,53 @@ import { Component } from '@angular/core';
     templateUrl: './dropdown-sample-1.component.html'
 })
 export class DropDownSample1Component {
+    @ViewChild('dropdown', { read: IgxDropDownComponent }) public dropDown: IgxDropDownComponent;
+    @ViewChild('nested', { read: DropDownSample2Component }) public nested: DropDownSample2Component;
+
+    private isHovered = false;
+
     public items: { field: string }[] = [
         { field: 'Option 1' },
         { field: 'Option 2' },
         { field: 'Option 3' }
     ];
+
+    public ngAfterViewInit() {
+        this.dropDown.selectionChanging.subscribe(ev => {
+           if (ev.newSelection.id === 'igx-drop-down-item-0') {
+            ev.cancel = true;
+           }
+        });
+    }
+
+    public onMouseEnter(ev) {
+        console.log('MouseEnter');
+        // debugger;
+        this.isHovered = true;
+        if(this.nested.nestedDropdown.collapsed) {
+            this.nested.open(ev.target);
+        }
+    }
+
+    public shouldClose() {
+        setTimeout(() => {
+
+            console.log('ShouldClose');
+            console.log(this.isHovered);
+            if(!this.isHovered) {
+
+                this.nested.nestedDropdown.close();
+            }
+        }, 150);
+    }
+
+    public onMouseLeave(ev) {
+        this.isHovered = false;
+        setTimeout(() => {
+
+            if(!this.nested.isHovered) {
+                this.nested.nestedDropdown.close();
+            }
+        }, 50);
+    }
 }
--- src/app/data-entries/dropdown/dropdown-sample-2/dropdown-sample-2.component.ts
+++ src/app/data-entries/dropdown/dropdown-sample-2/dropdown-sample-2.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, HostListener, Input, Output, ViewChild } from '@angular/core';
+import { ConnectedPositioningStrategy, IgxDropDownComponent, OverlaySettings } from 'igniteui-angular';
 
 @Component({
     // tslint:disable-next-line:component-selector
@@ -7,9 +8,46 @@ import { Component } from '@angular/core';
     templateUrl: './dropdown-sample-2.component.html'
 })
 export class DropDownSample2Component {
-    public items: { field: string }[] = [
-        { field: 'Option 1' },
-        { field: 'Option 2' },
-        { field: 'Option 3' }
-    ];
+    @ViewChild('nestedDropdown', { read: IgxDropDownComponent }) public nestedDropdown: IgxDropDownComponent;
+    @Input() parentDropdown: IgxDropDownComponent;
+
+    @Output() shouldClose = new EventEmitter<void>();
+
+    public isHovered = false;
+
+    public ngAfterViewInit() {
+        document.getElementById(this.nestedDropdown.id + '-list').addEventListener('mouseenter', () => {
+            this.isHovered = true;
+        });
+
+        document.getElementById(this.nestedDropdown.id + '-list').addEventListener('mouseleave', () => {
+            this.isHovered = false;
+            this.shouldClose.emit();
+        });
+    }
+
+    private overlaySettings: OverlaySettings = {
+        positionStrategy: new ConnectedPositioningStrategy(),
+        closeOnOutsideClick: true,
+        modal: false
+    };
+
+    public open(el: any) {
+        this.nestedDropdown.open({...this.overlaySettings, target: el});
+    }
+
+    public close() {
+        this.nestedDropdown.close();
+    }
+
+    public nestedSelection(ev) {
+        this.nestedDropdown.close();
+    }
+
+    public onClosing(ev) {
+        // console.log(this.isHovered);
+        if(this.isHovered) {
+            ev.cancel = true;
+        }
+    }
 }
