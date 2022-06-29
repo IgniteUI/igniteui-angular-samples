9cf18ca66c0d3cad286b9fd935d3451e926729c1
tishko <ttonev@infragistics.com>
2022-06-29 19:21:02 +0300

    docs(grid): editted sample + shift enter nav

--- src/app/grid/grid-editing-excel-style/grid-editing-excel-style.component.ts
+++ src/app/grid/grid-editing-excel-style/grid-editing-excel-style.component.ts
@@ -12,70 +12,65 @@ import { DATA } from './../../data/nwindData';
   templateUrl: 'grid-editing-excel-style.component.html'
 })
 export class GridExcelStyleEditingComponent implements OnInit {
-  @ViewChild('grid', { read: IgxGridComponent, static: true })
-  public grid: IgxGridComponent;
+    @ViewChild('grid', { read: IgxGridComponent, static: true })
+    public grid: IgxGridComponent;
 
-  public data: any[];
+    public data: any[];
 
-  public ngOnInit(): void {
-    this.data = DATA;
-  }
-
-  public keydownHandler(event) {
-    const key = event.keyCode;
-    const grid = this.grid;
-    const activeElem = grid.navigation.activeNode;
-
-    if (
-      (key >= 48 && key <= 57) ||
-      (key >= 65 && key <= 90) ||
-      (key >= 97 && key <= 122)
-    ) {
-      // Number or Alphabet upper case or Alphabet lower case
-
-      const columnName = grid.getColumnByVisibleIndex(activeElem.column).field;
-      const cell = grid.getCellByColumn(activeElem.row, columnName);
-      if (cell && !grid.crudService.cellInEditMode) {
-        grid.crudService.enterEditMode(cell);
-        cell.editValue = event.key;
-      }
+    public ngOnInit(): void {
+      this.data = DATA;
     }
 
-    if (key == 13) {
-      this.grid.endEdit();
-      this.grid.clearCellSelection();
-      let thisRow = activeElem.row;
-      let nextRow = thisRow + 1;
-      const column = activeElem.column;
-      const rowInfo = grid.dataView;
-      const maxRows = rowInfo.length;
+    public keydownHandler(event) {
+      const key = event.keyCode;
+      const grid = this.grid;
+      const activeElem = grid.navigation.activeNode;
 
-      if (rowInfo[thisRow + 1]) {
-        while ('expression' in rowInfo[thisRow + 1]) {
-          ++thisRow;
-          ++nextRow;
+      if (
+        (key >= 48 && key <= 57) ||
+        (key >= 65 && key <= 90) ||
+        (key >= 97 && key <= 122)
+      ) {
+        // Number or Alphabet upper case or Alphabet lower case
+
+        const columnName = grid.getColumnByVisibleIndex(activeElem.column).field;
+        const cell = grid.getCellByColumn(activeElem.row, columnName);
+        if (cell && !grid.crudService.cellInEditMode) {
+          grid.crudService.enterEditMode(cell);
+          cell.editValue = event.key;
         }
       }
-      if (nextRow < maxRows) {
-        requestAnimationFrame(() => {
-            this.grid.navigateTo(nextRow, column);
-            this.grid.navigation.setActiveNode({
-            gridID:grid.id,
-            row: nextRow,
-            column: column
-        });
-        this.grid.selectRange({
-          rowStart: nextRow,
-          rowEnd: nextRow,
-          columnStart: column,
-          columnEnd: column
-        });
-        })
-      }
-    }
-  }
 
-  public activeNodeChange(event) {
-    this.grid.endEdit();
-  }
+      if (key == 13) {
+        this.grid.clearCellSelection();
+        let thisRow = activeElem.row;
+        const column = activeElem.column;
+        const rowInfo = grid.dataView;
+
+        let nextRow = this.getNextEditableRowIndex(thisRow, rowInfo, event.shiftKey);
+
+        this.grid.navigateTo(nextRow, column, (obj) => {
+          obj.target.activate();
+          });
+      }
+      }
+
+    public activeNodeChange() {
+      this.grid.endEdit();
+    }
+
+    public getNextEditableRowIndex(currentRowIndex, dataView, previous){
+      if (currentRowIndex < 0 || (currentRowIndex === 0 && previous) || (currentRowIndex >= dataView.length - 1 && !previous)) {
+          return currentRowIndex;
+      }
+      if(previous){
+        return  dataView.findLastIndex((rec, index) => index < currentRowIndex && this.isEditableDataRecordAtIndex(index, dataView));
+      }
+      return dataView.findIndex((rec, index) => index > currentRowIndex && this.isEditableDataRecordAtIndex(index, dataView));
+    }
+
+    private isEditableDataRecordAtIndex(dataViewIndex, dataView) {
+      const rec = dataView[dataViewIndex];
+      return !rec.expression && !rec.summaries && !rec.childGridsData && !rec.detailsData
+    }
 }
