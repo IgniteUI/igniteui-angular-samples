d132b84efdd29272d9b7e60ac0516c9ce085a950
3phase <pbozhinow@gmail.com>
2022-08-23 13:52:11 +0300

    Hierarchical validator sample

--- src/app/hierarchical-grid/hierarchical-grid-validator-service/hierarchical-grid-validator-service.component.ts
+++ src/app/hierarchical-grid/hierarchical-grid-validator-service/hierarchical-grid-validator-service.component.ts
@@ -1,5 +1,5 @@
-import { Component, ViewChild } from '@angular/core';
-import { IgxDialogComponent, IgxHierarchicalGridComponent, IgxRowIslandComponent, Transaction } from 'igniteui-angular';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IgxHierarchicalGridComponent, IgxRowIslandComponent } from 'igniteui-angular';
 import { SINGERS } from '../../data/singersData';
 import { Singer } from '../models';
 
@@ -8,126 +8,23 @@ import { Singer } from '../models';
     styleUrls: ['./hierarchical-grid-validator-service.component.scss'],
     templateUrl: './hierarchical-grid-validator-service.component.html'
 })
-export class HierarchicalGridValidatorServiceComponent {
-    @ViewChild('dialogChanges', { read: IgxDialogComponent, static: true })
-    public dialogChanges: IgxDialogComponent;
-
+export class HierarchicalGridValidatorServiceComponent implements OnInit {
     @ViewChild('childGrid', { static: true })
     private childGrid: IgxRowIslandComponent;
 
     @ViewChild('hierarchicalGrid', { static: true })
     private hierarchicalGrid: IgxHierarchicalGridComponent;
 
-    @ViewChild('dialogAddSinger', { read: IgxDialogComponent, static: true })
-    private dialogSinger: IgxDialogComponent;
-
-    public get undoEnabledParent(): boolean {
-        return this.hierarchicalGrid.transactions.canUndo;
-    }
-
-    public get redoEnabledParent(): boolean {
-        return this.hierarchicalGrid.transactions.canRedo;
-    }
-
-    public get hasTransactions(): boolean {
-        return this.hierarchicalGrid.transactions.getAggregatedChanges(false).length > 0 || this.hasChildTransactions;
-    }
-
-    public get hasChildTransactions(): boolean {
-        return this.childGrid.gridAPI.getChildGrids()
-            .find(c => c.transactions.getAggregatedChanges(false).length > 0) !== undefined;
-    }
-
     public data: Singer[];
-    public singer: Singer;
-    public transactionsDataAll: Transaction[] = [];
-    private id = 14;
+
+    public rowEdit: boolean = false;
 
     constructor() { }
 
     public ngOnInit(): void {
         this.data = SINGERS;
-        this.singer = {
-            ID: this.id,
-            Artist: 'Mock Jagger',
-            Debut: 2005,
-            GrammyAwards: 4,
-            GrammyNominations: 7,
-            HasGrammyAward: false
-        };
     }
 
     public formatter = a => a;
 
-    public undo(grid: IgxHierarchicalGridComponent) {
-        /* exit edit mode and commit changes */
-        grid.endEdit(true);
-        grid.transactions.undo();
-    }
-
-    public redo(grid: IgxHierarchicalGridComponent) {
-        /* exit edit mode and commit changes */
-        grid.endEdit(true);
-        grid.transactions.redo();
-    }
-
-    public commit() {
-        this.hierarchicalGrid.transactions.commit(this.data);
-        this.childGrid.gridAPI.getChildGrids().forEach((grid) => {
-            grid.transactions.commit(grid.data);
-        });
-        this.dialogChanges.close();
-    }
-
-    public discard() {
-        this.hierarchicalGrid.transactions.clear();
-        this.childGrid.gridAPI.getChildGrids().forEach((grid) => {
-            grid.transactions.clear();
-        });
-        this.dialogChanges.close();
-    }
-
-    public openCommitDialog() {
-        this.transactionsDataAll = [...this.hierarchicalGrid.transactions.getAggregatedChanges(true)];
-        this.childGrid.gridAPI.getChildGrids().forEach((grid) => {
-            this.transactionsDataAll = this.transactionsDataAll.concat(grid.transactions.getAggregatedChanges(true));
-        });
-        this.dialogChanges.open();
-    }
-
-    public addSinger() {
-        this.hierarchicalGrid.addRow(this.singer);
-        ++this.id;
-        this.cancel();
-    }
-
-    public removeRow(rowIndex) {
-        const row = this.hierarchicalGrid.getRowByIndex(rowIndex);
-        row.delete();
-    }
-
-    public stateFormatter(value: string) {
-        return JSON.stringify(value);
-    }
-
-    public typeFormatter(value: string) {
-        return value.toUpperCase();
-    }
-
-    public classFromType(type: string): string {
-        return `transaction--${type.toLowerCase()}`;
-    }
-
-    public cancel() {
-        this.dialogChanges.close();
-        this.dialogSinger.close();
-        this.singer = {
-            ID: this.id,
-            Artist: 'Mock Jagger',
-            Debut: 2005,
-            GrammyAwards: 4,
-            GrammyNominations: 7,
-            HasGrammyAward: false
-        };
-    }
 }
