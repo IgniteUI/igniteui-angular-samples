83e1bd4b3dd08aa2f04d74a52c85cb4865eea5a2
Deyan Kamburov <dkamburov@infragistics.com>
2022-09-07 18:45:38 +0300

    docs(validation): Polish hierarchical grid sample and live-editing

--- live-editing/configs/HierarchicalGridConfigGenerator.ts
+++ live-editing/configs/HierarchicalGridConfigGenerator.ts
@@ -958,6 +958,17 @@ export class HierarchicalGridConfigGenerator implements IConfigGenerator {
             component: 'HGridColumnAutoSizingSampleComponent'
         }));
 
+        configs.push(new Config({
+            additionalFiles: ['/src/app/directives/prevent-scroll.directive.ts', '/src/app/data/singersData.ts',
+                '/src/app/hierarchical-grid/models.ts'],
+            appModuleConfig: new AppModuleConfig({
+                imports: ['IgxHierarchicalGridModule', 'HGridValidationStyleComponent', 'IgxPreventDocumentScrollModule'],
+                ngDeclarations: ['HGridValidationStyleComponent'],
+                ngImports: ['IgxPreventDocumentScrollModule', 'IgxHierarchicalGridModule']
+            }),
+            component: 'HGridValidationStyleComponent'
+        }));
+
         return configs;
     }
 }
--- src/app/hierarchical-grid/hierarchical-grid-validation-style/hierarchical-grid-validation-style.component.ts
+++ src/app/hierarchical-grid/hierarchical-grid-validation-style/hierarchical-grid-validation-style.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { RowType } from 'igniteui-angular';
 import { SINGERS } from '../../data/singersData';
 import { Singer } from '../models';
 
@@ -7,9 +8,11 @@ import { Singer } from '../models';
     styleUrls: ['./hierarchical-grid-validation-style.component.scss'],
     templateUrl: 'hierarchical-grid-validation-style.component.html'
 })
-
 export class HGridValidationStyleComponent {
     public localData: Singer[];
+    public rowStyles = {
+        background: (row: RowType) => row.cells.find(c => c.errors !== null && c.errors !== undefined) ? '#FF000033' : '#00000000'
+    };
 
     constructor() {
         this.localData = SINGERS;
