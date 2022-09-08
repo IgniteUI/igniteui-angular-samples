6841fa9866d58dd97e93733d825d76d3a1d058ee
Konstantin Dinev <kdinev@mail.bw.edu>
2022-09-08 16:34:53 +0300

    feat(list): adding list item selection sample

---
+++ src/app/lists/list/list-item-selection/list-item-selection.component.ts
@@ -0,0 +1,72 @@
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { IgxFilterOptions } from 'igniteui-angular';
+
+@Component({
+  selector: 'app-list-item-selection',
+  templateUrl: './list-item-selection.component.html',
+  styleUrls: ['./list-item-selection.component.scss']
+})
+export class ListItemSelectionComponent {
+    public searchContact: string;
+
+    public contacts = [
+      {
+        isFavorite: false,
+        name: 'Terrance Orta',
+        phone: '770-504-2217',
+        photo: 'assets/images/men/27.jpg',
+        selected: false
+      },
+      {
+        isFavorite: true,
+        name: 'Richard Mahoney',
+        phone: '423-676-2869',
+        photo: 'assets/images/men/1.jpg',
+        selected: false
+      },
+      {
+        isFavorite: false,
+        name: 'Donna Price',
+        phone: '859-496-2817',
+        photo: 'assets/images/women/50.jpg',
+        selected: false
+      },
+      {
+        isFavorite: false,
+        name: 'Lisa Landers',
+        phone: '901-747-3428',
+        photo: 'assets/images/women/3.jpg',
+        selected: false
+      },
+      {
+        isFavorite: true,
+        name: 'Dorothy H. Spencer',
+        phone: '573-394-9254',
+        photo: 'assets/images/women/67.jpg',
+        selected: false
+      }
+    ];
+
+    public displayDensities;
+
+    constructor(private cdr: ChangeDetectorRef) { }
+
+    public toggleFavorite(contact: any) {
+      contact.isFavorite = !contact.isFavorite;
+    }
+
+    public selectItem(item) {
+      if (!item.selected) {
+        this.contacts.forEach((c) => (c.selected = false));
+        item.selected = true;
+        this.cdr.detectChanges();
+      }
+    }
+
+    get filterContacts() {
+      const fo = new IgxFilterOptions();
+      fo.key = 'name';
+      fo.inputValue = this.searchContact;
+      return fo;
+    }
+}
