3f1742cd5ba0e4fe8de49dc6abfc815c2fbe7970
viktorkombov <viktorkombov@gmail.com>
2022-11-24 13:53:50 +0200

    fix(grids): improve the pinning icon in Custom Column Pinning UI samples

--- live-editing/configs/HierarchicalGridConfigGenerator.ts
+++ live-editing/configs/HierarchicalGridConfigGenerator.ts
@@ -342,7 +342,7 @@ export class HierarchicalGridConfigGenerator implements IConfigGenerator {
         }));
 
         configs.push(new Config({
-            additionalFiles: ['/src/app/directives/prevent-scroll.directive.ts', '/src/app/data/hierarchical-data.ts'],
+            additionalFiles: ['/src/app/directives/prevent-scroll.directive.ts', '/src/app/data/hierarchical-data.ts', '/src/app/services/svgIcons.ts'],
             appModuleConfig: new AppModuleConfig({
                 imports: ['IgxHierarchicalGridModule', 'HGridPinningSampleComponent', 'IgxPreventDocumentScrollModule'],
                 ngDeclarations: ['HGridPinningSampleComponent'],
