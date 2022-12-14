5ea7d5e1e0fbb4749c85df5cd44c4eec57aebc3a
Svetloslav Novoselski <svetli.novoselski@gmail.com>
2022-12-14 14:10:39 +0200

    feat(grid): add sample with state persistance

---
+++ src/app/grid/grid-state-persistence-sample/grid-state-persistance-sample.component.ts
@@ -0,0 +1,59 @@
+import { Component, ViewChild } from '@angular/core';
+
+import { IGridStateOptions, IgxGridStateDirective, IgxGridComponent, NoopSortingStrategy } from 'igniteui-angular';
+import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
+import { FinancialDataService } from '../../services/financial.service';
+
+@Component({
+    providers: [FinancialDataService],
+    selector: 'app-grid-state-persistance-sample',
+    styleUrls: ['./grid-state-persistance-sample.component.scss'],
+    templateUrl: 'grid-state-persistance-sample.component.html'
+})
+
+export class GridStatePersistenceSampleComponent {
+    @ViewChild('grid', { static: true })
+    public grid: IgxGridComponent;
+    @ViewChild(IgxGridStateDirective, { static: true })
+    public state!: IgxGridStateDirective;
+    
+    public data: Observable<any[]>;
+    public customStrategy = NoopSortingStrategy.instance();
+    public options: IGridStateOptions = {
+        rowSelection: true,
+        filtering: true,
+        sorting: true,
+        columnSelection: true
+    };
+
+    constructor(private localService: FinancialDataService) {
+        this.localService.getData(100000);
+        this.data = this.localService.records;
+    }
+
+    public formatNumber(value: number) {
+        return value.toFixed(2);
+    }
+
+    public formatCurrency(value: number) {
+        return '$' + value.toFixed(2);
+    }
+
+    public saveState() {
+        const state = this.state.getState() as string;
+        window.sessionStorage.setItem('grid-state', state);
+    }
+
+    public restoreState() {
+        const state = window.sessionStorage.getItem('grid-state');
+        this.state.stateParsed.pipe(take(1)).subscribe(parsedState => {
+            parsedState.sorting.forEach(expression => expression.strategy = NoopSortingStrategy.instance());
+        });
+        this.state.setState(state as string);
+    }
+
+    public clearStorage() {
+        window.sessionStorage.removeItem('grid-state');
+    }
+}
