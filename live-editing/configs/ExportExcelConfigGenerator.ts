2390037abf3eb1e4709cd6a4dff20c11582a9b95
dobromirts <dobromir.ts@gmail.com>
2023-05-17 13:34:05 +0300

    chore(*): fix failing test

--- live-editing/configs/ExportExcelConfigGenerator.ts
+++ live-editing/configs/ExportExcelConfigGenerator.ts
@@ -5,7 +5,8 @@ import {
     IgxDropDownModule,
     IgxExcelExporterService,
     IgxGridModule,
-    IgxTreeGridModule
+    IgxTreeGridModule,
+    IgxToggleModule 
 } from 'igniteui-angular';
 import { AppModuleConfig, Config, IConfigGenerator } from 'igniteui-live-editing';
 export class ExportExcelConfigGenerator implements IConfigGenerator {
