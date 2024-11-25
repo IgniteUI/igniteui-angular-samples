2044253a7e34b69dc8174b35b2c2eb14ab4333b2
Stamen Stoychev <SStoychev@infragistics.com>
2024-11-25 13:59:44 +0200

    fix(crm-app): moving to new routing

--- projects/app-crm/src/main.ts
+++ projects/app-crm/src/main.ts
@@ -1,24 +1,45 @@
 import { enableProdMode, importProvidersFrom } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { PreloadAllModules, provideRouter, Routes, withPreloading } from '@angular/router';
+import { BrowserModule, HammerModule, bootstrapApplication } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 import { environment } from './environments/environment';
 import { SEOService } from '../../app-lob/src/app/seo.service';
-import { IgxAutocompleteModule, IgxRippleModule, IgxGridModule, IgxIconModule, IgxLayoutModule, IgxAvatarModule, IgxInputGroupModule, IgxButtonModule, IgxProgressBarModule, IgxIconButtonDirective } from 'igniteui-angular';
-import { BrowserModule, HammerModule, bootstrapApplication } from '@angular/platform-browser';
-import { AppRoutingModule } from './app/app-routing.module';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
+import {
+    IgxAutocompleteModule,
+    IgxRippleModule,
+    IgxGridModule,
+    IgxIconModule,
+    IgxLayoutModule,
+    IgxAvatarModule,
+    IgxInputGroupModule,
+    IgxButtonModule,
+    IgxProgressBarModule,
+    IgxIconButtonDirective
+} from 'igniteui-angular';
+
 import { IgxPreventDocumentScrollDirective } from '../../../src/app/directives/prevent-scroll.directive';
 import { AppComponent } from './app/app.component';
+import { GridCRMComponent } from './app/grid-crm/grid-crm.component';
 
 if (environment.production) {
   enableProdMode();
 }
 
+const gridCrmRoutes: Routes = [
+    {
+        path: '', pathMatch: 'full', component: GridCRMComponent
+    },
+    {
+        path: 'grid-crm', component: GridCRMComponent
+    }
+];
+
 bootstrapApplication(AppComponent, {
     providers: [
-        importProvidersFrom(IgxAutocompleteModule, IgxRippleModule, IgxGridModule, IgxIconModule, IgxLayoutModule, IgxAvatarModule, IgxInputGroupModule, BrowserModule, AppRoutingModule, IgxButtonModule, IgxProgressBarModule, FormsModule, IgxPreventDocumentScrollDirective, HammerModule, IgxIconButtonDirective),
+        importProvidersFrom(IgxAutocompleteModule, IgxRippleModule, IgxGridModule, IgxIconModule, IgxLayoutModule, IgxAvatarModule, IgxInputGroupModule, BrowserModule, IgxButtonModule, IgxProgressBarModule, FormsModule, IgxPreventDocumentScrollDirective, HammerModule, IgxIconButtonDirective),
+        provideRouter(gridCrmRoutes, withPreloading(PreloadAllModules)),
         SEOService,
         provideAnimations()
     ]
