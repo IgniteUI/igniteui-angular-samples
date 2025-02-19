e267e403cceaf3b013eddd83a5b05486b56e4244
Konstantin Dinev <kdinev@mail.bw.edu>
2025-02-19 11:26:16 +0200

    feat(ssr): adding ssr to all projects

---
+++ src/app/app.config.server.ts
@@ -0,0 +1,14 @@
+import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
+import { provideServerRendering } from '@angular/platform-server';
+import { provideServerRoutesConfig } from '@angular/ssr';
+import { AppConfig } from './app.config';
+import { serverRoutes } from './app.routes.server';
+
+const serverConfig: ApplicationConfig = {
+  providers: [
+    provideServerRendering(),
+    provideServerRoutesConfig(serverRoutes)
+  ]
+};
+
+export const config = mergeApplicationConfig(AppConfig, serverConfig);
