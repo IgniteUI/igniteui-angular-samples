25f5d902d26562555398a46cf0cfeda996101512
igdmdimitrov <49060557+igdmdimitrov@users.noreply.github.com>
2025-02-26 16:21:10 +0200

    feat(query-builder): update overview and template samples (#3608)

--- src/app/interactions/query-builder/query-builder-sample-1/query-builder-sample-1.component.ts
+++ src/app/interactions/query-builder/query-builder-sample-1/query-builder-sample-1.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FilteringExpressionsTree, FilteringLogic, IExpressionTree, IgxBooleanFilteringOperand, IgxDateFilteringOperand, IgxNumberFilteringOperand, IgxQueryBuilderComponent, IgxQueryBuilderHeaderComponent, IgxStringFilteringOperand } from 'igniteui-angular';
+import { FilteringExpressionsTree, FilteringLogic, IExpressionTree, IgxBooleanFilteringOperand, IgxDateFilteringOperand, IgxNumberFilteringOperand, IgxQueryBuilderComponent, IgxStringFilteringOperand } from 'igniteui-angular';
 
 @Component({
     selector: 'app-query-builder-sample-1',
     styleUrls: ['./query-builder-sample-1.component.scss'],
     templateUrl: 'query-builder-sample-1.component.html',
-    imports: [IgxQueryBuilderComponent, IgxQueryBuilderHeaderComponent]
+    imports: [IgxQueryBuilderComponent]
 })
 export class QueryBuilderSample1Component implements OnInit {
     public entities: any[];
@@ -87,8 +87,8 @@ export class QueryBuilderSample1Component implements OnInit {
         const tree = new FilteringExpressionsTree(FilteringLogic.And, undefined, 'Orders', ['*']);
         tree.filteringOperands.push({
             fieldName: 'CompanyID',
-            condition: IgxStringFilteringOperand.instance().condition('in'),
-            conditionName: 'in',
+            condition: IgxStringFilteringOperand.instance().condition('inQuery'),
+            conditionName: 'inQuery',
             searchTree: innerTree
         });
         tree.filteringOperands.push({
