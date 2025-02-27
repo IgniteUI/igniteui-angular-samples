605b3b96d8ed64ac17105e322c76310e4801c690
Konstantin Dinev <kdinev@mail.bw.edu>
2025-02-27 18:28:14 +0200

    chore(ssr): working through the samples that don't build

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,60 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+    {
+        path: 'home',
+        renderMode: RenderMode.Prerender
+    },
+    {
+        path: 'data-display/**',
+        renderMode: RenderMode.Client // TODO: Fails with Prerender
+    },
+    {
+        path: 'data-entries/**',
+        renderMode: RenderMode.Client // TODO: Fails with Prerender
+    },
+    {
+        path: 'interactions/**',
+        renderMode: RenderMode.Prerender
+    },
+    {
+        path: 'notifications/**',
+        renderMode: RenderMode.Prerender
+    },
+    {
+        path: 'lists',
+        renderMode: RenderMode.Prerender
+    },
+    {
+        path: 'theming',
+        renderMode: RenderMode.Prerender
+    },
+    {
+        path: 'scheduling',
+        renderMode: RenderMode.Prerender
+    },
+    {
+        path: 'menus',
+        renderMode: RenderMode.Prerender
+    },
+    {
+        path: 'layouts',
+        renderMode: RenderMode.Prerender
+    },
+    {
+        path: 'services',
+        renderMode: RenderMode.Prerender
+    },
+    {
+        path: 'tree-grid',
+        renderMode: RenderMode.Prerender
+    },
+    {
+        path: 'grid',
+        renderMode: RenderMode.Prerender
+    },
   {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Client
   }
 ];
