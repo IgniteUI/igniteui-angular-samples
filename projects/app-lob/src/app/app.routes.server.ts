546b2a1116da62b11db33c355f5b8078ac460da7
Konstantin Dinev <kdinev@mail.bw.edu>
2025-02-28 08:14:05 +0200

    chore(*): shortening a route path without repetition

--- projects/app-lob/src/app/app.routes.server.ts
+++ projects/app-lob/src/app/app.routes.server.ts
@@ -10,7 +10,7 @@ export const serverRoutes: ServerRoute[] = [
         renderMode: RenderMode.Client // TODO: Fails with Prerender
     },
     {
-        path: 'grid-finjs-dock-manager/**',
+        path: 'grid-finjs-dock-manager',
         renderMode: RenderMode.Client // TODO: Fails with Prerender
     },
     {
