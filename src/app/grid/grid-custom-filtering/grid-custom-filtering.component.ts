928afba5e996102d55f91d9390b126c0c9c80162
Teodosia Hristodorova <52423497+teodosiah@users.noreply.github.com>
2025-03-07 12:32:38 +0200

    fix(grid): rename iconNames in custom filtering operands demos (#3620)

--- src/app/grid/grid-custom-filtering/grid-custom-filtering.component.ts
+++ src/app/grid/grid-custom-filtering/grid-custom-filtering.component.ts
@@ -33,7 +33,7 @@ export class CaseSensitiveFilteringOperand extends IgxStringFilteringOperand {
         super();
         const customOperations = [
             {
-                iconName: 'contains',
+                iconName: 'filter_contains',
                 isUnary: false,
                 logic: (target: string, searchVal: string, ignoreCase?: boolean) => {
                     ignoreCase = false;
@@ -44,7 +44,7 @@ export class CaseSensitiveFilteringOperand extends IgxStringFilteringOperand {
                 name: 'Contains (case sensitive)'
             },
             {
-                iconName: 'does-not-contain',
+                iconName: 'filter_does_not_contain',
                 isUnary: false,
                 logic: (target: string, searchVal: string, ignoreCase?: boolean) => {
                     ignoreCase = false;
