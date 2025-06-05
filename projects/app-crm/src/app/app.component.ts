04fd076009ac4b942f3e730de288faa5bc2c195b
Damyan Petev <damyanpetev@users.noreply.github.com>
2025-06-05 10:42:29 +0300

    refactor(crm): ng generate @angular/core:inject migration

--- projects/app-crm/src/app/app.component.ts
+++ projects/app-crm/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, OnInit, DOCUMENT } from '@angular/core';
+import { Component, OnInit, DOCUMENT, inject } from '@angular/core';
 import { SEOService } from '../../../app-lob/src/app/seo.service';
 import { RouterOutlet } from '@angular/router';
 
@@ -10,11 +10,12 @@ import { RouterOutlet } from '@angular/router';
     imports: [RouterOutlet]
 })
 export class AppComponent implements OnInit {
+    private seoService = inject(SEOService);
+    private document = inject<Document>(DOCUMENT);
+
     private title = 'Business propeller - Angular Data Grid Example';
     // eslint-disable-next-line max-len
     private metaDesc = 'In this angular grid example, you can see how users can customize their data view by leveraging the various features built into the grid.';
-
-    constructor(private seoService: SEOService, @Inject(DOCUMENT) private document: Document) { }
 
     public ngOnInit() {
         this.seoService.updateHeadProperties(this.title, this.metaDesc, this.document.defaultView.location.href);
