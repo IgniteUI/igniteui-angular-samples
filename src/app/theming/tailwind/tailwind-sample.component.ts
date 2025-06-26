0a27df608fc6f09f91017a1d42211de648d52bf4
didimmova <d.dimova11@gmail.com>
2025-06-26 12:03:22 +0300

    sample(tailwind): update tailwind sample

--- src/app/theming/tailwind/tailwind-sample.component.ts
+++ src/app/theming/tailwind/tailwind-sample.component.ts
@@ -1,21 +1,10 @@
 import { Component } from '@angular/core';
-import { Card } from '../../layouts/card/card.blueprint';
-import { IGX_CARD_DIRECTIVES, IgxDividerDirective, IgxIconButtonDirective, IgxRippleDirective, IgxIconComponent  } from 'igniteui-angular';
 
 @Component({
     standalone: true,
     selector: 'app-tailwind-sample',
     styleUrls: ['./tailwind-sample.component.scss'],
-    templateUrl: './tailwind-sample.component.html',
-    imports: [IGX_CARD_DIRECTIVES, IgxDividerDirective, IgxIconButtonDirective, IgxRippleDirective, IgxIconComponent]
+    templateUrl: './tailwind-sample.component.html'
 })
 
-export class TailwindSampleComponent {
-    public card = new Card({
-        icons: ['skip_previous', 'play_arrow', 'skip_next'],
-        imageUrl: 'assets/images/card/media/ROZES-Under-the-Grave.jpg',
-        subtitle: 'Under the Grave (2016)',
-        title: 'Rozes',
-        content: '"Under the Grave" by The Roses is a moody, emotional track exploring loss and remembrance. Haunting vocals and atmospheric melodies create a somber, reflective tone. It\'s a poignant blend of sorrow and beauty that lingers.'
-    });
-}
+export class TailwindSampleComponent {}
