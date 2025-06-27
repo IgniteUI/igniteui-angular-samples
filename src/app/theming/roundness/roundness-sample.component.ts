b18ba71d9963e2e24cb0f924a88240fa52206bc5
Adrian Petrov <adrian.ptrv1@gmail.com>
2025-06-27 15:34:47 +0300

    feat(theming): add roundness sample

---
+++ src/app/theming/roundness/roundness-sample.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { IgxChipComponent, IgxIconComponent, IgxPrefixDirective } from 'igniteui-angular';
+
+
+@Component({
+  selector: 'app-chip-simple',
+  styleUrls: ['./roundness-sample.component.css'],
+  templateUrl: './roundness-sample.component.html',
+  imports: [IgxChipComponent, IgxIconComponent, IgxPrefixDirective]
+})
+
+export class RoundnessComponent {
+}
