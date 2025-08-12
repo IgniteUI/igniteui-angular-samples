abe209d34ff7fe6d7949c7ce96eec7bdc6aeac41
Adrian Petrov <adrian.ptrv1@gmail.com>
2025-08-12 15:30:13 +0300

    feat(samples): add new calendar and input styling samples

--- src/app/data-entries/input-group/input-group-styling/input-group-styling.component.ts
+++ src/app/data-entries/input-group/input-group-styling/input-group-styling.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { IgxInputGroupComponent, IgxPrefixDirective, IgxLabelDirective, IgxInputDirective, IgxSuffixDirective, IgxIconComponent, IgxHintDirective } from 'igniteui-angular';
+import { IgxInputGroupComponent, IgxLabelDirective, IgxInputDirective, IgxHintDirective, THEME_TOKEN, ThemeToken } from 'igniteui-angular';
 
 @Component({
     selector: 'app-input-group-style',
     templateUrl: 'input-group-styling.component.html',
     styleUrls: ['input-group-styling.component.scss'],
-    imports: [IgxInputGroupComponent, IgxPrefixDirective, IgxLabelDirective, IgxInputDirective, IgxSuffixDirective, IgxIconComponent, IgxHintDirective]
+    imports: [IgxInputGroupComponent, IgxLabelDirective, IgxInputDirective, IgxHintDirective],
+    providers: [{provide: THEME_TOKEN, useFactory: () => new ThemeToken('indigo')}]
 })
 export class InputGroupStyleComponent { }
