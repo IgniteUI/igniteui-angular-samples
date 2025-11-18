daa39fe81aa239a0e78516ec61c79e7db923ad32
Simeon Simeonoff <sim.simeonoff@gmail.com>
2025-11-18 14:59:56 +0200

    refactor(custom-lite-grid): update sample to include themes

--- src/app/grid-lite/grid-lite-styling-custom/grid-lite-styling-custom.component.ts
+++ src/app/grid-lite/grid-lite-styling-custom/grid-lite-styling-custom.component.ts
@@ -1,4 +1,4 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit, inject } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit, inject, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { defineComponents, IgcSwitchComponent } from 'igniteui-webcomponents';
 import { IgcGridLite } from 'igniteui-grid-lite';
@@ -11,25 +11,26 @@ defineComponents(IgcSwitchComponent);
   selector: 'app-grid-lite-styling-custom',
   templateUrl: './grid-lite-styling-custom.component.html',
   styleUrls: ['./grid-lite-styling-custom.component.scss'],
+  encapsulation: ViewEncapsulation.None,
   imports: [CommonModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class GridLiteStylingCustomComponent implements OnInit {
   private dataService = inject(GridLiteDataService);
-  
+
   public data: ProductInfo[] = [];
   public columns: any[] = [];
   public theme: 'dark' | 'light' = 'dark';
 
   ngOnInit() {
     this.data = this.dataService.generateProducts(50);
-    
+
     this.columns = [
-      { 
-        key: 'name', 
-        headerText: 'Product', 
-        filter: true, 
-        sort: true 
+      {
+        key: 'name',
+        headerText: 'Product',
+        filter: true,
+        sort: true
       },
       {
         key: 'price',
@@ -38,15 +39,15 @@ export class GridLiteStylingCustomComponent implements OnInit {
         filter: true,
         sort: true
       },
-      { 
-        key: 'sold', 
-        type: 'number', 
+      {
+        key: 'sold',
+        type: 'number',
         headerText: 'Sold',
         filter: true,
         sort: true
       },
-      { 
-        key: 'total', 
+      {
+        key: 'total',
         headerText: 'Total',
         type: 'number',
         filter: true,
