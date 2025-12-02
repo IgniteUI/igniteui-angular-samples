a8bd10a9531ba9275924722fd42bfdce0985db57
Maria Tsvyatkova <mtsvyatkova@infragistics.com>
2025-12-02 05:10:02 +0200

    fix(pdf): add exportData sample

---
+++ src/app/services/export-pdf/pdf-export.component.ts
@@ -0,0 +1,23 @@
+import { Component, inject, signal } from '@angular/core';
+import { IgxPdfExporterOptions, IgxPdfExporterService } from 'igniteui-angular/grids/core';
+import { IgxButtonDirective } from 'igniteui-angular/directives';
+
+@Component({
+    selector: 'app-pdf-export',
+    styleUrls: ['./pdf-export.component.scss'],
+    templateUrl: './pdf-export.component.html',
+    imports: [IgxButtonDirective]
+})
+export class PdfExportComponent {
+  private pdfExportService = inject(IgxPdfExporterService);
+
+  public localData = signal([
+    { Name: 'Eric Ridley', Age: '26' },
+    { Name: 'Alanis Brook', Age: '22' },
+    { Name: 'Jonathan Morris', Age: '23' }
+  ]);
+
+  public exportButtonHandler() {
+    this.pdfExportService.exportData(this.localData(), new IgxPdfExporterOptions('ExportFileFromData'));
+  }
+}
