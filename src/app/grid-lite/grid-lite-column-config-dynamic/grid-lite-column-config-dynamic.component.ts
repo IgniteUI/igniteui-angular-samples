9281d013dbbcba895e467704128206280a96dd4a
Mariela Tihova <MTihova@infragistics.com>
2026-02-18 22:50:29 +0200

    fix(column-config-dynamic): dropdown, non-working filtering and other

--- src/app/grid-lite/grid-lite-column-config-dynamic/grid-lite-column-config-dynamic.component.ts
+++ src/app/grid-lite/grid-lite-column-config-dynamic/grid-lite-column-config-dynamic.component.ts
@@ -1,18 +1,30 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { defineComponents, IgcButtonComponent, IgcCheckboxComponent, IgcDropdownComponent, IgcSwitchComponent } from 'igniteui-webcomponents';
+import { IgxButtonDirective, IgxToggleActionDirective } from 'igniteui-angular/directives';
+import { IgxCheckboxComponent } from 'igniteui-angular/checkbox';
+import { IgxDropDownComponent, IgxDropDownItemComponent, IgxDropDownItemNavigationDirective } from 'igniteui-angular/drop-down';
 import { IgcGridLite } from 'igniteui-grid-lite';
+import { defineComponents, IgcRatingComponent } from 'igniteui-webcomponents';
 import { GridLiteDataService, ProductInfo } from '../grid-lite-data.service';
 
 IgcGridLite.register();
-defineComponents(IgcCheckboxComponent, IgcDropdownComponent, IgcSwitchComponent, IgcButtonComponent);
+defineComponents(IgcRatingComponent);
 
 @Component({
   selector: 'app-grid-lite-column-config-dynamic',
   templateUrl: './grid-lite-column-config-dynamic.component.html',
   styleUrls: ['./grid-lite-column-config-dynamic.component.scss'],
-  imports: [CommonModule],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  imports: [
+    CommonModule,
+    IgxButtonDirective,
+    IgxToggleActionDirective,
+    IgxDropDownItemNavigationDirective,
+    IgxDropDownComponent,
+    IgxDropDownItemComponent,
+    IgxCheckboxComponent
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  standalone: true
 })
 export class GridLiteColumnConfigDynamicComponent implements OnInit {
   private dataService = inject(GridLiteDataService);
@@ -20,18 +32,22 @@ export class GridLiteColumnConfigDynamicComponent implements OnInit {
   public data: ProductInfo[] = [];
   public hasFormatters = true;
 
-  public formatter = new Intl.NumberFormat('en-150', {
-    style: 'currency',
-    currency: 'EUR'
-  });
+  public columns = [
+    { field: 'id', header: 'ID', hidden: true, resizable: true, sortable: false, filterable: false },
+    { field: 'name', header: 'Product Name', hidden: false, resizable: true, sortable: false, filterable: false },
+    { field: 'price', header: 'Price', hidden: false, resizable: true, sortable: false, filterable: false },
+    { field: 'sold', header: 'Units sold', hidden: false, resizable: true, sortable: false, filterable: false, dataType: 'number' },
+    { field: 'total', header: 'Total sold', hidden: false, resizable: true, sortable: false, filterable: false },
+    { field: 'rating', header: 'Customer rating', hidden: false, resizable: true, sortable: false, filterable: false, dataType: 'number' }
+  ];
+
+  public formatter = new Intl.NumberFormat('en-150', { style: 'currency', currency: 'EUR' });
 
   ngOnInit() {
     this.data = this.dataService.generateProducts(50);
   }
 
-  protected formatCurrency = (params: any) => {
-    return this.formatter.format(params.value);
-  };
+  protected formatCurrency = (params: any) => this.formatter.format(params.value);
 
   protected ratingTemplate = (params: any) => {
     const rating = document.createElement('igc-rating');
@@ -40,4 +56,14 @@ export class GridLiteColumnConfigDynamicComponent implements OnInit {
     rating.setAttribute('value', params.value.toString());
     return rating;
   };
+
+  toggleColumnProperty(column: any, property: string, value: boolean) {
+    column[property] = value;
+    // Trigger Angular change detection
+    this.columns = [...this.columns];
+  }
+
+  toggleFormatters(event: any) {
+    this.hasFormatters = event.detail.checked;
+  }
 }
