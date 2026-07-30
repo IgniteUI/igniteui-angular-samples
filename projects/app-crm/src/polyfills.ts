972665dc1797d473f403c8cedf92f04eb925c7f4
Viktor Kombov <vkombov@infragistics.com>
2026-07-30 15:08:44 +0300

    Update igniteui-angular and related packages to 22.1.0-beta.0

--- projects/app-crm/src/polyfills.ts
+++ projects/app-crm/src/polyfills.ts
@@ -1,5 +1,4 @@
 // /***************************************************************************************************
 //  * Zone JS is required by Angular itself.
 // //  */
-import 'hammerjs/hammer';
 import 'zone.js';  // Included with Angular CLI.
