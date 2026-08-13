de011017b4600a0165aab808747b19f139576d45
Simeon Simeonoff <sim.simeonoff@gmail.com>
2026-08-13 14:50:28 +0300

    fix(live-editing): remove stale layout references

--- live-editing/configs/CalendarConfigGenerator.ts
+++ live-editing/configs/CalendarConfigGenerator.ts
@@ -75,7 +75,6 @@ export class CalendarConfigGenerator implements IConfigGenerator {
 
         configs.push(new Config({
             component: 'CalendarStylingSampleComponent',
-            additionalFiles: ["/src/app/scheduling/calendar/calendar-styling-sample/layout.scss"],
             appConfig: BaseAppConfig,
             shortenComponentPathBy: "/scheduling/calendar/"
         }));
