f7a74fc4820179bed7470d17382db2bf023abf0b
Simeon Simeonoff <sim.simeonoff@gmail.com>
2026-08-28 17:22:22 +0300

    revert(theming): restore eager routes for Material and Bootstrap samples

--- src/app/theming/theming.routes.ts
+++ src/app/theming/theming.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { AnimationsSampleComponent } from './animations/animations-sample-1/animations-sample-1.component';
 import { AnimationsSample2Component } from './animations/animations-sample-2/animations-sample-2.component';
+import { AngularMaterialComponent } from './angular/angular-sample.component';
+import { BootstrapComponent } from './bootstrap/bootstrap-sample.component';
 import { DefaultThemeSampleComponent } from './default-theme-sample/default-theme-sample.component';
 import { GridThemePlaygroundComponent } from './grid-theme-playground/grid-theme-playground.component';
 import { DisplayDensityComponent } from './display-density/display-density.component';
@@ -54,12 +56,12 @@ export const ThemingRoutes: Routes = [
         path: 'animations-sample-2'
     },
     {
-        loadComponent: () => import('./angular/angular-sample.component').then(m => m.AngularMaterialComponent),
+        component: AngularMaterialComponent,
         data: themingRoutesData['angular-sample'],
         path: 'angular-sample'
     },
     {
-        loadComponent: () => import('./bootstrap/bootstrap-sample.component').then(m => m.BootstrapComponent),
+        component: BootstrapComponent,
         data: themingRoutesData['bootstrap-sample'],
         path: 'bootstrap-sample'
     },
