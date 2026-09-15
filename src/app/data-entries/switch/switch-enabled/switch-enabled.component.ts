721ee8489286eb73d127b5f06dc4a386878188e0
Hristo Hristov <57346540+Hristo313@users.noreply.github.com>
2026-09-15 17:07:00 +0300

    feat(switch): update samples content (#4025)
    * feat(switch): update samples content
    
    * fix(switch): update styling sample css
    
    * fix(switch): update theme to bootstrap
    
    ---------
    
    Co-authored-by: Stamen Stoychev <sstoychev@infragistics.com>

---
+++ src/app/data-entries/switch/switch-enabled/switch-enabled.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { IgxSwitchComponent } from 'igniteui-angular/switch';
+
+@Component({
+    selector: 'app-switch-enabled',
+    styleUrls: ['./switch-enabled.component.scss'],
+    templateUrl: './switch-enabled.component.html',
+    imports: [IgxSwitchComponent]
+})
+export class SwitchEnabledComponent { }
